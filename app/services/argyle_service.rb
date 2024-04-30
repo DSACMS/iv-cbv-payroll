ca1219b8c53efbec828fc09b7f7fe10a8446a88f
George Byers <github@georgebyers.me>
2024-04-30 11:49:08 -0400

    ArgyleService is working, removing .env file from version control

--- app/services/argyle_service.rb
+++ app/services/argyle_service.rb
@@ -1,36 +1,28 @@
 # frozen_string_literal: true
-require 'net/http'
-require 'logger'
-require 'faraday'
+require "faraday"
 
 class ArgyleService
-  ARGYLE_BASE_URL = 'https://api-sandbox.argyle.com/v2/'
-
   def initialize(api_key)
     @api_key = api_key
-    @url = URI.parse(ARGYLE_BASE_URL)
+    base_url = ENV["ARGYLE_API_URL"] || "https://api-sandbox.argyle.com/v2"
     client_options = {
       request: {
         open_timeout: 5,
         timeout: 5
       },
-      ssl: {
-        verify: @url.scheme == 'https'
-      },
-      url: ARGYLE_BASE_URL,
+      url: base_url,
       headers: {
-        'Content-Type' => 'application/json',
-        'Authorization ' => "Bearer #{@api_key}"
-      },
+        "Content-Type" => "application/json",
+        "Authorization" => "Basic #{@api_key}"
+      }
     }
-
     @http = Faraday.new(client_options)
   end
 
   # Fetch all Argyle items
   def items(query = nil)
     # get "items" and pass the query as the q parameter for the Faraday instance
-    response = @http.get('items', { q: query })
+    response = @http.get("items", { q: query })
     JSON.parse(response.body)
   end
 end
