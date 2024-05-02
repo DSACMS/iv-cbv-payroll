6907c0c6361723e4719cf95d9b740633e166c3b0
Matt Gardner <wilburnforce@gmail.com>
2024-05-02 11:06:09 -0400

    Validate that the webhook is authorized

--- app/controllers/webhooks/argyle/events_controller.rb
+++ app/controllers/webhooks/argyle/events_controller.rb
@@ -2,13 +2,20 @@ class Webhooks::Argyle::EventsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    if params['event'] == 'paystubs.fully_synced'
-      @cbv_flow = CbvFlow.find_by_argyle_user_id(params['data']['user'])
+    digest = OpenSSL::Digest.new('sha512')
+    signature = OpenSSL::HMAC.hexdigest('SHA512', ENV['ARGYLE_WEBHOOK_SECRET'], request.raw_post)
 
-      if @cbv_flow
-        @cbv_flow.update(payroll_data_available_from: params['data']['available_from'])
-        ArgylePaystubsChannel.broadcast_to(@cbv_flow, params)
+    if request.headers["X-Argyle-Signature"] == signature
+      if params['event'] == 'paystubs.fully_synced'
+        @cbv_flow = CbvFlow.find_by_argyle_user_id(params['data']['user'])
+
+        if @cbv_flow
+          @cbv_flow.update(payroll_data_available_from: params['data']['available_from'])
+          ArgylePaystubsChannel.broadcast_to(@cbv_flow, params)
+        end
       end
+    else
+      render json: { error: 'Invalid signature' }, status: :unauthorized
     end
   end
 end
