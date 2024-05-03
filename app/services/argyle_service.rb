db7a15c525c6673a1d9dfd1b86389e652b314cf9
George Byers <github@georgebyers.me>
2024-05-03 14:01:49 -0400

    added ability to query payroll-data from argyle for connected account. added webhook logic to persist connected accounts into the database

--- app/services/argyle_service.rb
+++ app/services/argyle_service.rb
@@ -5,6 +5,7 @@ class ArgyleService
   def initialize
     @api_key = Rails.application.credentials.argyle[:api_key]
     base_url = ENV["ARGYLE_API_URL"] || "https://api-sandbox.argyle.com/v2"
+
     client_options = {
       request: {
         open_timeout: 5,
@@ -25,4 +26,19 @@ class ArgyleService
     response = @http.get("items", { q: query })
     JSON.parse(response.body)
   end
+
+  def payroll_documents(account_id, user_id)
+    # Check if the ConnectedArgyleAccount exists for the given user_id and account_id
+    account_exists = ConnectedArgyleAccount.exists?(user_id: user_id, account_id: account_id)
+
+    # Proceed with the HTTP request only if the account exists
+    if account_exists
+      response = @http.get("payroll-documents", { account: account_id, user: user_id })
+      JSON.parse(response.body)
+    else
+      # Return an appropriate message or handle the logic when the account does not exist
+      { error: "No matching account found for the provided user_id and account_id." }
+    end
+  end
+
 end
