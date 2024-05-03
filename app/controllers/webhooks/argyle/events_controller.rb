6990315d7fc2a8062f82d5731c10f478d789e428
George Byers <github@georgebyers.me>
2024-05-03 14:02:16 -0400

    uncommented signature check

--- app/controllers/webhooks/argyle/events_controller.rb
+++ app/controllers/webhooks/argyle/events_controller.rb
@@ -2,11 +2,11 @@ class Webhooks::Argyle::EventsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    # signature = OpenSSL::HMAC.hexdigest("SHA512", ENV["ARGYLE_WEBHOOK_SECRET"], request.raw_post)
-    #
-    # unless request.headers["X-Argyle-Signature"] == signature
-    #   return render json: { error: "Invalid signature" }, status: :unauthorized
-    # end
+    signature = OpenSSL::HMAC.hexdigest("SHA512", ENV["ARGYLE_WEBHOOK_SECRET"], request.raw_post)
+
+    unless request.headers["X-Argyle-Signature"] == signature
+      return render json: { error: "Invalid signature" }, status: :unauthorized
+    end
 
     if params["event"] == "paystubs.fully_synced" || params["event"] == "paystubs.partially_synced"
       @cbv_flow = CbvFlow.find_by_argyle_user_id(params["data"]["user"])
