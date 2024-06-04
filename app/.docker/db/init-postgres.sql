4df3cb1efc40f3507e58f2907614c7d92aebefc0
George Byers <github@georgebyers.me>
2024-06-04 15:54:05 -0400

    updated docker-compose to work as the production release works

--- app/.docker/db/init-postgres.sql
+++ app/.docker/db/init-postgres.sql
@@ -1,1 +1,8 @@
-CREATE DATABASE "app_rails";
+DO
+$do$
+BEGIN
+   IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'app') THEN
+      PERFORM dblink_exec('CREATE DATABASE "app"');
+END IF;
+END
+$do$;
