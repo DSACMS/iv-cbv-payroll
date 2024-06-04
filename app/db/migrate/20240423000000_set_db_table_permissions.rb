b811894fed7c5954557a01b626a064976ca7075e
Tom Dooner <tomdooner@navapbc.com>
2024-06-04 15:31:58 -0700

    Fix table permission migration so it runs in development
    In development, we may not have an "app" user and a "migrate" user.
    Also, this removes a duplicate migration.

--- app/db/migrate/20240423000000_set_db_table_permissions.rb
+++ app/db/migrate/20240423000000_set_db_table_permissions.rb
@@ -1,6 +1,10 @@
 # see https://github.com/navapbc/template-infra/blob/main/docs/infra/set-up-database.md#important-note-on-postgres-table-permissions
 class SetDbTablePermissions < ActiveRecord::Migration[7.0]
   def change
+    # This is only useful to fix permissions in deploy environments.
+    # In development: do nothing.
+    return if Rails.env.development?
+
     execute <<-SQL
       ALTER DEFAULT PRIVILEGES GRANT ALL ON TABLES TO app
     SQL
