426629c69404739bf68b995a14739df7b2ba536b
Tom Dooner <tomdooner@navapbc.com>
2024-06-07 15:41:16 -0700

    Add PDF test rendering endpoint
    I wanted to test PDF rendering without having to go through the whole
    flow. So this adds a development-only endpoint to test PDF rendering, at
    /health/test_rendering.pdf.
    
    This may be useful in production environments, too, but runs a DDOS
    risk, so that's why I've restricted its access for now. We could figure
    out a more secure approach to open it up later.

--- app/app/controllers/health_check_controller.rb
+++ app/app/controllers/health_check_controller.rb
@@ -2,4 +2,17 @@ class HealthCheckController < ActionController::Base
   def ok
     render json: { status: "ok", version: ENV["IMAGE_TAG"] }
   end
+
+  def test_rendering
+    return head :not_found unless Rails.env.development?
+
+    respond_to do |format|
+      format.html do
+        render inline: "Missing .pdf extension in the URL!"
+      end
+      format.pdf do
+        render pdf: "test_rendering", inline: "<strong>The PDF <em>works</em>!</strong>"
+      end
+    end
+  end
 end
