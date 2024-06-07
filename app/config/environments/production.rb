4e30ad20b0be86ca16cd950aadb47b5b4194f8fe
Tom Dooner <tomdooner@navapbc.com>
2024-06-07 14:42:00 -0700

    Set default host for emails based on a DOMAIN_NAME envvar
    When generating links in email, we need to specify a
    `default_url_options[:host]` configuration so Rails knows what the
    hostname will be. We have been doing this in development (with
    "localhost"); let's do the same thing in production (with whatever the
    value of the service's `domain_name` variable is).

--- app/config/environments/production.rb
+++ app/config/environments/production.rb
@@ -33,6 +33,8 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
+  routes.default_url_options[:host] = ENV["DOMAIN_NAME"]
+
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
