1406ab08a21a8516557ff0f4fe358a70d544e810
Tom Dooner <tomdooner@navapbc.com>
2024-06-07 11:24:53 -0700

    Add aws-sdk-sesv2 gem for AWS SES emails

--- app/config/environments/production.rb
+++ app/config/environments/production.rb
@@ -60,6 +60,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "iv_cbv_payroll_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :sesv2
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
