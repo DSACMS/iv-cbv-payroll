108d4982934d99ff3b6b8316d6f6873833becb82
George Byers <github@georgebyers.me>
2024-06-07 10:32:37 -0400

    removed the wkhtmltopdf gem to see if wickedpdf will fallback to the system-level binary

--- app/config/initializers/wicked_pdf.rb
+++ app/config/initializers/wicked_pdf.rb
@@ -24,7 +24,7 @@ WickedPdf.configure do |config|
   # config.exe_path = Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
 
   # Needed for wkhtmltopdf 0.12.6+ to use many wicked_pdf_temp asset helpers
-  config.enable_local_file_access = true
+  # config.enable_local_file_access = true
 
   # Layout file to be used for all PDFs
   # (but can be overridden in `render :pdf` calls)
