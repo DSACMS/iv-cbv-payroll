2fed99746b1189cb8307c521a29b15f552f85b9b
Matt Gardner <wilburnforce@gmail.com>
2024-06-11 14:06:24 -0400

    Rename

--- app/app/services/pinwheel_webhook_manager.rb
+++ app/app/services/pinwheel_webhook_manager.rb
@@ -1,4 +1,4 @@
-# This class manages argyle webhook subscriptions, and is intended for development environment setup only
+# This class manages pinwheel webhook subscriptions, and is intended for development environment setup only
 class PinwheelWebhookManager
   def initialize
     @pinwheel = PinwheelService.new
@@ -10,19 +10,19 @@ class PinwheelWebhookManager
     ngrok_subscriptions = subscriptions.find_all { |subscription| subscription["url"].match(format_identifier_hash(name)) }
 
     ngrok_subscriptions.each do |subscription|
-      puts "  Removing existing Argyle webhook subscription (url = #{subscription["url"]})"
+      puts "  Removing existing Pinwheel webhook subscription (url = #{subscription["url"]})"
       @pinwheel.delete_webhook_subscription(subscription["id"])
     end
   end
 
   def create_subscription(tunnel_url, name)
-    puts "  Registering Argyle webhooks for Ngrok tunnel..."
+    puts "  Registering Pinwheel webhooks for Ngrok tunnel..."
     response = @pinwheel.create_webhook_subscription([
       "account.added",
       "paystubs.added"
     ], URI.join(tunnel_url, "/webhooks/pinwheel/events", format_identifier_hash(name)))
     new_webhook_subscription_id = response["data"]["id"]
-    puts "  ✅ Set up Argyle webhooks (https://console.argyle.com/webhooks/#{new_webhook_subscription_id})"
+    puts "  ✅ Set up Pinwheel webhook: #{new_webhook_subscription_id}"
   end
 
   def format_identifier_hash(identifier)
