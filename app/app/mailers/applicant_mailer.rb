f6ed8b8258a11600b117b53f78550693dbbb73e6
George Byers <github@georgebyers.me>
2024-06-13 14:42:26 -0400

    fixing rubocop issues

--- app/app/mailers/applicant_mailer.rb
+++ app/app/mailers/applicant_mailer.rb
@@ -7,7 +7,7 @@ class ApplicantMailer < ApplicationMailer
   end
   
   def send_pdf_to_applicant(email_address, pdf_path)
-    attachments['income_verification.pdf'] = File.read(pdf_path)
+    attachments["income_verification.pdf"] = File.read(pdf_path)
     mail(to: email_address, subject: I18n.t("applicant_mailer.send_pdf_to_applicant.subject"))
   end
 end
