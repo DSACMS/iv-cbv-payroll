bf1fc3d298a63ee22dc132817454368a1ff84544
George Byers <github@georgebyers.me>
2024-06-13 23:05:17 -0400

    removed comment about retrieving the case_number from the session, it's irrelevant

--- app/app/mailers/applicant_mailer.rb
+++ app/app/mailers/applicant_mailer.rb
@@ -6,7 +6,6 @@ class ApplicantMailer < ApplicationMailer
     mail(to: params[:email_address], subject: I18n.t("applicant_mailer.invitation_email.subject"))
   end
 
-  # once the user is authenticated, we can should be able to pull the case_number from the session
   def send_pdf_to_caseworker(email_address, case_number)
     attachments["income_verification.pdf"] = WickedPdf.new.pdf_from_string(
       render_to_string("cbv_flows/summary.pdf.erb", layout: "mailer.html.erb")
