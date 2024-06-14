1bf725ceb1db037221e1ac779dcd268868cc66a3
George Byers <github@georgebyers.me>
2024-06-14 14:50:08 -0400

    updating mailer attachment to be properly rendered

--- app/app/mailers/applicant_mailer.rb
+++ app/app/mailers/applicant_mailer.rb
@@ -8,8 +8,8 @@ class ApplicantMailer < ApplicationMailer
 
   def send_pdf_to_caseworker(email_address, case_number)
     attachments["income_verification.pdf"] = WickedPdf.new.pdf_from_string(
-      render_to_string("cbv_flows/summary.pdf.erb", layout: "mailer.html.erb")
+      render_to_string(template: "cbv_flows/summary", formats: [ :pdf ])
     )
-    mail = mail(to: email_address, subject: I18n.t("applicant_mailer.send_pdf_to_caseworker.subject", case_number: case_number))
+    mail(to: email_address, subject: I18n.t("applicant_mailer.send_pdf_to_caseworker.subject", case_number: case_number))
   end
 end
--- app/spec/mailers/applicant_mailer_spec.rb
+++ app/spec/mailers/applicant_mailer_spec.rb
@@ -20,4 +20,23 @@ RSpec.describe ApplicantMailer, type: :mailer do
   it "renders the body" do
     expect(mail.body.encoded).to match(I18n.t('applicant_mailer.invitation_email.body'))
   end
+
+  describe 'send_pdf_to_caseworker' do
+    let(:email_address) { 'caseworker@example.com' }
+    let(:case_number) { '123ABC' }
+    let(:mail) { ApplicantMailer.send_pdf_to_caseworker(email_address, case_number) }
+
+    it 'renders the subject with case number' do
+      expect(mail.subject).to eq(I18n.t('applicant_mailer.send_pdf_to_caseworker.subject', case_number: case_number))
+    end
+
+    it 'sends to the correct email' do
+      expect(mail.to).to eq([email_address])
+    end
+
+    it 'attaches a PDF' do
+      expect(mail.attachments['income_verification.pdf']).to be_present
+      expect(mail.attachments['income_verification.pdf'].content_type).to start_with('application/pdf')
+    end
+  end
 end
