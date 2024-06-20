99b56bbe65b23b5a78a24f54378a31d53941ea8c
George Byers <github@georgebyers.me>
2024-06-20 12:18:03 -0400

    Updated ApplicationMailer so that it explicitly imports helpers (Rails views don't have a 'helper' method). Added en localization entries for email and PDF generation. Updated tests pertaining to PDFs and mailing (#66)

--- app/spec/mailers/previews/applicant_mailer_preview.rb
+++ app/spec/mailers/previews/applicant_mailer_preview.rb
@@ -1,13 +1,16 @@
 # Preview all emails at http://localhost:3000/rails/mailers/applicant_mailer
+require Rails.root.join('spec/support/test_helpers')
 class ApplicantMailerPreview < ActionMailer::Preview
+  include ViewHelper
+  include TestHelpers # in order to use stub_payments
+
   def invitation_email
     ApplicantMailer.with(email_address: "test@example.com", link: "http://example.com").invitation_email
   end
 
   def caseworker_summary_email
-    payments = stub_payments
-
-    cbv_flow = CbvFlow.create(case_number: "ABC1234", argyle_user_id: "abc-def-ghi", email_address: "test@example.com")
+    payments =  stub_payments
+    cbv_flow = CbvFlow.create(case_number: "ABC1234", argyle_user_id: "abc-def-ghi")
     ApplicantMailer.with(cbv_flow: cbv_flow, case_number: "12345", payments: payments).caseworker_summary_email
   end
 end
