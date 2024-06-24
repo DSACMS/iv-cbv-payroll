DSACMS/iv-cbv-payroll

commit 8b1a5c9b73adde5f1e37c940987ed4d9e2f5d354

Split CbvFlowsController into multiple controllers
Multiple controllers will be easier to test, reason about, and
simultaneously edit as we continue development. This commit splits up
the controllers as such:

* All CBV flow pages still have URLs under `/cbv/`
* The controllers are nested in the `app/controllers/cbv` directory, and
  are within a new `Cbv` Ruby module accordingly. E.g. `/cbv/entry` is
  in `app/controllers/cbv/entries_controller.rb` and the class name is
  `Cbv::EntriesController`.
* Common functionality is extracted into a `Cbv::BaseController` which
  all pages inherit from.

This refactor also changes a couple other things:
1. The invitation URL is now not in the `/cbv/` URL path. It is now
   `/invitations/new?secret=[secret]`. I've added a redirect.
2. Moves I18n keys to relative keys where possible

[Finishes FFS-807](https://jiraent.cms.gov/browse/FFS-807)

app/app/controllers/cbv/resets_controller.rb
@@ -1,0 +1,8 @@
+class Cbv::ResetsController < Cbv::BaseController
+  skip_before_action :set_cbv_flow
+
+  def reset
+    session[:cbv_flow_id] = nil
+    redirect_to root_url
+  end
+end

app/app/controllers/cbv/base_controller.rb
@@ -1,0 +1,66 @@
+class Cbv::BaseController < ApplicationController
+  before_action :set_cbv_flow
+
+  private
+
+  def set_cbv_flow
+    if params[:token].present?
+      invitation = CbvFlowInvitation.find_by(auth_token: params[:token])
+      if invitation.blank?
+        return redirect_to(root_url, flash: { alert: t("cbv.error_invalid_token") })
+      end
+
+      @cbv_flow = invitation.cbv_flow || CbvFlow.create_from_invitation(invitation)
+    elsif session[:cbv_flow_id]
+      begin
+        @cbv_flow = CbvFlow.find(session[:cbv_flow_id])
+      rescue ActiveRecord::RecordNotFound
+        return redirect_to root_url
+      end
+    else
+      # TODO: Restrict ability to enter the flow without a valid token
+      @cbv_flow = CbvFlow.create
+    end
+
+    session[:cbv_flow_id] = @cbv_flow.id
+  end
+
+  def set_payments
+    @payments = fetch_payroll.map do |payment|
+      {
+        employer: payment["employer_name"],
+        amount: payment["net_pay_amount"].to_i,
+        start: payment["pay_period_start"],
+        end: payment["pay_period_end"],
+        hours: payment["earnings"][0]["hours"],
+        rate: payment["earnings"][0]["rate"]
+      }
+    end
+  end
+
+  def next_path
+    case params[:controller]
+    when "cbv/entries"
+      cbv_flow_employer_search_path
+    when "cbv/employer_searches"
+      cbv_flow_summary_path
+    when "cbv/summaries"
+      cbv_flow_share_path
+    when "cbv/shares"
+      root_url
+    end
+  end
+  helper_method :next_path
+
+  def pinwheel
+    PinwheelService.new
+  end
+
+  def fetch_payroll
+    end_user_account_ids = pinwheel.fetch_accounts(end_user_id: @cbv_flow.pinwheel_end_user_id)["data"].map { |account| account["id"] }
+
+    end_user_account_ids.map do |account_id|
+      pinwheel.fetch_paystubs(account_id: account_id)["data"]
+    end.flatten
+  end
+end

app/app/controllers/cbv/employer_searches_controller.rb
@@ -1,0 +1,21 @@
+class Cbv::EmployerSearchesController < Cbv::BaseController
+  def show
+    @query = search_params[:query]
+    @employers = @query.blank? ? [] : fetch_employers(@query)
+  end
+
+  private
+
+  def search_params
+    params.permit(:query)
+  end
+
+  def fetch_employers(query = "")
+    request_params = {
+      q: query,
+      supported_jobs: [ "paystubs" ]
+    }
+
+    pinwheel.fetch_items(request_params)["data"]
+  end
+end

app/app/controllers/cbv/entries_controller.rb
@@ -1,0 +1,4 @@
+class Cbv::EntriesController < Cbv::BaseController
+  def show
+  end
+end

app/app/controllers/cbv/shares_controller.rb
@@ -1,0 +1,15 @@
+class Cbv::SharesController < Cbv::BaseController
+  before_action :set_payments, only: %i[update]
+
+  def show
+  end
+
+  def update
+    email_address = ENV["SLACK_TEST_EMAIL"]
+    ApplicantMailer.with(
+      email_address: email_address,
+      cbv_flow: @cbv_flow,
+      payments: @payments
+    ).caseworker_summary_email.deliver_now
+  end
+end

app/app/controllers/cbv/summaries_controller.rb
@@ -1,0 +1,24 @@
+class Cbv::SummariesController < Cbv::BaseController
+  before_action :set_payments, only: %i[show]
+
+  def show
+    respond_to do |format|
+      format.html
+      format.pdf do
+        render pdf: "#{@cbv_flow.id}"
+      end
+    end
+  end
+
+  def update
+    @cbv_flow.update(summary_update_params)
+
+    redirect_to next_path
+  end
+
+  private
+
+  def summary_update_params
+    params.fetch(:cbv_flow, {}).permit(:additional_information)
+  end
+end

app/app/controllers/cbv_flow_invitations_controller.rb
@@ -17,7 +17,7 @@ class CbvFlowInvitationsController < ApplicationController
                         error_message: ex.message
                        )
       Rails.logger.error("Error sending CBV invitation: #{ex.class} - #{ex.message}")
-      return redirect_to new_cbv_flow_invitation_path(secret: params[:secret])
+      return redirect_to new_invitation_path(secret: params[:secret])
     end
 
     flash[:notice] = t(".invite_success", email_address: cbv_flow_invitation_params[:email_address])

app/app/controllers/cbv_flows_controller.rb
@@ -1,117 +1,0 @@
-class CbvFlowsController < ApplicationController
-  before_action :set_cbv_flow
-  before_action :set_payments, only: [ :summary, :share ]
-
-  def entry
-  end
-
-  def employer_search
-    @query = search_params[:query]
-    @employers = @query.blank? ? [] : fetch_employers(@query)
-  end
-
-  def summary
-    if request.patch?
-      @cbv_flow.update(summary_update_params)
-      return redirect_to next_path
-    end
-
-    respond_to do |format|
-      format.html
-      format.pdf do
-        render pdf: "#{@cbv_flow.id}", template: "cbv_flows/summary"
-      end
-    end
-  end
-
-  def share
-    email_address = ENV["SLACK_TEST_EMAIL"]
-    ApplicantMailer.with(email_address: email_address, cbv_flow: @cbv_flow, payments: @payments).caseworker_summary_email.deliver_now
-  end
-
-  def reset
-    session[:cbv_flow_id] = nil
-    redirect_to root_url
-  end
-
-  private
-
-  def set_cbv_flow
-    if params[:token].present?
-      invitation = CbvFlowInvitation.find_by(auth_token: params[:token])
-      if invitation.blank?
-        return redirect_to(root_url, flash: { alert: t("cbv_flows.entry.error_invalid_token") })
-      end
-
-      @cbv_flow = invitation.cbv_flow || CbvFlow.create_from_invitation(invitation)
-    elsif session[:cbv_flow_id]
-      begin
-        @cbv_flow = CbvFlow.find(session[:cbv_flow_id])
-      rescue ActiveRecord::RecordNotFound
-        return redirect_to root_url
-      end
-    else
-      # TODO: Restrict ability to enter the flow without a valid token
-      @cbv_flow = CbvFlow.create
-    end
-
-    session[:cbv_flow_id] = @cbv_flow.id
-  end
-
-  def set_payments
-    @payments = fetch_payroll.map do |payment|
-      {
-        employer: payment["employer_name"],
-        amount: payment["net_pay_amount"].to_i,
-        start: payment["pay_period_start"],
-        end: payment["pay_period_end"],
-        hours: payment["earnings"][0]["hours"],
-        rate: payment["earnings"][0]["rate"]
-      }
-    end
-  end
-
-  def next_path
-    case params[:action]
-    when "entry"
-      cbv_flow_employer_search_path
-    when "employer_search"
-      cbv_flow_summary_path
-    when "summary"
-      cbv_flow_share_path
-    when "share"
-      root_url
-    end
-  end
-
-  helper_method :next_path
-
-  def fetch_employers(query = "")
-    request_params = {
-      q: query,
-      supported_jobs: [ "paystubs" ]
-    }
-
-    provider.fetch_items(request_params)["data"]
-  end
-
-  def fetch_payroll
-    end_user_account_ids = provider.fetch_accounts(end_user_id: @cbv_flow.pinwheel_end_user_id)["data"].map { |account| account["id"] }
-
-    end_user_account_ids.map do |account_id|
-      provider.fetch_paystubs(account_id: account_id)["data"]
-    end.flatten
-  end
-
-  def provider
-    PinwheelService.new
-  end
-
-  def search_params
-    params.permit(:query)
-  end
-
-  def summary_update_params
-    params.fetch(:cbv_flow, {}).permit(:additional_information)
-  end
-end

commit f975dfb5592ab36cc7b2daecf8685bc3da9165bf

Fix /cbv/reset controller method name
I had accidentally broken this in 8b1a5c9 by converting it to a RESTful
route but not changing the controller method name to match. Thankfully
[a code review comment][1] drew suggested doing this.

[1]: https://github.com/DSACMS/iv-cbv-payroll/pull/69#discussion_r1651356780

app/app/controllers/cbv/resets_controller.rb
@@ -1,7 +1,7 @@
 class Cbv::ResetsController < Cbv::BaseController
   skip_before_action :set_cbv_flow
 
-  def reset
+  def show
     session[:cbv_flow_id] = nil
     redirect_to root_url
   end