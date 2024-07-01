920063b7f0f16165142f1cce9d346af0506f04cd
George Byers <github@georgebyers.me>
2024-07-01 16:21:03 -0400

    Added New Relic Custom Events Module (#87)
    * Enabling new relic agent, adding newrelic_key env var to terraform config
    
    * add unsafe_inline csp rule
    
    * initial commit for new relic event module and service. deleting the service in the next commit since it requires additional config, but ensuring its in the git history in case we need to restore it
    
    * deleted new relic event service in favor of using a module
    
    * making rubocop happy by adding a new line

---
+++ app/spec/lib/new_relic_event_tracker_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+require 'new_relic_event_tracker'
+
+RSpec.describe NewRelicEventTracker do
+  describe '.track' do
+    let(:event_type) { 'TestEvent' }
+    let(:attributes) { { key: 'value' } }
+
+    it 'calls NewRelic::Agent.record_custom_event with correct parameters' do
+      expect(NewRelic::Agent).to receive(:record_custom_event).with(event_type, attributes)
+      described_class.track(event_type, attributes)
+    end
+
+    context 'when an error occurs' do
+      before do
+        allow(NewRelic::Agent).to receive(:record_custom_event).and_raise(StandardError.new('Test error'))
+      end
+
+      it 'logs an error message' do
+        expect(Rails.logger).to receive(:error).with("Failed to send New Relic event: Test error")
+        described_class.track(event_type, attributes)
+      end
+    end
+  end
+end
