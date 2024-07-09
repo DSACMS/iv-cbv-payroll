a4c7c4fc0bfbbb3e3d432aa5a8cdc9aaae70a44e
Joe Gasiorek <Joseph.J.Gasiorek@omb.eop.gov>
2024-07-09 09:12:23 -0700

    rubocop

--- app/app/controllers/pages_controller.rb
+++ app/app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   def home
-    unless params['format'].nil?
-      head 401, content_type: 'text/html'
+    unless params["format"].nil?
+      head 401, content_type: "text/html"
     end
   end
 end
