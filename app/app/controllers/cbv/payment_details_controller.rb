934f3f914d0593e984bc78ee0e7f2f238077d1e2
Matt Gardner <wilburnforce@gmail.com>
2024-07-12 13:42:44 -0400

    Add new endpoints, show employment info

--- app/app/controllers/cbv/payment_details_controller.rb
+++ app/app/controllers/cbv/payment_details_controller.rb
@@ -1,17 +1,45 @@
 class Cbv::PaymentDetailsController < Cbv::BaseController
-  helper_method :employer_name, :start_date, :end_date, :gross_pay
+  helper_method :employer_name,
+    :start_date,
+    :end_date,
+    :gross_pay,
+    :employment_start_date,
+    :employment_end_date,
+    :employment_status,
+    :pay_period_frequency,
+    :compensation_amount
 
   def show
     account_id = params[:user][:account_id]
+    @employment = pinwheel.fetch_employment(account_id: account_id)["data"]
+    @income_metadata = pinwheel.fetch_income_metadata(account_id: account_id)["data"]
     @payments = set_payments account_id
   end
 
   private
 
   def employer_name
-    if @payments.any?
-      @payments.first[:employer_name]
-    end
+    @employment["employer_name"]
+  end
+
+  def employment_start_date
+    @employment["start_date"]
+  end
+
+  def employment_end_date
+    @employment["termination_date"]
+  end
+
+  def employment_status
+    @employment["status"]
+  end
+
+  def pay_period_frequency
+    @income_metadata["compensation_unit"]
+  end
+
+  def compensation_amount
+    @income_metadata["compensation_amount"]
   end
 
   def start_date
