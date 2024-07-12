5f43aad19eac6490962b90081488f33203a89556
Joe Gasiorek <Joseph.J.Gasiorek@omb.eop.gov>
2024-07-12 06:45:37 -0700

    FFS-1091: updates to /agreement page

--- app/app/controllers/cbv/agreements_controller.rb
+++ app/app/controllers/cbv/agreements_controller.rb
@@ -1,4 +1,16 @@
 class Cbv::AgreementsController < Cbv::BaseController
   def show
   end
+
+  def create
+    if params["agreement"] == "1"
+      NewRelicEventTracker.track("ApplicantAgreed", {
+        timestamp: Time.now.to_i,
+        cbv_flow_id: @cbv_flow.id
+      })
+      redirect_to cbv_flow_employer_search_path
+    else
+      redirect_to(cbv_flow_agreement_path, flash: { alert: t(".error") })
+    end
+  end
 end
