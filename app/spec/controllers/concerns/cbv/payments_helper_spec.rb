ffd8f7bf88c7bc46fa3ff6ddf61915dc75fcd252
Matt Gardner <wilburnforce@gmail.com>
2024-07-17 13:46:41 -0400

    Add test for payment structurign

--- app/spec/controllers/concerns/cbv/payments_helper_spec.rb
+++ app/spec/controllers/concerns/cbv/payments_helper_spec.rb
@@ -12,9 +12,29 @@ RSpec.describe Cbv::PaymentsHelper, type: :helper do
       helper.parse_payments(payments)
     end
 
+    it "parses payments" do
+      expect(helper.parse_payments(payments)).to eq(
+        [ { account_id: "5c1952df-3a84-4f28-8318-58291452061f",
+          amount: 321609,
+          deductions:            [ { amount: 7012, category: "retirement" },
+            { amount: 57692, category: "commuter" } ],
+          employer: "Acme Corp",
+          end: "2020-12-24",
+          gross_pay_amount: 480720,
+          hours: 80,
+          pay_date: "2020-12-31",
+          rate: 4759,
+          start: "2020-12-10" } ]
+      )
+    end
+
     it "groups by employer" do
       expect(helper.summarize_by_employer(parsed_payments)).to eq({
-        "5c1952df-3a84-4f28-8318-58291452061f" => { employer_name: "Acme Corp", payments: [ { account_id: "5c1952df-3a84-4f28-8318-58291452061f", amount: 321609, deductions: [ { amount: 7012, category: "retirement" }, { amount: 57692, category: "commuter" } ], employer: "Acme Corp", end: "2020-12-24", gross_pay_amount: 480720, hours: 80, pay_date: "2020-12-31", rate: 4759, start: "2020-12-10" } ], total: 321609 }
+        "5c1952df-3a84-4f28-8318-58291452061f" => {
+          employer_name: "Acme Corp",
+          payments: [ { account_id: "5c1952df-3a84-4f28-8318-58291452061f", amount: 321609, deductions: [ { amount: 7012, category: "retirement" }, { amount: 57692, category: "commuter" } ], employer: "Acme Corp", end: "2020-12-24", gross_pay_amount: 480720, hours: 80, pay_date: "2020-12-31", rate: 4759, start: "2020-12-10" } ],
+          total: 321609
+        }
       })
     end
   end
