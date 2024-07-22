518c70bf172de0ce8c545098e0dfd0f1a5ef1586
George Byers <github@georgebyers.me>
2024-07-22 14:03:27 -0400

    fixed broken paths. I renamed successes->success, but reverted due to following rails convention. updated controller logic for successes to pass failing test

--- app/app/controllers/cbv/successes_controller.rb
+++ app/app/controllers/cbv/successes_controller.rb
@@ -1,20 +1,23 @@
 class Cbv::SuccessesController < Cbv::BaseController
-  helper_method :generate_confirmation_number
+  helper_method :confirmation_number
 
   def show
     if @cbv_flow.confirmation_number.blank?
-      @cbv_flow.update(confirmation_number: generate_confirmation_number)
+      confirmation_number = generate_confirmation_number
+      @cbv_flow.update(confirmation_number: confirmation_number)
     end
     render :show
   end
 
   private
 
+  def confirmation_number
+    @cbv_flow["confirmation_number"]
+  end
+
   def generate_confirmation_number(prefix = nil)
-    if(@cbv_flow.confirmation_number.blank?)
-      @cbv_flow.update(confirmation_number: confirmation_number)
-    end
     confirmation_number = "#{@cbv_flow.id}-#{Time.now.to_i.to_s(36)}"
     prefix.present? ? "#{prefix}-#{confirmation_number}" : confirmation_number
   end
+
 end
