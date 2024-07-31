d92fd19816970b029f7f973819c82a98cb8f8491
Matt Gardner <wilburnforce@gmail.com>
2024-07-31 10:48:10 -0400

    Drop the validation

--- app/lib/site_config.rb
+++ app/lib/site_config.rb
@@ -28,7 +28,7 @@ class SiteConfig
 
       raise ArgumentError.new("Site missing id") if @id.blank?
       raise ArgumentError.new("Site #{@id} missing required attribute `agency_name`") if @agency_name.blank?
-      raise ArgumentError.new("Site #{@id} missing required dependent attribute `transmission_method_configuration.email`") if @transmission_method.present? && @transmission_method_configuration.dig("email").blank?
+      # TODO: Add a validation for the dependent attribute, transmission_method_configuration.email, if transmission_method is present
     end
   end
 end
