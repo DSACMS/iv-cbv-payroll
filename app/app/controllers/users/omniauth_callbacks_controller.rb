abccba46dc02536754b8d450f9c5c4963d921d56
Matt Gardner <wilburnforce@gmail.com>
2024-07-31 22:49:34 -0400

    Find or create the auth’d email

--- app/app/controllers/users/omniauth_callbacks_controller.rb
+++ app/app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,12 +1,14 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def ma_dta
     response_params = request.env['omniauth.auth']['info']
-    @user = User.find_by!(email: response_params['email'])
-    
+    # TODO: Check that the email is permissible according to its domain
+    @user = User.find_or_create_by(email: response_params['email'])
+  
     if @user&.persisted?
+      flash[:notice] = 'Signed in!'
       sign_in_and_redirect @user, event: :authentication
     else
-      flash[:danger] = 'You have not yet an account!'
+      flash[:alert] = 'You have not yet an account!'
       redirect_back(fallback_location: root_path)
     end
   end
