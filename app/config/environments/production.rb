DSACMS/iv-cbv-payroll

commit 7befe9697cd0c405ffe437ce011b5cb6ec3963ed

Require SSL/TLS for all web requests in production environments
Both demo and production actually are runinng in production, so we
should be able to test this in demo.

app/config/environments/production.rb
@@ -45,7 +45,7 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Include generic and useful information about system operation, but avoid logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII).

app/config/environments/ci.rb
@@ -2,6 +2,7 @@ require_relative "production"
 
 Rails.application.configure do
   config.public_file_server.enabled = true
+  config.force_ssl = false
 
   logger = ActiveSupport::Logger.new($stdout)
   logger.formatter = config.log_formatter

commit 8c21554c1112c082a35bb02f0925868e9c492071

Fix healthchecks by not requiring SSL
Healthchecks are failing after 7befe96 because they now try to redirect
to HTTPS. Since we can't configure the health checker to pass the
"X-Forwarded-Proto" header, let's just exempt this one path from the SSL
check.

app/config/environments/production.rb
@@ -46,6 +46,11 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
+  config.ssl_options = {
+    redirect: {
+      exclude: ->(request) { %r{^/health}.match?(request.path) }
+    }
+  }
 
   # Include generic and useful information about system operation, but avoid logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII).