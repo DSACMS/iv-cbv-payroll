ac433b5193ee58a544dfdab7a1b7ba7e123d59e0
Tom Dooner <tomdooner@navapbc.com>
2024-08-05 15:38:41 -0700

    Add Azure SSO for "sandbox" site (#136)
    * Duplicate the SSO logic for MA and Sandbox
    * DRY it up in the callbacks controller
    * Create the sign in button appropriately

--- app/app/models/user.rb
+++ app/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  devise :omniauthable, omniauth_providers: %i[ma_dta nyc_dss]
+  devise :omniauthable, omniauth_providers: %i[ma_dta nyc_dss sandbox]
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :rememberable, :trackable, :timeoutable, :omniauthable
