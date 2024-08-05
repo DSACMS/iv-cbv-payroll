67f0627b6e263c056892841e8cfc70c016f5ce98
Matt Gardner <wilburnforce@gmail.com>
2024-08-05 17:31:02 -0400

    Add conditional signout button

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
       omniauth_callbacks: "users/omniauth_callbacks"
     }
 
+  devise_scope :user do
+    delete "sign_out", to: "devise/sessions#destroy", as: :destroy_user_session
+  end
+
   if Rails.env.development?
     mount Sidekiq::Web => "/sidekiq"
   end
