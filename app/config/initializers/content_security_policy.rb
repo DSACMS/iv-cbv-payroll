a90251eff79c3c413f4486115903073c768a03b9
Tom Dooner <tomdooner@navapbc.com>
2024-08-06 15:52:58 -0700

    Fix CSP configuration for app
    * Use `content_security_policy_nonce` in usa banner partial so it
      actually has the right nonce rather than the literal nonce "true"
    * Update CSP config to use require for <script> and <style> tags
    * Remove unused origins from CSP config
    * Disable CSP on Pinwheel page as it blocks Pinwheel inline <style> tags
      from loading. Annoying, but, we are using Pinwheel and we should try
      to not break it via CSP blocking random resources it tries to load.

--- app/config/initializers/content_security_policy.rb
+++ app/config/initializers/content_security_policy.rb
@@ -7,23 +7,22 @@
 Rails.application.configure do
   config.content_security_policy do |policy|
     policy.default_src :self
-    policy.font_src :self, "https://*.cloudinary.com"
+    policy.font_src :self
     policy.form_action :self, "https://login.microsoftonline.com"
     policy.frame_ancestors :none
-    policy.img_src :self, :data, "https://*.cloudinary.com", "http://*.cloudinary.com", "https://www.google-analytics.com", "https://cdn.getpinwheel.com"
+    policy.img_src :self, :data, "https://*.cloudinary.com", "https://cdn.getpinwheel.com"
     policy.object_src :none
-    policy.script_src :self, :unsafe_inline, "https://js-agent.newrelic.com", "https://*.nr-data.net", "https://dap.digitalgov.gov", "https://www.google-analytics.com", "https://cdn.getpinwheel.com"
+    policy.script_src :self, "https://js-agent.newrelic.com", "https://*.nr-data.net", "https://cdn.getpinwheel.com"
     policy.connect_src :self, "https://*.nr-data.net"
     policy.worker_src :self, "blob:"
     policy.frame_src :self, "https://cdn.getpinwheel.com"
-    # 'unsafe-inline' is needed because Turbo uses inline CSS for at least the progress bar
-    policy.style_src :self, "'unsafe-inline'"
+    policy.style_src :self
   end
 
   #
   #   # Generate session nonces for permitted importmap and inline scripts
   config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
-  config.content_security_policy_nonce_directives = %w[script-src]
+  config.content_security_policy_nonce_directives = %w[script-src style-src]
   #
   #   # Report violations without enforcing the policy.
   #   # config.content_security_policy_report_only = true
