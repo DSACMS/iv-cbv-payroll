e6023cfdbed13be854ea2159f6fbb5855a6a8114
Matt Gardner <wilburnforce@gmail.com>
2024-08-07 15:49:01 -0400

    Add test coverage for pinwheel account supported jobs status

--- app/app/models/pinwheel_account.rb
+++ app/app/models/pinwheel_account.rb
@@ -2,7 +2,6 @@ class PinwheelAccount < ApplicationRecord
   belongs_to :cbv_flow
 
   def has_fully_synced?
-    # paystubs_synced_at.present? && employment_synced_at.present? && income_synced_at.present?
     (supported_jobs.exclude?("paystubs") || paystubs_synced_at.present?) &&
     (supported_jobs.exclude?("employment") || employment_synced_at.present?) &&
     (supported_jobs.exclude?("income") || income_synced_at.present?)
