e64dd0d8a2b4160cb2d8d0a434d9c70c4cae7c1b
George Byers <github@georgebyers.me>
2024-08-08 23:31:06 -0400

    fixed locales, removed confirmation_number from schema.rb

--- app/db/schema.rb
+++ app/db/schema.rb
@@ -45,7 +45,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_08_205627) do
     t.string "pinwheel_token_id"
     t.uuid "pinwheel_end_user_id", default: -> { "gen_random_uuid()" }, null: false
     t.jsonb "additional_information", default: {}
-    t.string "confirmation_number"
     t.string "site_id"
     t.string "confirmation_code"
     t.index ["cbv_flow_invitation_id"], name: "index_cbv_flows_on_cbv_flow_invitation_id"
