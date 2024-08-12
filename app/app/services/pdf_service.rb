085ac247aa49947c1ba483ed5655942f74197560
George Byers <github@georgebyers.me>
2024-08-12 15:36:03 -0400

    minor changes to the generate_pdf service

--- app/app/services/pdf_service.rb
+++ app/app/services/pdf_service.rb
@@ -1,14 +1,12 @@
 class PdfService
   def generate_pdf(template, variables)
-    pdf_content = WickedPdf.new.pdf_from_string(
-      render_to_string(template: template, variables: variables)
-    )
-
+    pdf_content = render_to_string(template: template, variables: variables)
+    pdf_file = WickedPdf.new.pdf_from_string(pdf_content)
     file_name = "#{SecureRandom.uuid}.pdf"
     file_path = Rails.root.join("tmp", file_name)
 
     FileUtils.mkdir_p(File.dirname(file_path))
-    File.open(file_path, "wb") { |file| file.write(pdf_content) }
+    File.open(file_path, "wb") { |file| file.write(pdf_file) }
 
     file_path
   end
