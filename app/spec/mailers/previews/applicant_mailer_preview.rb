f12dc1cbe58d32c82d8e7905653d1c630db12fd3
Tom Dooner <tomdooner@navapbc.com>
2024-08-15 12:23:38 -0700

    Fix mailer previews so they work with FactoryBot
    They were returning an error because when FactoryBot is used outside of
    rspec, you have to manually call `find_definitions`. This may be
    overkill, but I created a BaseMailerPreview class so we can get that
    behavior automatically in other preview clasess.
    
    This also adds a new mailer preview for DTA vs NYC so we can try viewing
    them both.

--- app/spec/mailers/previews/applicant_mailer_preview.rb
+++ app/spec/mailers/previews/applicant_mailer_preview.rb
@@ -1,12 +1,16 @@
 # Preview all emails at http://localhost:3000/rails/mailers/applicant_mailer
-require Rails.root.join('spec/support/test_helpers')
-class ApplicantMailerPreview < ActionMailer::Preview
+class ApplicantMailerPreview < BaseMailerPreview
   include ViewHelper
-  include TestHelpers
 
-  def invitation_email
+  def invitation_email_dta
     ApplicantMailer.with(
-      cbv_flow_invitation: create(:cbv_flow_invitation)
+      cbv_flow_invitation: FactoryBot.create(:cbv_flow_invitation, :ma)
+    ).invitation_email
+  end
+
+  def invitation_email_nyc
+    ApplicantMailer.with(
+      cbv_flow_invitation: FactoryBot.create(:cbv_flow_invitation, :nyc)
     ).invitation_email
   end
 end
