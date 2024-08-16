033eb9379ccdab1b939a5c1465c641ce26acfe17
George Byers <github@georgebyers.me>
2024-08-16 17:22:29 -0400

    added a title_checker test helper, added content_for referencing h1 for pages

---
+++ app/spec/support/title_checker_helper.rb
@@ -0,0 +1,60 @@
+module AutoTitleTestHelper
+  H1_REGEX = /<h1>(.*?)<\/h1>/im
+  TITLE_REGEX = /<title>(.*?)<\/title>/im
+  INTERPOLATION_REGEX = /%\{.*?}/
+
+  def assert_title_contains_h1(response_body)
+    h1_content = decode_html_entities(extract_content(response_body, H1_REGEX))
+    expect(h1_content).to be_present, "H1 is missing"
+
+    title_content = decode_html_entities(extract_content(response_body, TITLE_REGEX))
+    expect(title_content).to be_present, "Title is missing"
+
+    cleaned_title = remove_interpolation_placeholders(title_content)
+    main_title = cleaned_title.split('|').first.strip
+
+    expect(h1_content).to include(main_title), "H1 and title content differ: `#{h1_content}` vs `#{main_title}`"
+  end
+
+  private
+
+  def extract_content(html, regex)
+    match = html.match(regex)
+    match ? match[1].strip : nil
+  end
+
+  def remove_interpolation_placeholders(text)
+    text.gsub(INTERPOLATION_REGEX, '').squeeze(' ').strip
+  end
+
+  def decode_html_entities(text)
+    CGI.unescapeHTML(text)
+  end
+end
+
+RSpec.configure do |config|
+  config.include AutoTitleTestHelper, type: :controller
+
+  config.before(:suite) do
+    config.instance_variable_set(:@checked_routes, Set.new)
+  end
+
+  config.after(:each, type: :controller) do |example|
+    if should_check_title?(request, response)
+      route_key = "#{request.method}:#{request.path}"
+      checked_routes = config.instance_variable_get(:@checked_routes)
+
+      unless checked_routes.include?(route_key)
+        assert_title_contains_h1(response.body)
+        checked_routes.add(route_key)
+      end
+    end
+  end
+
+  def should_check_title?(request, response)
+    (request.get? || request.params[:action] == 'show') &&
+      !%w[new create].include?(request.params[:action]) &&
+      response.status == 200 &&
+      response.content_type =~ /html/
+  end
+end
