1e8847792dc00598e6fb76492c79360975384a60
George Byers <github@georgebyers.me>
2024-08-19 17:17:09 -0400

    fixed some weird spacing issues

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -2,10 +2,10 @@ require "sidekiq/web"
 
 Rails.application.routes.draw do
   devise_for :users,
-             controllers: {
-               sessions:           "users/sessions",
-               omniauth_callbacks: "users/omniauth_callbacks"
-             }
+    controllers: {
+      sessions: "users/sessions",
+      omniauth_callbacks: "users/omniauth_callbacks"
+    }
 
   devise_scope :user do
     delete "sign_out", to: "devise/sessions#destroy", as: :destroy_user_session
