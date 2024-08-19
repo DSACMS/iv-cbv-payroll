97258d22221c287a56f60735326c9444b0a71ced
George Byers <github@georgebyers.me>
2024-08-19 17:19:16 -0400

    linter spacing changes

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -3,7 +3,7 @@ require "sidekiq/web"
 Rails.application.routes.draw do
   devise_for :users,
     controllers: {
-      sessions: "users/sessions",
+      sessions:           "users/sessions",
       omniauth_callbacks: "users/omniauth_callbacks"
     }
 
