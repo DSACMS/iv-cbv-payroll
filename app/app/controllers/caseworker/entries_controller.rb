04c3affa3737d5c9a6113f1df4190e1931a92282
George Byers <github@georgebyers.me>
2024-08-19 15:15:11 -0400

    removed unused method

--- app/app/controllers/caseworker/entries_controller.rb
+++ app/app/controllers/caseworker/entries_controller.rb
@@ -8,8 +8,4 @@ class Caseworker::EntriesController < ApplicationController
   def current_site
     site_config[params[:site_id]]
   end
-
-  def agency_short_name
-    current_site.agency_short_name
-  end
 end
