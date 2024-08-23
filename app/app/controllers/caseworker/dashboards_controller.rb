64d6c52f2c51df739532040c6bc49c56ea4277d7
Tom Dooner <tomdooner@navapbc.com>
2024-08-23 13:10:35 -0700

    Prevent URL-hacking to send invitations for other sites
    This was convenient for testing purposes, but alas, its days are
    numbered because we don't want the pen testers to find this.

--- app/app/controllers/caseworker/dashboards_controller.rb
+++ app/app/controllers/caseworker/dashboards_controller.rb
@@ -1,4 +1,4 @@
-class Caseworker::DashboardsController < ApplicationController
+class Caseworker::DashboardsController < Caseworker::BaseController
   before_action :authenticate_user!
 
   def show
