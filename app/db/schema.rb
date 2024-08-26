8606921f9c006807c7a4a4909433686d56693899
George Byers <github@georgebyers.me>
2024-08-26 15:29:17 -0400

    changed array syntax

--- app/db/schema.rb
+++ app/db/schema.rb
@@ -61,7 +61,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_26_161050) do
     t.uuid "account_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id", "account_id"], name: "index_connected_argyle_accounts_on_user_id_and_account_id", unique: true
+    t.index %w[user_id account_id], name: "index_connected_argyle_accounts_on_user_id_and_account_id", unique: true
   end
 
   create_table "pinwheel_accounts", force: :cascade do |t|
@@ -95,7 +95,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_26_161050) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email", "site_id"], name: "index_users_on_email_and_site_id", unique: true
+    t.index %w[email site_id], name: "index_users_on_email_and_site_id", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
