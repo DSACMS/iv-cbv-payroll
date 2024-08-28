d3ce6d65b3ebaa99b3e5c3b084a32c81100bca7d
Matt Gardner <wilburnforce@gmail.com>
2024-08-28 19:43:27 -0400

    Update app/app/controllers/cbv/synchronizations_controller.rb
    Co-authored-by: Tom Dooner <tomdooner@navapbc.com>

--- app/app/controllers/cbv/synchronizations_controller.rb
+++ app/app/controllers/cbv/synchronizations_controller.rb
@@ -2,7 +2,7 @@ class Cbv::SynchronizationsController < Cbv::BaseController
   def show
     account_id = params[:user][:account_id]
 
-    @pinwheel_account = PinwheelAccount.find_by_pinwheel_account_id(account_id)
+    @pinwheel_account = @cbv_flow.pinwheel_accounts.find_by(pinwheel_account_id: account_id)
 
     if @pinwheel_account && @pinwheel_account.has_fully_synced?
       redirect_to cbv_flow_payment_details_path(user: { account_id: @pinwheel_account.pinwheel_account_id })
