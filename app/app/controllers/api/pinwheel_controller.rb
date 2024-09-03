593bafbe76d0435b6478be0de75d7a6528600d7b
Tom Dooner <tomdooner@navapbc.com>
2024-09-03 12:02:39 -0700

    Track 3 new NewRelic events (#249)

--- app/app/controllers/api/pinwheel_controller.rb
+++ app/app/controllers/api/pinwheel_controller.rb
@@ -1,12 +1,14 @@
 class Api::PinwheelController < ApplicationController
+  after_action :track_event, only: :create_token
+
   # run the token here with the included employer/payroll provider id
   def create_token
-    cbv_flow = CbvFlow.find(session[:cbv_flow_id])
-    pinwheel = pinwheel_for(cbv_flow)
+    @cbv_flow = CbvFlow.find(session[:cbv_flow_id])
+    pinwheel = pinwheel_for(@cbv_flow)
     token_response = pinwheel.create_link_token(
       response_type: token_params[:response_type],
       id: token_params[:id],
-      end_user_id: cbv_flow.pinwheel_end_user_id
+      end_user_id: @cbv_flow.pinwheel_end_user_id
     )
     token = token_response["data"]["token"]
 
@@ -17,5 +19,14 @@ class Api::PinwheelController < ApplicationController
 
   def token_params
     params.require(:pinwheel).permit(:response_type, :id)
+  end
+
+  def track_event
+    NewRelicEventTracker.track("ApplicantBeganLinkingEmployer", {
+      cbv_flow_id: @cbv_flow.id,
+      response_type: token_params[:response_type]
+    })
+  rescue => ex
+    Rails.logger.error "Unable to track NewRelic event (ApplicantBeganLinkingEmployer): #{ex}"
   end
 end
