6094a6bb7679f6e840940dc5c6c4a812b058a58d
allthesignals <wilburnforce@gmail.com>
2024-10-22 18:08:51 -0400

    Refactor sync spec

--- app/spec/controllers/cbv/synchronizations_spec.rb
+++ app/spec/controllers/cbv/synchronizations_spec.rb
@@ -5,14 +5,14 @@ RSpec.describe Cbv::SynchronizationsController do
 
   let(:cbv_flow) { create(:cbv_flow) }
 
+  let(:pinwheel_account) { create(:pinwheel_account, cbv_flow: cbv_flow) }
+
   before do
     session[:cbv_flow_id] = cbv_flow.id
   end
 
   describe "#update" do
     it "redirects to the payment details page" do
-      pinwheel_account = create(:pinwheel_account, cbv_flow: cbv_flow)
-
       patch :update, params: { user: { account_id: pinwheel_account.pinwheel_account_id } }
 
       expect(response).to redirect_to(cbv_flow_payment_details_path(user: { account_id: pinwheel_account.pinwheel_account_id }))
