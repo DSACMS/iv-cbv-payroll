ff356ad51b4c5e7c34a2ae7d682e4fdb27b87c3b
allthesignals <wilburnforce@gmail.com>
2024-10-22 14:26:58 -0400

    More spec

--- app/spec/controllers/cbv/synchronization_failures_spec.rb
+++ app/spec/controllers/cbv/synchronization_failures_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe Cbv::MissingResultsController do
+RSpec.describe Cbv::SynchronizationFailuresController do
   describe "#show" do
     render_views
 
@@ -10,17 +10,21 @@ RSpec.describe Cbv::MissingResultsController do
       session[:cbv_flow_id] = cbv_flow.id
     end
 
-    it "renders successfully" do
-      get :show
-      expect(response).to be_successful
-    end
-
     context "when the user has already linked a pinwheel account" do
       let!(:pinwheel_account) { create(:pinwheel_account, cbv_flow: cbv_flow) }
 
-      it "renders successfully" do
+      it "shows continue to report button" do
         get :show
-        expect(response).to be_successful
+        expect(response.body).to include I18n.t("cbv.synchronization_failures.show.continue_to_report")
+      end
+    end
+
+    context "when the user has no successful pinwheel_accounts" do
+      let!(:pinwheel_account) { create(:pinwheel_account, :with_paystubs_errored, cbv_flow: cbv_flow) }
+
+      it "shows cta button" do
+        get :show
+        expect(response.body).to include I18n.t("cbv.expired_invitations.show.cta_button_html.sandbox")
       end
     end
   end
