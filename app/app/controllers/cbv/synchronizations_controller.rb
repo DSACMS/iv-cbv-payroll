3881fff0a4fb12719ac489e2151dfc0d8382dc94
allthesignals <wilburnforce@gmail.com>
2024-10-22 18:28:06 -0400

    Used before _action

--- app/app/controllers/cbv/synchronizations_controller.rb
+++ app/app/controllers/cbv/synchronizations_controller.rb
@@ -1,17 +1,16 @@
 class Cbv::SynchronizationsController < Cbv::BaseController
   before_action :set_pinwheel_account, only: %i[show update]
+  before_action :redirect_if_sync_finished, only: %i[show update]
 
   def show
-    handle_redirect
   end
 
   def update
-    handle_redirect
   end
 
   private
 
-  def handle_redirect
+  def redirect_if_sync_finished
     if @pinwheel_account && @pinwheel_account.has_fully_synced?
       if @pinwheel_account.has_required_data?
         redirect_to cbv_flow_payment_details_path(user: { account_id: @pinwheel_account.pinwheel_account_id })
