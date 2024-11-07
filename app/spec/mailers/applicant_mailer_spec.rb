9a96156819ff72a837a898182fa0dc3d32a170d6
Tim Miller <timmiller@navapbc.com>
2024-11-07 09:10:55 -0600

    minor string changes and email spacing updates, starting work on spec

--- app/spec/mailers/applicant_mailer_spec.rb
+++ app/spec/mailers/applicant_mailer_spec.rb
@@ -1,5 +1,6 @@
 require "rails_helper"
 require "active_support/testing/time_helpers"
+require "rake"
 
 RSpec.describe ApplicantMailer, type: :mailer do
   include ActiveSupport::Testing::TimeHelpers
@@ -68,4 +69,139 @@ RSpec.describe ApplicantMailer, type: :mailer do
       expect(unescaped_body).to match(I18n.t("applicant_mailer.invitation_email.body_3.ma", app_name: "DTA Connect"))
     end
   end
+end
+
+RSpec.describe ApplicantMailer, type: :mailer do
+  include ActiveSupport::Testing::TimeHelpers
+  include ActiveJob::TestHelper
+
+  let(:now) { DateTime.new(2024, 9, 9, 9, 0, 0, "-04:00") }
+  let(:site_id) { "nyc" }
+  let(:invitation_sent_at) { now - 5.days }
+  let(:snap_app_date) { now.strftime("%Y-%m-%d") }
+  let(:cbv_flow_invitation) do
+    create(:cbv_flow_invitation,
+           site_id.to_sym,
+           created_at: invitation_sent_at,
+           snap_application_date: invitation_sent_at - 1.day,
+          )
+  end
+  let(:cbv_flow) do
+    create(
+      :cbv_flow, :with_pinwheel_account,
+      case_number: cbv_flow_invitation.case_number,
+      confirmation_code: "00001",
+      created_at: invitation_sent_at + 15.minutes,
+      site_id: site_id,
+      transmitted_at: invitation_sent_at + 30.minutes,
+      cbv_flow_invitation_id: cbv_flow_invitation.id,
+      consented_to_authorized_use_at: invitation_sent_at + 30.minutes
+    )
+  end
+
+  Rails.application.load_tasks
+  Rake::Task['invitation_reminders:send_all'].invoke
+
+  # let(:previous_week_start_date) { now.beginning_of_week - 7.days }
+  # let(:week_start_date) { now.beginning_of_week }
+  # let(:parsed_csv) do
+  #   CSV.parse(mail.attachments.first.body.encoded, headers: :first_row).map(&:to_h)
+  # end
+
+
+  before do
+    travel_to(now)
+  end
+  email = ActionMailer::Base.deliveries.last
+  it "renders the subject" do
+    expect(email.subject).to eq("Reminder: Verify your income to renew or apply for your SNAP benefits")
+  end
+
+  # it "renders the body" do
+  #   expect(mail.body.encoded).to include("Attached is a report from")
+  # end
+
+  # it "tracks newrelic" do
+  #   expect(NewRelicEventTracker).to receive(:track)
+  #     .with("EmailSent", hash_including(
+  #       mailer: "WeeklyReportMailer",
+  #       action: "report_email",
+  #       message_id: be_a(String)
+  #     ))
+
+  #   mail.deliver_now
+  # end
+
+  # it "renders the csv data from the week before the report_date" do
+  #   expect(mail.attachments.first.filename).to eq("weekly_report_20240902-20240908.csv")
+  #   expect(mail.attachments.first.content_type).to start_with('text/csv')
+
+  #   expect(parsed_csv[0]).to match(
+  #     "client_id_number" => cbv_flow_invitation.client_id_number,
+  #     "transmitted_at" => "2024-09-04 13:30:00 UTC",
+  #     "case_number" => cbv_flow.case_number,
+  #     "invited_at" => "2024-09-04 13:00:00 UTC",
+  #     "snap_application_date" => "2024-09-03",
+  #     "completed_at" => "2024-09-04 13:30:00 UTC",
+  #   )
+  #   expect(parsed_csv.length).to eq(1)
+  # end
+
+  # context "when the invitation was sent before the week of the report" do
+  #   let(:invitation_sent_at) { now.prev_week.beginning_of_week - 1.minute }
+
+  #   it "excludes the record from the CSV" do
+  #     expect(parsed_csv.length).to eq(0)
+  #     expect(parsed_csv).not_to include(hash_including(
+  #       "client_id_number" => cbv_flow_invitation.client_id_number
+  #     ))
+  #   end
+  # end
+
+  # context "when there are is an incomplete CbvFlow" do
+  #   let!(:incomplete_invitation) do
+  #     create(:cbv_flow_invitation,
+  #            :nyc,
+  #            created_at: invitation_sent_at,
+  #           )
+  #   end
+  #   let!(:incomplete_flow) do
+  #     create(:cbv_flow, :with_pinwheel_account,
+  #            created_at: invitation_sent_at,
+  #            site_id: site_id,
+  #            cbv_flow_invitation: incomplete_invitation
+  #           )
+  #   end
+
+  #   it "includes them in the CSV data" do
+  #     expect(parsed_csv.length).to eq(2)
+  #     expect(parsed_csv).to include(hash_including(
+  #       "client_id_number" => incomplete_invitation.client_id_number,
+  #       "transmitted_at" => nil,
+  #       "case_number" => incomplete_invitation.case_number,
+  #       "invited_at" => "2024-09-04 13:00:00 UTC",
+  #       "snap_application_date" => match(/\d\d\d\d-\d\d-\d\d/),
+  #       "completed_at" => nil,
+  #     ))
+  #   end
+  # end
+
+  # context "for the MA site" do
+  #   let(:site_id) { "ma" }
+
+  #   it "renders the CSV data with MA-specific columns" do
+  #     expect(mail.attachments.first.filename).to eq("weekly_report_20240902-20240908.csv")
+  #     expect(mail.attachments.first.content_type).to start_with('text/csv')
+
+  #     expect(parsed_csv[0]).to match(
+  #       "beacon_id" => cbv_flow_invitation.beacon_id,
+  #       "transmitted_at" => "2024-09-04 13:30:00 UTC",
+  #       "agency_id_number" => cbv_flow_invitation.agency_id_number,
+  #       "invited_at" => "2024-09-04 13:00:00 UTC",
+  #       "snap_application_date" => "2024-09-03",
+  #       "completed_at" => "2024-09-04 13:30:00 UTC",
+  #     )
+  #     expect(parsed_csv.length).to eq(1)
+  #   end
+  # end
 end
