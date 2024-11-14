e88b779d1921442c1b009285f7ca67ec8ef37bfd
George Byers <github@georgebyers.me>
2024-11-14 13:58:04 -0500

    added events for tracking when a user selects a popular payroll provider or app-based employer

--- app/app/controllers/cbv/employer_searches_controller.rb
+++ app/app/controllers/cbv/employer_searches_controller.rb
@@ -9,6 +9,13 @@ class Cbv::EmployerSearchesController < Cbv::BaseController
     @employers = @query.blank? ? [] : fetch_employers(@query)
     @has_pinwheel_account = @cbv_flow.pinwheel_accounts.any?
     @type = search_params[:type]
+
+    case @type
+    when "payroll"
+      track_applicant_clicked_popular_payroll_provider_event
+    when "app"
+      track_applicant_clicked_popular_app_employer_event
+    end
   end
 
   private
@@ -24,6 +31,26 @@ class Cbv::EmployerSearchesController < Cbv::BaseController
     }
 
     pinwheel.fetch_items(request_params)["data"]
+  end
+
+  def track_applicant_clicked_popular_payroll_provider_event
+    NewRelicEventTracker.track("ApplicantClickedPopularPayrollProvider", {
+      timestamp: Time.now.to_i,
+      cbv_flow_id: @cbv_flow.id,
+      invitation_id: @cbv_flow.cbv_flow_invitation_id,
+    })
+  rescue => ex
+    Rails.logger.error "Unable to track NewRelic event (ApplicantClickedPopularPayrollProvider): #{ex}"
+  end
+
+  def track_applicant_clicked_popular_app_employer_event
+    NewRelicEventTracker.track("ApplicantClickedPopularAppEmployer", {
+      timestamp: Time.now.to_i,
+      cbv_flow_id: @cbv_flow.id,
+      invitation_id: @cbv_flow.cbv_flow_invitation_id,
+    })
+  rescue => ex
+    Rails.logger.error "Unable to track NewRelic event (ApplicantClickedPopularAppEmployer): #{ex}"
   end
 
   def track_accessed_search_event
