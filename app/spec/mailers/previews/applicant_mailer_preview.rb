f68f7beaa976b8a147cc3d454d10b7a13a7bd470
Tim Miller <timmiller@navapbc.com>
2024-11-21 07:36:01 -0600

    FFS-2086: Invitation reminder mailer fixes (#362)
    Previously, the "Share your feedback" button's border wasn't visible when rendered in gmail. Now, it will be.
    Previously, the headers in the reminders were all h3, which is not proper structure. Now, they're all h1, and there is no visible change.

--- app/spec/mailers/previews/applicant_mailer_preview.rb
+++ app/spec/mailers/previews/applicant_mailer_preview.rb
@@ -14,6 +14,18 @@ class ApplicantMailerPreview < BaseMailerPreview
     ).invitation_email
   end
 
+  def invitation_reminder_email_dta
+    ApplicantMailer.with(
+      cbv_flow_invitation: FactoryBot.create(:cbv_flow_invitation, :ma, user: unique_user, language: I18n.locale)
+    ).invitation_reminder_email
+  end
+
+  def invitation_reminder_email_nyc
+    ApplicantMailer.with(
+      cbv_flow_invitation: FactoryBot.create(:cbv_flow_invitation, :nyc, user: unique_user, language: I18n.locale)
+    ).invitation_reminder_email
+  end
+
   private
   def unique_user
     FactoryBot.create(:user, email: "#{SecureRandom.uuid}@example.com")
