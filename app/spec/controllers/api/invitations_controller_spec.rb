ae397a69c38ceb389f14d2649b6c44238b80fe42
allthesignals <wilburnforce@gmail.com>
2025-01-29 14:21:02 -0500

    Add validation handling

--- app/spec/controllers/api/invitations_controller_spec.rb
+++ app/spec/controllers/api/invitations_controller_spec.rb
@@ -15,5 +15,22 @@ RSpec.describe Api::InvitationsController do
       expect(response).to have_http_status(:created)
       expect(JSON.parse(response.body).keys).to include("url")
     end
+
+    context "invalid params" do
+      let(:invalid_params) do
+        valid_params.except(:first_name)
+      end
+
+      it "returns unprocessable entity" do
+        post :create, params: {
+          site_id: :sandbox,
+          cbv_flow_invitation: invalid_params
+        }
+        pp response.body
+
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(JSON.parse(response.body).keys).to include("first_name")
+      end
+    end
   end
 end
