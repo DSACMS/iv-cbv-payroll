8e7c8e63bd492fa06113306cfc66a7f9f318d5e8
Jeff Catania <jeffrey.m.catania@omb.eop.gov>
2025-01-30 11:08:15 -0500

    Remove applicants table

--- app/db/schema.rb
+++ app/db/schema.rb
@@ -10,15 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_01_30_154521) do
+ActiveRecord::Schema[7.1].define(version: 2025_01_30_160642) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
-
-  create_table "applicants", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "cbv_clients", force: :cascade do |t|
     t.string "case_number"
