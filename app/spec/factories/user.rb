0652187ba5eac323c9b52f872a3b7cd44dba9709
allthesignals <wilburnforce@gmail.com>
2025-01-31 15:11:32 -0500

    Add some User spec tests

--- app/spec/factories/user.rb
+++ app/spec/factories/user.rb
@@ -2,6 +2,5 @@ FactoryBot.define do
   factory :user do
     site_id { "sandbox" }
     sequence(:email) { |n| "user#{n}@example.com" }
-    # association :api_access_token, factory: [ :api_access_token ]
   end
 end
--- app/spec/models/user_spec.rb
+++ app/spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "creates a user with an access token" do
+    user = create(:user)
+    expect(user.create_api_access_token).to be_present
+  end
+
+  it "finds a user by access token" do
+    user = create(:user)
+    token = user.create_api_access_token
+    found_user = User.find_by_access_token(token)
+    expect(found_user.id).to eq(user.id)
+  end
+
+  it "rejects an access token if it's marked as deleted" do
+    user = create(:user)
+    token = user.create_api_access_token
+    user.api_access_tokens.first.update(deleted_at: Time.now)
+    expect(User.find_by_access_token(token)).to be_nil
+  end
 end
