7fc8acdfce1f255f8b426a14c7d6e6a7e2f7ff27
George Byers <github@georgebyers.me>
2025-02-04 14:44:34 -0500

    FFS-2295: updated help_controller to infer site_config and current_site

--- app/app/controllers/help_controller.rb
+++ app/app/controllers/help_controller.rb
@@ -10,12 +10,14 @@ class HelpController < ApplicationController
     @help_topic = params[:topic].gsub("-", "_")
     @title = t("help.show.#{@help_topic}.title")
 
+    cbv_flow = session[:cbv_flow_id] ? CbvFlow.find_by(id: session[:cbv_flow_id]) : nil
+
     event_logger.track("ApplicantViewedHelpTopic", request, {
       topic: @help_topic,
-      cbv_flow_id: @cbv_flow&.id,
-      invitation_id: @cbv_flow&.cbv_flow_invitation_id,
-      site_id: params[:site_id],
-      flow_started_seconds_ago: @cbv_flow ? (Time.now - @cbv_flow.created_at).to_i : nil,
+      cbv_flow_id: session[:cbv_flow_id],
+      invitation_id: cbv_flow&.cbv_flow_invitation_id,
+      site_id: current_site&.id,
+      flow_started_seconds_ago: cbv_flow ? (Time.now - cbv_flow.created_at).to_i : nil,
       language: I18n.locale
     }.compact)
   rescue => ex
@@ -25,7 +27,13 @@ class HelpController < ApplicationController
   private
 
   def current_site
-    @current_site ||= site_config[params[:site_id]]
+    @current_site ||= begin
+      if session[:cbv_flow_id]
+        if (cbv_flow = CbvFlow.find_by(id: session[:cbv_flow_id]))
+          site_config[cbv_flow.site_id]
+        end
+      end || site_config[params[:site_id]]
+    end
   end
 
   def site_config
