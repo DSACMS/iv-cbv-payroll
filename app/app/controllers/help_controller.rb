94f88b57f778988d6f7b325338a6df19e20a91d3
George Byers <github@georgebyers.me>
2025-02-04 13:07:18 -0500

    FFS-2295: restoring much of the csp settings and removing skip

--- app/app/controllers/help_controller.rb
+++ app/app/controllers/help_controller.rb
@@ -1,7 +1,5 @@
 class HelpController < Cbv::BaseController
   layout "help"
-  skip_before_action :set_cbv_flow, :ensure_cbv_flow_not_yet_complete, :prevent_back_after_complete, :capture_page_view
-
   def index
     @title = t("help.index.title")
   end
