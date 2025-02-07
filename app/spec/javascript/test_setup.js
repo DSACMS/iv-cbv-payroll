c568183958f6269a978008145241c32e547235c1
Jeff Catania <Jeffrey.M.Catania@omb.eop.gov>
2025-02-07 11:49:17 -0500

    added Node to enviornment. Explicitly included JSDom into test_setup

--- app/spec/javascript/test_setup.js
+++ app/spec/javascript/test_setup.js
@@ -1,5 +1,12 @@
 import { vi } from 'vitest';
 import { Application } from '@hotwired/stimulus';
+import { JSDOM } from 'jsdom';
+
+const { window } = new JSDOM();
+
+global.window = window;
+global.document = window.document;
+global.Node = window.Node;
 
 // Set up Stimulus
 window.Stimulus = Application.start();
