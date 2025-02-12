b2cce21aceab52580dd8bea8291c3122ba543323
Jeff Catania <Jeffrey.M.Catania@omb.eop.gov>
2025-02-11 19:02:10 -0500

    this isnt' needed yet

--- app/app/javascript/utilities/api.js
+++ app/app/javascript/utilities/api.js
@@ -2,7 +2,6 @@ import { fetchInternal } from './fetchInternal';
 
 export const PINWHEEL_USER_ACTION = '/api/pinwheel/user_action';
 export const PINWHEEL_TOKENS_GENERATE = '/api/pinwheel/tokens';
-export const SYNCHRONIZATIONS_FORM_ENDPOINT = '/en/cbv/synchronizations';
 
 export const trackUserAction = (eventName, attributes, scope="pinwheel") => {
   return fetchInternal(PINWHEEL_USER_ACTION, {
@@ -17,14 +16,3 @@ export const fetchToken = (response_type, id, locale) => {
     body: JSON.stringify({ response_type, id, locale }),
   })
 };
-
-export const synchronizeCbvData = (userId) => {
-  const formData = new FormData().append('user[account_id]', userId)
-
-  return fetchInternal(SYNCHRONIZATIONS_FORM_ENDPOINT, {
-    method: 'post',
-    body: formData,
-  })
-};
-
-
