a5bf1864ec179439b1fb5e335b31e2840e6d0afa
Matt Gardner <wilburnforce@gmail.com>
2025-02-12 15:33:27 -0500

    FFS-2336: First sketch of multi-provider search (#442)
    * First sketch of multi-provider search
    
    * Refactor Provider Search Service
    
    * Keyed by symbol
    
    * Basic test
    
    * Extract provider result into response objects
    
    * Add terraform configuration
    
    * Resolve conflicts
    
    * Lint infra

---
+++ app/app/services/provider_search_service.rb
@@ -0,0 +1,28 @@
+class ProviderSearchService
+  SUPPORTED_PROVIDERS = (ENV["SUPPORTED_PROVIDERS"] || "pinwheel")&.split(",")&.map(&:to_sym)
+
+  def initialize(client_agency_id)
+    @client_agency_config = site_config[client_agency_id]
+  end
+
+  def search(query = "")
+    SUPPORTED_PROVIDERS.map do |provider|
+      case provider
+      when :pinwheel
+        PinwheelService.new(@client_agency_config.pinwheel_environment).fetch_items(q: query)["data"].map do |result|
+          ResponseObjects::SearchResult.from_pinwheel(result)
+        end
+      when :argyle
+        ArgyleService.new(@client_agency_config.argyle_environment).items(query)["results"].map do |result|
+          ResponseObjects::SearchResult.from_argyle(result)
+        end
+      end
+    end.flatten
+  end
+
+  private
+
+  def site_config
+    Rails.application.config.client_agencies
+  end
+end
