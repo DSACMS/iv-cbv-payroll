2bc5f29f1166c97bf6a4454940a3fc1039c95c9c
Jeff Catania <Jeffrey.M.Catania@omb.eop.gov>
2025-02-19 17:13:18 -0500

    drops "should" from tests"

--- app/spec/javascript/adapters/PinwheelIncomeDataAdapter.spec.js
+++ app/spec/javascript/adapters/PinwheelIncomeDataAdapter.spec.js
@@ -34,71 +34,71 @@ describe('PinwheelModalAdapter', () => {
     })
 
     describe('open', () => {
-        it('should call track user action', async () => {
+        it('calls track user action', async () => {
             expect(trackUserAction).toHaveBeenCalled()
             expect(trackUserAction.mock.calls[0][0]).toBe('ApplicantSelectedEmployerOrPlatformItem')
             expect(trackUserAction.mock.calls[0]).toMatchSnapshot()
         })
-        it('should fetch token successfully', async () => {
+        it('fetches token successfully', async () => {
             expect(fetchToken).toHaveBeenCalledTimes(1)
             expect(fetchToken).toHaveBeenCalledWith("response-type", "id", "en")
             expect(fetchToken).toHaveResolvedWith({ token: 'test-token' })
         })
-        it('should open Pinwheel modal', async () => {
+        it('opens Pinwheel modal', async () => {
             expect(Pinwheel.open).toHaveBeenCalledTimes(1)
         })
     })
 
     describe('onSuccess', () => {
-        it('should call track user action', async() => {
+        it('calls track user action', async() => {
             await triggers.triggerSuccessEvent()
             expect(trackUserAction).toHaveBeenCalledTimes(2)
             expect(trackUserAction.mock.calls[1][0]).toBe('PinwheelSuccess')
         })
-        it('should trigger the provided onSuccess callback', async () => {
+        it('triggers the provided onSuccess callback', async () => {
             await triggers.triggerSuccessEvent()
             expect(pinwheelModalAdapterArgs.onSuccess).toHaveBeenCalled()
         })
     })
     describe('onExit', () => {
-        it('should trigger the provided onExit callback', async () => {
+        it('triggers the provided onExit callback', async () => {
             await triggers.triggerSuccessEvent()
             expect(pinwheelModalAdapterArgs.onSuccess).toHaveBeenCalled()
         })
     })
     describe('onEvent', () => {
-        it('should log screen_transition.LOGIN Event', async () => {
+        it('logs screen_transition.LOGIN Event', async () => {
             await triggers.triggerEvent("screen_transition", { "screenName": "LOGIN", selectedEmployerName: "ACME Inc", selectedPlatformName: "ADP" })
             expect(trackUserAction).toHaveBeenCalledTimes(2)
             expect(trackUserAction.mock.calls[1][0]).toBe('PinwheelShowLoginPage')
         })
-        it('should log screen_transition.PROVIDER_CONFIRMATION event', async () => {
+        it('logs screen_transition.PROVIDER_CONFIRMATION event', async () => {
             await triggers.triggerEvent("screen_transition", { "screenName": "PROVIDER_CONFIRMATION"})
             expect(trackUserAction).toHaveBeenCalledTimes(2)
             expect(trackUserAction.mock.calls[1][0]).toBe('PinwheelShowProviderConfirmationPage')
         })
-        it('should log screen_transition.SEARCH_DEFAULT event', async () => {
+        it('logs screen_transition.SEARCH_DEFAULT event', async () => {
             await triggers.triggerEvent("screen_transition", { "screenName": "SEARCH_DEFAULT"})
             expect(trackUserAction).toHaveBeenCalledTimes(2)
             expect(trackUserAction.mock.calls[1][0]).toBe('PinwheelShowDefaultProviderSearch')
         })
-        it('should log screen_transition.EXIT_CONFIRMATION event', async () => {
+        it('logs screen_transition.EXIT_CONFIRMATION event', async () => {
             await triggers.triggerEvent("screen_transition", { "screenName": "EXIT_CONFIRMATION"})
             expect(trackUserAction).toHaveBeenCalledTimes(2)
             expect(trackUserAction.mock.calls[1][0]).toBe('PinwheelAttemptClose')
         })
-        it('should log login_attempt event', async () => {
+        it('logs login_attempt event', async () => {
             await triggers.triggerEvent("login_attempt")
             expect(trackUserAction).toHaveBeenCalledTimes(2)
             expect(trackUserAction.mock.calls[1][0]).toBe('PinwheelAttemptLogin')
         })
-        it('should log error event', async () => {
+        it('logs error event', async () => {
             await triggers.triggerEvent("error", { type: "error-type", "code": "code", "message":"default message"})
             expect(trackUserAction).toHaveBeenCalledTimes(2)
             expect(trackUserAction.mock.calls[1][0]).toBe('PinwheelError')
             expect(trackUserAction.mock.calls[1][1]['type']).toBe('error-type')
         })
-        it('should log exit event', async () => {
+        it('logs exit event', async () => {
             await triggers.triggerEvent("exit")
             expect(trackUserAction).toHaveBeenCalledTimes(2)
             expect(trackUserAction.mock.calls[1][0]).toBe('PinwheelCloseModal')
