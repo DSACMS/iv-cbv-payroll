cf05ba0e04dd796cb93f92bb31b13b3874fb527e
Tim Miller <timmiller@navapbc.com>
2025-02-19 14:48:14 -0600

    removed new unnecessary whitespace

--- app/app/controllers/cbv/base_controller.rb
+++ app/app/controllers/cbv/base_controller.rb
@@ -24,7 +24,6 @@ class Cbv::BaseController < ApplicationController
 
       @cbv_flow = CbvFlow.create_from_invitation(invitation)
       session[:cbv_flow_id] = @cbv_flow.id
-
       track_invitation_clicked_event(invitation, @cbv_flow)
 
     elsif session[:cbv_flow_id]
