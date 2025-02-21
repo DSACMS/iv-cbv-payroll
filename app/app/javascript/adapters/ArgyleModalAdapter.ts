fee06b81084a7bfc9274a846a59fda9005808c90
allthesignals <wilburnforce@gmail.com>
2025-02-21 13:36:56 -0500

    Fix spacing

--- app/app/javascript/adapters/ArgyleModalAdapter.ts
+++ app/app/javascript/adapters/ArgyleModalAdapter.ts
@@ -16,46 +16,49 @@ export default class ArgyleModalAdapter extends ModalAdapter {
   Argyle: Argyle;
 
   async load() {
-      this.Argyle = await new Promise((resolve, reject) => {
-          loadScript('https://plugin.argyle.com/argyle.web.v5.js', (err, script) => {
-              if (err) {
-                  reject(err);
-              } else {
-                  resolve(Argyle);
-              }
-          });
-      })
+    this.Argyle = await new Promise((resolve, reject) => {
+      loadScript('https://plugin.argyle.com/argyle.web.v5.js', (err, script) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(Argyle);
+        }
+      });
+    })
   }
   
   async open() {
-      const locale = getDocumentLocale();
-      if (this.requestData) {
-          await trackUserAction("ApplicantSelectedEmployerOrPlatformItem", {
-              item_type: this.requestData.responseType,
-              item_id: this.requestData.id,
-              item_name: this.requestData.name,
-              is_default_option: this.requestData.isDefaultOption,
-              provider_name: this.requestData.providerName,
-              locale
-          })
+    const locale = getDocumentLocale();
 
-          const { user } = await fetchToken();
-          return this.Argyle.create({
-            userToken: user.user_token,
-            items: [this.requestData.id],
-            onAccountConnected: this.onSuccess.bind(this),
-            sandbox: true, 
-          }).open();
-      }
+    if (this.requestData) {
+      await trackUserAction("ApplicantSelectedEmployerOrPlatformItem", {
+        item_type: this.requestData.responseType,
+        item_id: this.requestData.id,
+        item_name: this.requestData.name,
+        is_default_option: this.requestData.isDefaultOption,
+        provider_name: this.requestData.providerName,
+        locale
+      })
+
+      const { user } = await fetchToken();
+
+      return this.Argyle.create({
+        userToken: user.user_token,
+        items: [this.requestData.id],
+        onAccountConnected: this.onSuccess.bind(this),
+        sandbox: true, 
+      }).open();
+    }
   }
 
   async onSuccess(eventPayload: LinkResult) {
-      await trackUserAction("ArgyleSuccess", {
-          account_id: eventPayload.accountId,
-          platform_id: eventPayload.platformId
-      })
-      if (this.successCallback) {
-          this.successCallback(eventPayload.accountId);
-      }
+    await trackUserAction("ArgyleSuccess", {
+      account_id: eventPayload.accountId,
+      platform_id: eventPayload.platformId
+    });
+
+    if (this.successCallback) {
+      this.successCallback(eventPayload.accountId);
+    }
   }
 }
