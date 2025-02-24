0d8081eaf4cca4f197f7526e9a162f8b5ed0dbb9
George Byers <georgebyers@navapbc.com>
2025-02-24 14:33:01 -0500

    fixed spacing

--- app/app/javascript/controllers/help.js
+++ app/app/javascript/controllers/help.js
@@ -12,9 +12,8 @@ export default class extends Controller {
         });
       }
     };
-
     
-     // reset the help modal src on mousedown to ensure the help modal src is reset to "/help"
+    // reset the help modal src on mousedown to ensure the help modal src is reset to "/help"
     document
       .querySelector('[aria-controls="help-modal"]')
       .addEventListener("mousedown", () => {
