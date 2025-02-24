6d9d3cc59eb51ffd857721621cc17389c70b1fb5
George Byers <georgebyers@navapbc.com>
2025-02-24 16:05:15 -0500

    updated handleClick to be an instance method and moved the resetting of the turboframe into it

--- app/app/javascript/controllers/help.js
+++ app/app/javascript/controllers/help.js
@@ -4,16 +4,17 @@ import { trackUserAction } from "../utilities/api";
 export default class extends Controller {
   static targets = ["content"];
 
+  handleClick(event) {
+    if (event.target.href?.includes("#help-modal")) {
+      trackUserAction("ApplicantOpenedHelpModal", {
+        source: event.target.dataset.source,
+      });
+      // reset the help modal src on mousedown to ensure the help modal src is reset to "/help"
+      document.querySelector("#help_modal_content").src = "/help";
+    }
+  }
+
   connect() {
-    this.handleClick = (event) => {
-      if (event.target.href?.includes("#help-modal")) {
-        trackUserAction("ApplicantOpenedHelpModal", {
-          source: event.target.dataset.source,
-        });
-        // reset the help modal src on mousedown to ensure the help modal src is reset to "/help"
-        document.querySelector("#help_modal_content").src = "/help";
-      }
-    };
     document.addEventListener("click", this.handleClick);
   }
 
