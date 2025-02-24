378bb3604e6f7a4c1c06ba8d04ec41e1272c372b
George Byers <georgebyers@navapbc.com>
2025-02-24 14:27:29 -0500

    removed mututation observer in favor of a mousedown event handler

--- app/app/javascript/controllers/help.js
+++ app/app/javascript/controllers/help.js
@@ -1,41 +1,28 @@
-import { Controller } from "@hotwired/stimulus"
-import { trackUserAction } from "../utilities/api"
+import { Controller } from "@hotwired/stimulus";
+import { trackUserAction } from "../utilities/api";
 
 export default class extends Controller {
-  static targets = ["content"]
+  static targets = ["content"];
 
   connect() {
     this.handleClick = (event) => {
       if (event.target.href?.includes("#help-modal")) {
-        trackUserAction("ApplicantOpenedHelpModal", { source: event.target.dataset.source })
+        trackUserAction("ApplicantOpenedHelpModal", {
+          source: event.target.dataset.source,
+        });
       }
-    }
+    };
 
-    // Watch for modal visibility changes
-    this.observer = new MutationObserver((mutations) => {
-      mutations.forEach((mutation) => {        
-        if (mutation.target.classList.contains('is-hidden')) {
-          const frame = document.querySelector('#help_modal_content')
-          if (frame) {
-            frame.src = '/help'
-          }
-        }
-      })
-    })
+    document
+      .querySelector('[aria-controls="help-modal"]')
+      .addEventListener("mousedown", () => {
+        document.querySelector("#help_modal_content").src = "/help";
+      });
 
-    const modalElement = document.getElementById('help-modal')
-    this.observer.observe(modalElement, {
-      attributes: true,
-      attributeFilter: ['class']
-    })
-
-    document.addEventListener("click", this.handleClick)
+    document.addEventListener("click", this.handleClick);
   }
 
   disconnect() {
-    document.removeEventListener("click", this.handleClick)
-    if (this.observer) {
-      this.observer.disconnect()
-    }
+    document.removeEventListener("click", this.handleClick);
   }
 }
