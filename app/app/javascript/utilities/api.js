cb25f142416a722117cfc77dd5baa7d9a7d14952
Jeff Catania <Jeffrey.M.Catania@omb.eop.gov>
2025-02-25 20:04:22 -0500

    changed trackUserAction to use the fetchInternal method

--- app/app/javascript/utilities/api.js
+++ app/app/javascript/utilities/api.js
@@ -6,15 +6,10 @@ export const PINWHEEL_TOKENS_GENERATE = '/api/pinwheel/tokens';
 const ARGYLE_TOKENS_GENERATE = '/api/argyle/tokens';
 
 export const trackUserAction = async  (eventName, attributes) => {
-  console.log("this should never happen track user action")
-  return fetch(EVENTS_USER_ACTION, {
+  return fetchInternal(EVENTS_USER_ACTION, {
     method: 'post',
-    headers: {
-      'X-CSRF-Token': CSRF.token,
-      'Content-Type': 'application/json'
-    },
     body: JSON.stringify({ events: { event_name: eventName, attributes } }),
-  }).then(response => response.json());
+  })
 }
 
 
@@ -25,7 +20,8 @@ export const fetchToken = (response_type, id, locale) => {
   })
 };
 
-export const fetchArgyleToken = () => {
+
+const fetchArgyleToken = () => {
   return fetchInternal(ARGYLE_TOKENS_GENERATE, {
     method: 'post',
   })
