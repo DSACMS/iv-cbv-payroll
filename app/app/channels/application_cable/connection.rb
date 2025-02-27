7d99621bf9c8f1b95c0f05fcffa2b3b4ab181191
George Byers <georgebyers@navapbc.com>
2025-02-27 09:23:15 -0500

    cleaning some things up

--- app/app/channels/application_cable/connection.rb
+++ app/app/channels/application_cable/connection.rb
@@ -1,11 +1,5 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
-    identified_by :session_id
-
-    def connect
-      self.session_id = session.id
-      self.current_user = find_verified_user
-
     def session
       @request.session
     end
--- app/app/channels/session_channel.rb
+++ app/app/channels/session_channel.rb
@@ -3,8 +3,9 @@ class SessionChannel < ApplicationCable::Channel
   periodically :check_session_activity, every: Rails.env.development? ? 5.seconds : 60.seconds
 
   def subscribed
-    @cbv_flow_id = connection.session[:cbv_flow_id]
-    stream_for current_channel_identifier
+    return unless connection.session[:cbv_flow_id]
+    @cbv_flow = CbvFlow.find(connection.session[:cbv_flow_id])
+    stream_for @cbv_flow
 
     # Record connection time
     connection.instance_variable_set(:@session_channel_connected_at, Time.current)
@@ -17,44 +18,26 @@ class SessionChannel < ApplicationCable::Channel
   end
 
   def received(data)
-    # This method is called for general messages received from the client
-    # that don't match a specific method name in perform('method_name').
-    # ActionCable automatically routes messages to methods named in 'perform'.
-    # Example: Client calls subscription.perform('extend_session') will directly 
-    # call the extend_session method, bypassing this received method.
-    
     Rails.logger.debug "Data received on SessionChannel: #{data}"
-
-    # Update last activity time
     update_last_activity_time
   end
 
-  def extend_session(data)    
+  def extend_session(data)
     update_last_activity_time
-
-    if @cbv_flow_id
-      connection.session[:last_activity_at] = Time.current.to_i
-      Rails.logger.debug "Updated session timestamp for CBV flow #{@cbv_flow_id}"
-    end
-    
     # Clear any warning flags since the session has been extended
     connection.instance_variable_set(:@warning_sent_at, nil)
 
-    Rails.logger.info "Transmitting session.extended event"
     transmit({
       event: "session.extended",
       message: "Session extended successfully",
       extended_at: Time.current.iso8601
     })
 
-    # Also update session info with new timestamps
     transmit_session_info
-    
-    Rails.logger.info "Session successfully extended for #{current_channel_identifier}"
+    Rails.logger.info "Session successfully extended for #{@cbv_flow.id}"
   end
 
   def transmit_session_info
-    # Get the Devise timeout setting
     timeout_in = get_timeout_setting
 
     # Calculate time remaining
@@ -65,14 +48,15 @@ class SessionChannel < ApplicationCable::Channel
       event: "session.info",
       timeout_in_ms: timeout_in * 1000,
       time_remaining_ms: time_until_timeout * 1000,
-      last_activity: last_active.iso8601
+      last_activity: last_active.iso8601,
+      cbv_flow_id: @cbv_flow.id
     })
   end
 
   private
 
   def current_channel_identifier
-    current_user&.id || @cbv_flow_id || connection.session.id
+    @cbv_flow.id
   end
 
   def get_timeout_setting
@@ -80,17 +64,14 @@ class SessionChannel < ApplicationCable::Channel
     Rails.env.development? ? 2.minutes : 30.minutes
   end
 
-  # Update the last activity timestamp
   def update_last_activity_time
     current_time = Time.current
     connection.instance_variable_set(:@session_channel_last_activity, current_time)
-    
     # Also update the session timestamp
     if connection.session
       connection.session[:last_activity_at] = current_time.to_i
     end
-    
-    Rails.logger.debug "Updated activity time for #{current_channel_identifier} to #{current_time}"
+    Rails.logger.debug "Updated activity time for #{@cbv_flow.id} to #{current_time}"
   end
 
   def get_last_activity_time
@@ -107,7 +88,7 @@ class SessionChannel < ApplicationCable::Channel
       session_activity: session_activity, 
       connection_time: connection_time
     }
-    Rails.logger.debug "Available timestamps for #{current_channel_identifier}: #{timestamps.inspect}"
+    Rails.logger.debug "Available timestamps for #{current_channel_identifier}: #{timestamps}"
 
     # Use the most recent timestamp from any source, fallback to current time
     [ websocket_activity, session_activity, connection_time, Time.current ].compact.max
@@ -115,7 +96,7 @@ class SessionChannel < ApplicationCable::Channel
 
   def check_session_activity
     Rails.logger.debug "SessionChannel#check_session_activity called at #{Time.current}"
-    return unless @cbv_flow_id
+    return unless @cbv_flow
 
     # Get the timeout setting
     session_timeout_limit = get_timeout_setting
@@ -134,17 +115,17 @@ class SessionChannel < ApplicationCable::Channel
     # Send warning when approaching timeout (5 minutes before)
     warning_threshold = 5.minutes.to_i * 1000
     warning_already_sent = connection.instance_variable_get(:@warning_sent_at)
-    
-    # Only send a warning if we're in the warning window AND 
+
+    # Only send a warning if we're in the warning window AND
     # (we haven't sent a warning yet OR the previous warning was sent more than 1 minute ago)
     if time_until_timeout_ms <= warning_threshold && time_until_timeout_ms > 0 &&
        (warning_already_sent.nil? || Time.current - warning_already_sent > 1.minute)
-      
+
       # Record that we sent a warning
       connection.instance_variable_set(:@warning_sent_at, Time.current)
-      
+
       Rails.logger.info "Sending session timeout warning to #{current_channel_identifier}, #{time_until_timeout_ms / 1000} seconds remaining"
-      
+
       broadcast_to(current_channel_identifier, {
         event: "session.warning",
         message: "Your session will expire soon",
@@ -161,7 +142,7 @@ class SessionChannel < ApplicationCable::Channel
         last_activity: last_active.iso8601,
         timeout_in: session_timeout_limit.to_i,
         current_time: Time.current.iso8601,
-        cbv_flow_id: @cbv_flow_id,
+        cbv_flow_id: @cbv_flow.id,
         warning_sent_at: connection.instance_variable_get(:@warning_sent_at)&.iso8601
       })
     end
