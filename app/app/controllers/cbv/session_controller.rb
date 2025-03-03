e17a8059671096adc72f8c6029ae97493fd68047
George Byers <georgebyers@navapbc.com>
2025-03-03 14:37:09 -0500

    Added controller to extend the session, updated _timeout_modal.html.erb to use turbo streams

--- app/app/controllers/cbv/session_controller.rb
+++ app/app/controllers/cbv/session_controller.rb
@@ -2,16 +2,8 @@ class Cbv::SessionController < ApplicationController
   def refresh
     session[:last_seen] = Time.current
     respond_to do |format|
-      format.turbo_stream { render turbo_stream: turbo_stream.update("session-status", "Session extended successfully!") }
+      format.turbo_stream { render turbo_stream: turbo_stream.update("session-timeout-description", "Session extended successfully!") }
+      format.html { redirect_to request.referrer || root_path }
     end
-  end
-
-  # should we use the resets_controller for this?
-  def destroy
-    session[:cbv_flow_id] = nil
-    if params[:user_action] == "false"
-      track_timeout_event
-    end
-    redirect_to root_url
   end
 end
--- app/app/javascript/controllers/cbv/session_controller.js
+++ app/app/javascript/controllers/cbv/session_controller.js
@@ -1,21 +1,25 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = [
-    "modal",
-  ];
+  static targets = ["modal"];
 
   timeoutValue;
 
   connect() {
     console.log("Session controller connected");
-
     this.timeoutValue = parseInt(this.modalTarget.dataset.itemTimeoutParam, 10);
-    
+    this.setupTimers();
+  }
+
+  setupTimers() {
+    // Clear existing timers if they exist
+    if (this.warningTimer) clearTimeout(this.warningTimer);
+    if (this.expirationTimer) clearTimeout(this.expirationTimer);
+
     // Calculate when to show the warning (5 minutes before expiration)
     const warningDelay = Math.max(this.timeoutValue - 5 * 60, 0) * 1000;
     const expirationDelay = this.timeoutValue * 1000;
-    
+
     // Set timer for the warning (5 minutes before expiration)
     this.warningTimer = setTimeout(() => {
       document.getElementById("open-session-modal-button").click();
@@ -23,8 +27,16 @@ export default class extends Controller {
 
     // Set timer for the actual session expiration
     this.expirationTimer = setTimeout(() => {
-      window.location = `/cbv/resets`;
+      window.location = `/cbv/reset?timeout=true`;
     }, expirationDelay);
+  }
+
+  /*
+   reset the timers when the session is extended
+   see _timeout_modal.html.erb for the button that triggers this
+  */
+  resetTimers() {
+    this.setupTimers();
   }
 
   disconnect() {
