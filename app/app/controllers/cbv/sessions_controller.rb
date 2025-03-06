941e94c3b666f9c0359b5dedbf54a24fa848d819
George Byers II <georgebyers@navapbc.com>
2025-03-06 11:32:09 -0500

    FFS-2377 Extend Session Modal (#469)
    * Session extension modal properly appears after 5 seconds (for development purposes), need to work on extending the actual session
    
    * removed redundant controller
    
    * added turbo click event to session extend button
    
    * using action cable and devise to determine session length
    
    * Using ActionCable to poll the backend via websockets to determine user activity and extend or cancel the session
    
    * cleaning some things up
    
    * hiding the session modal trigger link
    
    * lots of updates, Stimulus really disappointed me. just need to clear the session now, might have to implement a controller for that?
    
    * logging is a bit too verbose and noisy
    
    * explored a bunch of options with action cable and stimulus- concluding with this approach
    
    * adding modified files
    
    * removed logging of dataset
    
    * updating the expiration/redirection on the client side
    
    * removing the cookie name since we can simply refresh the page
    
    * inlining the location.reload for the extend session
    
    * addressing PR feedback
    
    * putting a pin in things for now
    
    * Added controller to extend the session, updated _timeout_modal.html.erb to use turbo streams
    
    * removing unnecessary code
    
    * linting
    
    * removed session delete route in favor of the existing reset controller
    
    * eliminated the resets_controller in favor of the session controller
    
    * renamed session to sessions
    
    * renamed files in accordance with Rails conventions
    
    * adding tests and simplifying the session controller
    
    * updating packages
    
    * restoring Gemfile.lock
    
    * updated session_controller_spec test
    
    * fixed failing tests and linted files
    
    * adding faker
    
    * renamed the cmd to be seed_cbv_user_flow, ensuring that other associated records are also cleaned up
    
    * adding a z-index toggle to dynamically change the z-index of the pinwheel modal if its visible
    
    * removed some console.log statements
    
    * bugfix that closes all visible uswds modals prior to the session timeout modal appearing
    
    * removed seed cbv user flow rake task
    
    * minor updates to ensure that the session expiry shows the session ended verbiage

---
+++ app/app/controllers/cbv/sessions_controller.rb
@@ -0,0 +1,18 @@
+class Cbv::SessionsController < Cbv::BaseController
+  skip_before_action :set_cbv_flow, :ensure_cbv_flow_not_yet_complete, :prevent_back_after_complete, :capture_page_view
+
+  def refresh
+    session[:last_seen] = Time.current
+    head :ok
+  end
+
+  def end
+    if params[:timeout] == "true"
+      track_timeout_event
+      flash[:notice] = t("cbv.error_missing_token_html")
+    end
+
+    session[:cbv_flow_id] = nil
+    redirect_to root_url
+  end
+end
