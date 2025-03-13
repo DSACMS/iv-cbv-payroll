32d718cd5116ea9b1d735d782ddfa9b9b168afbc
Daphne Gold <daphnegold@navapbc.com>
2025-03-13 15:55:05 -0700

    Add comment to explain USWDS reinitalization

--- app/app/javascript/application.js
+++ app/app/javascript/application.js
@@ -8,6 +8,9 @@ import "@uswds/uswds"
 import components from "@uswds/uswds/src/js/components"
 let initialLoad = true
 
+// Loops through USWDS components and reinitializes them on the page
+// Each behavior handles the JavaScript functionality for a UI component
+// See: https://github.com/uswds/uswds/blob/852076b6409e20ba95e3a589ad3cf38ca9b68442/packages/uswds-core/src/js/start.js
 const reinitializeUSWDS = () => {
   const target = document.body
   Object.keys(components).forEach((key) => {
