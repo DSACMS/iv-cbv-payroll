2662ee941ce60765ea52999502c67d75c2cb3e10
Jeff Catania <Jeffrey.M.Catania@omb.eop.gov>
2025-03-15 13:00:57 -0400

    test income.rb

---
+++ app/spec/services/aggregators/response_objects/income_spec.rb
@@ -0,0 +1,49 @@
+require 'rails_helper'
+
+RSpec.describe Aggregators::ResponseObjects::Income do
+  let(:pinwheel_response) do
+    {
+      "account_id" => "12345",
+      "pay_frequency" => "bi-weekly",
+      "compensation_amount" => 50.24,
+      "compensation_unit" => "USD"
+    }
+  end
+
+  let(:argyle_response) do
+    {
+      "account" => "67890",
+      "base_pay" => {
+        "period" => "semimonthly",
+        "amount" => "50.24",
+        "currency" => "USD"
+      }
+    }
+  end
+
+  describe 'Income.from_pinwheel' do
+    it 'creates an Income object from pinwheel response' do
+      income = described_class.from_pinwheel(pinwheel_response)
+
+      expect(income.account_id).to eq("12345")
+      expect(income.pay_frequency).to eq("semimonthly")
+      expect(income.compensation_amount).to eq(50.24)
+      expect(income.compensation_unit).to eq("USD")
+    end
+  end
+
+  describe 'Income.from_argyle' do
+    # before do
+    #  allow(Aggregators::FormatMethods::Argyle).to receive(:format_currency).and_return(3000)
+    # end
+
+    it 'creates an Income object from argyle response' do
+      income = described_class.from_argyle(argyle_response)
+
+      expect(income.account_id).to eq("67890")
+      expect(income.pay_frequency).to eq("monthly")
+      expect(income.compensation_amount).to eq(50.24)
+      expect(income.compensation_unit).to eq("USD")
+    end
+  end
+end
