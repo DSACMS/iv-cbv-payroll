1441a8627679e90bfe1bc8dacaddb57fa40ac409
Daphne Gold <daphnegold@navapbc.com>
2025-03-18 11:26:01 -0700

    invitation reminders deletions

---
+++ app/db/migrate/20250318171130_remove_invitation_reminder_sent_at_column_from_cbv_flow_invitation.rb
@@ -0,0 +1,5 @@
+class RemoveInvitationReminderSentAtColumnFromCbvFlowInvitation < ActiveRecord::Migration[7.1]
+  def change
+    remove_column :cbv_flow_invitations, :invitation_reminder_sent_at
+  end
+end
