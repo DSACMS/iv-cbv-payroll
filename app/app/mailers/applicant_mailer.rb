ab28c0e2fdf51812024817e0a3c6bda9c8119175
Daphne Gold <daphnegold@navapbc.com>
2025-03-19 15:17:17 -0700

    FFS-2539: Remove CBV Invitation Reminder Emails (#515)

--- app/app/mailers/applicant_mailer.rb
+++ app/app/mailers/applicant_mailer.rb
@@ -10,13 +10,6 @@ class ApplicantMailer < ApplicationMailer
     )
   end
 
-  def invitation_reminder_email
-    mail(
-      to: @cbv_flow_invitation.email_address,
-      subject: view_context.agency_translation("applicant_mailer.invitation_reminder_email.subject")
-    )
-  end
-
   private
 
   def set_locale(&action)
