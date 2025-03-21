b3730b3da05af78a39c1e7db4788ef691cea20b7
Jeff Catania <Jeffrey.M.Catania@omb.eop.gov>
2025-03-20 23:54:34 -0400

    fix errors in argyle_report

--- app/app/services/aggregators/aggregator_reports/argyle_report.rb
+++ app/app/services/aggregators/aggregator_reports/argyle_report.rb
@@ -8,11 +8,12 @@ module Aggregators::AggregatorReports
     end
 
     private
-    def fetch_report_data(from_date, to_date)
+    # TODO: bring this to abstract class
+    def fetch_report_data
       all_successful = true
       @payroll_accounts.each do |account|
         begin
-          fetch_report_data_for_account(account, from_date, to_date)
+          fetch_report_data_for_account(account)
         rescue StandardError => e
           Rails.logger.error("Report Fetch Error: #{e.message}")
           all_successful = false
@@ -21,9 +22,9 @@ module Aggregators::AggregatorReports
       @has_fetched = all_successful
     end
 
-    def fetch_report_data_for_account(account, from_date, to_date)
+    def fetch_report_data_for_account(account)
       identities_json = @argyle_service.fetch_identities_api(account: account)
-      paystubs_json = @argyle_service.fetch_paystubs_api(account: account, from_start_date: from_date, to_start_date: to_date)
+      paystubs_json = @argyle_service.fetch_paystubs_api(account: account, from_start_date: @from_date, to_start_date: @to_date)
 
       @identities.append(*transform_identities(identities_json))
       @employments.append(*transform_employments(identities_json))
