bc39ed0899350a7693d4baf9306c0685fcf01f35
Tim Miller <timmiller@navapbc.com>
2025-03-25 08:44:42 -0500

    working form -- stores info to applicant

--- app/app/controllers/cbv/applicant_informations_controller.rb
+++ app/app/controllers/cbv/applicant_informations_controller.rb
@@ -1,13 +1,21 @@
 class Cbv::ApplicantInformationsController < Cbv::BaseController
   include Cbv::PinwheelDataHelper
+  before_action :set_cbv_applicant, only: %i[show update]
   before_action :redirect_when_in_invitation_flow, :redirect_when_info_present
   before_action :set_identities, only: %i[show update]
 
   def show
-    Rails.logger.info("TIMOTEST ApplicantInformationsController show: identities: #{@identities.inspect}")
   end
 
   def update
+    begin
+      @cbv_applicant.update!(applicant_params[:cbv_applicant])
+    rescue => e
+      Rails.logger.error("Error updating applicant: #{e.message}")
+      flash[:alert] = "An error occurred while updating the applicant information. Please try again."
+      return redirect_to cbv_flow_applicant_information_path
+    end
+
     redirect_to next_path
   end
 
@@ -22,5 +30,20 @@ class Cbv::ApplicantInformationsController < Cbv::BaseController
 
   def redirect_when_in_invitation_flow
     redirect_to next_path if @cbv_flow.cbv_flow_invitation.present?
+  end
+
+  def applicant_params
+    params.fetch("cbv_applicant_#{@cbv_flow.client_agency_id}", {}).permit(
+      :cbv_applicant => [
+        :first_name,
+        :middle_name,
+        :last_name,
+        :case_number
+      ]
+    )
+  end
+
+  def set_cbv_applicant
+    @cbv_applicant = @cbv_flow.cbv_applicant
   end
 end
