5fec9a9f4192dc9e9f734285ee9fb47f6f4cf225
Tom Dooner <tomdooner@navapbc.com>
2025-03-28 16:56:42 -0700

    Fix `rails users:promote_to_service_account`
    Not sure how this worked before. I also provided example invocations
    as comments because passing in arguments to rake tasks is a dark art.

--- app/spec/lib/users_rake_spec.rb
+++ app/spec/lib/users_rake_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "users.rake" do
   describe "promotes and demotes user service accounts" do
     it "promotes a user to a service account" do
       user = create(:user)
-      Rake::Task['users:promote_to_service_account'].execute(user.id.to_s)
+      Rake::Task['users:promote_to_service_account'].execute(user_id: user.id.to_s)
       user.reload
       expect(user.is_service_account).to eq(true)
       expect(user.api_access_tokens.count).to eq(1)
@@ -12,7 +12,7 @@ RSpec.describe "users.rake" do
 
     it "demotes a service account user and removes all access tokens" do
       user = create(:user, :with_access_token, is_service_account: true)
-      Rake::Task['users:demote_service_account'].execute(user.id.to_s)
+      Rake::Task['users:demote_service_account'].execute(user_id: user.id.to_s)
       user.reload
       expect(user.is_service_account).to eq(false)
       expect(user.api_access_tokens.where(deleted_at: nil).count).to eq(0)
