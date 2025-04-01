fd5fc60f0466e9e52799b336a9c6b46a3879d2c2
George Byers <georgebyers@navapbc.com>
2025-04-01 15:40:16 -0400

    Updated the comment for the argyle_controller#create method

--- app/app/controllers/api/argyle_controller.rb
+++ app/app/controllers/api/argyle_controller.rb
@@ -1,16 +1,18 @@
 class Api::ArgyleController < ApplicationController
+
+  # This API endpoint is used to associate the user's CbvFlow
+  # with a user_token and user id (id) supplied by Argyle.
+  # This enables the client to retrieve an Argyle user_token to open
+  # the Argyle modal or create an Argyle Link. It also provides a means
+  # to associate an incoming webhook request sent by Argyle with an instance
+  # of CBV PayrollAccount::Argyle
+  #
+  # @see https://docs.argyle.com/link/user-tokens
   def create
     @cbv_flow = CbvFlow.find(session[:cbv_flow_id])
     argyle = argyle_for(@cbv_flow)
     argyle.create_user(@cbv_flow.end_user_id) => {id:, user_token:}
 
-    # CbvFlow.end_user_id is automatically generated
-    # We need to update the CbvFlow with the Argyle user ID so we can
-    # associate the Argyle user with the CbvFlow.
-    # Providing an initial external_id to Argyle does not always
-    # result in Argyle webhooks returning the custom external_id
-    # so we cannot rely on Argyle to provide the "external_id" .e.g
-    # the auto-generated CbvFlow.end_user_id in the webhook response
     @cbv_flow.update({
       pinwheel_token_id: user_token,
       end_user_id: id
