b7f6fb299921e14279da31a1e1e7ac73063960fe
Daphne Gold <daphnegold@navapbc.com>
2025-04-02 13:14:30 -0700

    Add sandbox boolean to Argyle Controller

--- app/app/controllers/api/argyle_controller.rb
+++ app/app/controllers/api/argyle_controller.rb
@@ -3,7 +3,8 @@ class Api::ArgyleController < ApplicationController
     @cbv_flow = CbvFlow.find(session[:cbv_flow_id])
     argyle = argyle_for(@cbv_flow)
     user = argyle.create_user
+    is_sandbox_environment = ENV["ARGYLE_SANDBOX"].present? && ENV["ARGYLE_SANDBOX"] == "true"
 
-    render json: { status: :ok, user: user }
+    render json: { status: :ok, user: user, isSandbox: is_sandbox_environment }
   end
 end
