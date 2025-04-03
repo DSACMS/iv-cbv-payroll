bfc581d0507843b7cf509f58bc286cfc2aa4a128
Daphne Gold <daphnegold@navapbc.com>
2025-04-03 09:38:59 -0700

    Adapter test is more explicit now

--- app/spec/javascript/adapters/ArgyleModalAdapter.spec.js
+++ app/spec/javascript/adapters/ArgyleModalAdapter.spec.js
@@ -49,6 +49,7 @@ describe("ArgyleModalAdapter", () => {
           sandbox: mockArgyleAuthToken.isSandbox,
         })
       )
+      expect(mockArgyleAuthToken.isSandbox).toBe(true)
     })
   })
 
