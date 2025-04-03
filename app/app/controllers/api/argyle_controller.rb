c85f899303284e6fa6274c250b8c5cb401f53a32
Daphne Gold <daphnegold@navapbc.com>
2025-04-03 08:54:09 -0700

    Simplify check in argyle controller, .present? was redundant

--- app/app/controllers/api/argyle_controller.rb
+++ app/app/controllers/api/argyle_controller.rb
@@ -3,7 +3,7 @@ class Api::ArgyleController < ApplicationController
     @cbv_flow = CbvFlow.find(session[:cbv_flow_id])
     argyle = argyle_for(@cbv_flow)
     user = argyle.create_user
-    is_sandbox_environment = ENV["ARGYLE_SANDBOX"].present? && ENV["ARGYLE_SANDBOX"] == "true"
+    is_sandbox_environment = ENV["ARGYLE_SANDBOX"] == "true"
 
     render json: { status: :ok, user: user, isSandbox: is_sandbox_environment }
   end
