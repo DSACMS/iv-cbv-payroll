fcfb83c5bb92a0a9b1232697023ab5d97b4263e3
Tom Dooner <tomdooner@navapbc.com>
2025-04-04 09:25:53 -0700

    Merge via Squash - cle/ffs-2598-payment-details-sync

---
+++ app/app/javascript/controllers/polling_controller.js
@@ -0,0 +1,24 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static values = { url: String }
+  headers = { Accept: "text/vnd.turbo-stream.html" }
+
+  connect() {
+    this.interval = setInterval(() => {
+      const csrfToken = document.querySelector("meta[name='csrf-token']").content
+      fetch(this.urlValue, {
+        headers: { ...this.headers, "X-CSRF-Token": csrfToken },
+        method: "PATCH",
+      })
+        .then((response) => response.text())
+        .then((html) => {
+          // on redirect stop the interval to ensure that the payment details page loads before the turboframe gets another command
+          if (html.includes('turbo-stream action="redirect')) {
+            clearInterval(this.interval)
+          }
+          return Turbo.renderStreamMessage(html)
+        })
+    }, 2000)
+  }
+}
