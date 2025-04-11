679e6e6730ea6c1db84ff169ae7d498e46ffb614
Ian Norris <ian.norris1@cms.hhs.gov>
2025-04-11 13:40:12 -0700

    adjust session tokens to be cookies so they are actually reusable

--- load_testing/loadtest.js
+++ load_testing/loadtest.js
@@ -20,25 +20,14 @@ if(URL === undefined) {
 
 export default function () {
     let headers;
-    let tokenList = TOKENS.split(",");
+    // let tokenList = TOKENS.split(",");
     group("logging in", () => {
         let res = http.get(`${URL}/sandbox`);
         check(res, { 'status is 200': (r) => r.status === 200 });
 
-        let randomToken = tokenList[Math.floor(Math.random() * tokenList.length)]
-        let loginRes = http.get(`${URL}/en/cbv/entry?token=${randomToken}`)
-        check(loginRes, { 'load test status is 200': (r) => r.status === 200 });
-
-        const cookies = loginRes.cookies['_iv_cbv_payroll_session'];
-        const sessionCookie = cookies && cookies.length > 0 ? cookies[0].value : null;
-
-        if (!sessionCookie) {
-            console.error('No session cookie found');
-            return;
-        }
-
+        let randomToken = TOKENS[Math.floor(Math.random() * TOKENS.length)]
         headers = {
-            'Cookie': `_ib_cbv_payroll_session=${sessionCookie}`,
+            'Cookie': `_iv_cbv_payroll_session=${randomToken}`,
         };
     })
 
