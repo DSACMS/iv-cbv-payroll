4f051a01c7c946e68c3fd263b88842c20c8388ba
Daphne Gold <daphnegold@navapbc.com>
2025-04-18 15:38:10 -0700

    Simplify and test

--- app/app/javascript/controllers/language_controller.js
+++ app/app/javascript/controllers/language_controller.js
@@ -5,12 +5,9 @@ import { trackUserAction } from "../utilities/api"
 export default class extends Controller {
   switchLocale(event) {
     const locale = event.currentTarget.dataset.locale
-    const previousLocale = document.documentElement.lang
 
     trackUserAction("UserManuallySwitchedLanguage", {
       locale: locale,
-      previous_locale: previousLocale,
-      current_locale: previousLocale,
     })
   }
 }
