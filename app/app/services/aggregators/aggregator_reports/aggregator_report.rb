08952ade0e9347dc327af7c7ab6076c9bce21373
Ian Norris <iannorris@Ians-MacBook-Air.local>
2025-04-18 13:35:07 -0700

    fix it up a level instead

--- app/app/services/aggregators/aggregator_reports/aggregator_report.rb
+++ app/app/services/aggregators/aggregator_reports/aggregator_report.rb
@@ -24,20 +24,20 @@ module Aggregators::AggregatorReports
       @has_fetched
     end
 
-    def payroll_fetchable_accounts
+    def fetchable_payroll_accounts
       @payroll_accounts.select do |payroll_account|
-        payroll_account.has_fully_synced?
+        payroll_account.has_fully_synced? && payroll_account.suppor
       end
     end
 
     def is_ready_to_fetch?
-      payroll_fetchable_accounts.present?
+      fetchable_payroll_accounts.present?
     end
 
     def fetch_report_data
       begin
         all_successful = true
-        payroll_fetchable_accounts.each do |payroll_account|
+        fetchable_payroll_accounts.each do |payroll_account|
           fetch_report_data_for_account(payroll_account)
         end
       rescue StandardError => e
