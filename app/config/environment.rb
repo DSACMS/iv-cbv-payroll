86b0094b187398fc523c44a4d15479c147eb64a1
Ian Norris <iannorris@Ians-MacBook-Air.local>
2025-04-21 09:25:54 -0700

    [FFS-2750] add ability to see the dashboard across environments
    by default, the jobs dashboard engine we use leverages rails credentials. This trick lets us hook into the configuration directly instead so we can inject environment variables, since I don't think we have time right now to refactor into rails creds.

--- app/config/environment.rb
+++ app/config/environment.rb
@@ -1,5 +1,15 @@
 # Load the Rails application.
 require_relative "application"
 
+
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Rails.application.config.after_initialize do
+  # Your code here
+  # You can call services, start background tasks, preload data, etc.
+  # if the password is not defined, Mission Control will 401
+  # TODO once we're on rails credentials, remove this and replace with rails creds
+  MissionControl::Jobs.http_basic_auth_user = ENV["MISSION_CONTROL_USER"]
+  MissionControl::Jobs.http_basic_auth_password = ENV["MISSION_CONTROL_PASSWORD"]
+end
