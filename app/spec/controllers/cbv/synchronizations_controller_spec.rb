892cbed3a505dc1bf305713152888ed68e7657ca
Daphne Gold <daphnegold@navapbc.com>
2025-04-24 11:56:55 -0700

    Add contexts to all the things

--- app/spec/controllers/cbv/synchronizations_controller_spec.rb
+++ app/spec/controllers/cbv/synchronizations_controller_spec.rb
@@ -13,59 +13,53 @@ RSpec.describe Cbv::SynchronizationsController do
   end
 
   describe "#show" do
-    it "redirects to the payment details page" do
-      get :show, params: { user: { account_id: payroll_account.pinwheel_account_id } }
+    context "when account exists" do
+      it "redirects to the payment details page" do
+        get :show, params: { user: { account_id: payroll_account.pinwheel_account_id } }
 
-      expect(response).to redirect_to(cbv_flow_payment_details_path(user: { account_id: payroll_account.pinwheel_account_id }))
+        expect(response).to redirect_to(cbv_flow_payment_details_path(user: { account_id: payroll_account.pinwheel_account_id }))
+      end
     end
 
-    it "renders the page when account doesn't exist" do
-      get :show, params: { user: { account_id: nonexistent_id } }
+    context "when account doesn't exist" do
+      it "renders the page" do
+        get :show, params: { user: { account_id: nonexistent_id } }
 
-      expect(response).to be_successful
-      expect(response).to render_template(:show)
+        expect(response).to be_successful
+        expect(response).to render_template(:show)
+      end
     end
   end
 
   describe "#update" do
-    it "redirects to the payment details page" do
-      patch :update, params: { user: { account_id: payroll_account.pinwheel_account_id } }
-
-      expect(response.body).to include("cbv/payment_details")
-      expect(response.body).to include("turbo-stream action=\"redirect\"")
-    end
-
     it "does not fire tracking event if its for the polling purposes" do
       expect_any_instance_of(GenericEventTracker).not_to receive(:track)
 
       patch :update, params: { user: { account_id: payroll_account.pinwheel_account_id } }
     end
 
-    it "continues polling when account doesn't exist" do
-      patch :update, params: { user: { account_id: nonexistent_id } }
+    context "when account exists and is fully synced" do
+      it "redirects to the payment details page" do
+        patch :update, params: { user: { account_id: payroll_account.pinwheel_account_id } }
 
-      expect(response.body).to include("turbo-frame id=\"synchronization\"")
+        expect(response.body).to include("cbv/payment_details")
+        expect(response.body).to include("turbo-stream action=\"redirect\"")
+      end
     end
 
-    it "continues polling when account exists but is not fully synced" do
-      allow_any_instance_of(PayrollAccount::Pinwheel).to receive(:has_fully_synced?).and_return(false)
+    context "when account exists but is not fully synced" do
+      before do
+        allow_any_instance_of(PayrollAccount::Pinwheel).to receive(:has_fully_synced?).and_return(false)
+      end
 
-      patch :update, params: { user: { account_id: payroll_account.pinwheel_account_id } }
+      it "continues polling" do
+        patch :update, params: { user: { account_id: payroll_account.pinwheel_account_id } }
 
-      expect(response.body).to include("turbo-frame id=\"synchronization\"")
+        expect(response.body).to include("turbo-frame id=\"synchronization\"")
+      end
     end
 
-    it "renders partial and continues polling when payroll_account is nil" do
-      controller.instance_variable_set(:@payroll_account, nil)
-
-      patch :update, params: { user: { account_id: nonexistent_id } }
-
-      expect(response.body).to include("turbo-frame id=\"synchronization\"")
-      expect(response.body).not_to include("synchronization_failures")
-      expect(response).to render_template(partial: "_status")
-    end
-
-    context "when the paystubs synchronization fails" do
+    context "when account exists but paystubs synchronization fails" do
       let(:errored_jobs) { [ "paystubs" ] }
 
       it "redirects to the payment details page" do
@@ -73,6 +67,28 @@ RSpec.describe Cbv::SynchronizationsController do
 
         expect(response.body).to include("cbv/payment_details")
         expect(response.body).to include("turbo-stream action=\"redirect\"")
+      end
+    end
+
+    context "when account doesn't exist" do
+      it "continues polling" do
+        patch :update, params: { user: { account_id: nonexistent_id } }
+
+        expect(response.body).to include("turbo-frame id=\"synchronization\"")
+      end
+    end
+
+    context "when payroll_account is nil" do
+      before do
+        controller.instance_variable_set(:@payroll_account, nil)
+      end
+
+      it "renders partial and continues polling" do
+        patch :update, params: { user: { account_id: nonexistent_id } }
+
+        expect(response.body).to include("turbo-frame id=\"synchronization\"")
+        expect(response.body).not_to include("synchronization_failures")
+        expect(response).to render_template(partial: "_status")
       end
     end
   end
