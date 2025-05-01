e17eb874db686c206e1aaecbbb3f4e80da017bc5
George Byers <georgebyers@navapbc.com>
2025-05-01 15:15:14 -0400

    removed host header setting that isnt needed

--- app/spec/controllers/cbv/generic_links_controller_spec.rb
+++ app/spec/controllers/cbv/generic_links_controller_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe Cbv::GenericLinksController do
   describe '#show' do
     context 'when the hostname matches a client agency domain' do
       it "starts a CBV flow" do
-        request.host = "sandbox.reportmyincome.org"
         get :show, params: { client_agency_id: "sandbox" }
         expect(response).to redirect_to(cbv_flow_entry_path)
         expect(assigns(:cbv_flow).client_agency_id).to eq("sandbox")
