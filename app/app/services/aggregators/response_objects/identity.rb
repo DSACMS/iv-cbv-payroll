c0fae5bf12929d57b9f89eec376e6ce59d4b90ae
George Byers II <george@codehosted.com>
2025-05-14 17:15:55 -0400

    FFS-2825: Add zip code and age range to metrics (#669)
    * Added a helper method get_age_range. Updated events_controllers for aggregators to include identity_zip_code, identity_age_range, and identity_age_range_applicant
    
    * Removed redundant test
    
    * addressing PR feedback. Ensuring single responsbility by creating a new method to cast date strings to date obj. Swapping native attribute accessor with dig
    
    * updated age range from 18-25 -> 19-25
    
    * fixing test for previously invalid age range
    
    * added timecop to prevent date drifting during tests
    
    * Taking Jeffs suggestion and updating get_age_range to accept a second optional arg for [now]

--- app/app/services/aggregators/response_objects/identity.rb
+++ app/app/services/aggregators/response_objects/identity.rb
@@ -6,6 +6,7 @@ module Aggregators::ResponseObjects
     emails
     ssn
     phone_numbers
+    zip_code
   ]
 
   Identity = Struct.new(*IDENTITY_FIELDS, keyword_init: true) do
@@ -16,7 +17,8 @@ module Aggregators::ResponseObjects
         date_of_birth: response_body["date_of_birth"],
         emails: response_body["emails"],
         ssn: ("XXX-XX-#{response_body["last_four_ssn"]}" if response_body["last_four_ssn"].present?),
-        phone_numbers: response_body["phone_numbers"]
+        phone_numbers: response_body["phone_numbers"],
+        zip_code: response_body.dig("address", "postal_code")
       )
     end
 
@@ -27,7 +29,8 @@ module Aggregators::ResponseObjects
         date_of_birth: identity_response_body["birth_date"],
         emails: [ identity_response_body["email"] ],
         ssn: Aggregators::FormatMethods::Argyle.obfuscate_ssn(identity_response_body["ssn"]),
-        phone_numbers: [ identity_response_body["phone_number"] ]
+        phone_numbers: [ identity_response_body["phone_number"] ],
+        zip_code: identity_response_body.dig("address", "postal_code")
       )
     end
   end
