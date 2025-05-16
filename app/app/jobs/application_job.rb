de4f4de7e846bc688b28a5debf05db2a1adc2bb8
Ian Norris <iannorris@Ians-MacBook-Air.local>
2025-05-16 16:15:06 -0700

    fix oops and lets test it while we're at it

--- app/app/jobs/application_job.rb
+++ app/app/jobs/application_job.rb
@@ -6,22 +6,18 @@ class ApplicationJob < ActiveJob::Base
   # Uses https://edgeguides.rubyonrails.org/active_job_basics.html#error-reporting-on-jobs as a pattern
   # in order to send information to newrelic that we had a failed job and enable alerting on said failed job.
   rescue_from(Exception) do |error|
-    ActiveSupport::Notifications.subscribe("") do |name, start, finish, id, payload|
-      job = self.class
-      queue_name = job.queue_name || "default"
-      job_class = job.job_class || "UnknownJob"
+    puts "what the cluck"
+    job = self.class
+    queue_name = job.queue_name || "default"
+    job_class = job || "UnknownJob"
 
-
-      NewRelic::Agent.record_custom_event("SolidQueueJobFailed", {
-        job_class: job_class,
-        queue_name: queue_name,
-        failed_at: finish,
-        error_class: error.class.name,
-        error_message: error.message
-      })
-
-      NewRelic::Agent.record_metric("Custom/SolidQueue/Failed", 1)
-    end
+    NewRelic::Agent.record_custom_event("SolidQueueJobFailed", {
+      job_class: job_class,
+      queue_name: queue_name,
+      failed_at: Time.now,
+      error_class: error.class.name,
+      error_message: error.message
+    })
     raise error
   end
 end
