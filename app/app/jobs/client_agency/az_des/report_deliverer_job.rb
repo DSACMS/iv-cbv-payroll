e01fef6ac4b430bd571c67363f83685c6ce7d949
Ian Norris <iannorris@Ians-MacBook-Air.local>
2025-05-19 16:26:38 -0700

    rename per feedback

--- app/app/jobs/client_agency/az_des/report_deliverer_job.rb
+++ app/app/jobs/client_agency/az_des/report_deliverer_job.rb
@@ -1,16 +1,16 @@
 class ClientAgency::AzDes::ReportDelivererJob < ApplicationJob
   def perform(date_start, date_end)
-    cbv_applicants_delivered_recently = CbvFlow.where(transmitted_at: date_start..date_end).where(client_agency_id: ClientAgency::AzDes::Configuration.client_agency_id)
-    if cbv_applicants_delivered_recently.empty?
+    cbv_flows_delivered_recently = CbvFlow.where(transmitted_at: date_start..date_end).where(client_agency_id: ClientAgency::AzDes::Configuration.client_agency_id)
+    if cbv_flows_delivered_recently.empty?
       Rails.logger.info "delivered 0 applications for az_des in time range #{date_start}..#{date_end}"
       return
     end
 
-    csv = ClientAgency::AzDes::RecentlySubmittedCasesCsv.new.generate_csv(cbv_applicants_delivered_recently)
+    csv = ClientAgency::AzDes::RecentlySubmittedCasesCsv.new.generate_csv(cbv_flows_delivered_recently)
 
     sftp_gateway.upload_data(csv, "#{config["sftp_directory"]}/#{filename(date_start)}")
 
-    Rails.logger.info "delivered #{cbv_applicants_delivered_recently.count} applications for az_des in time range #{date_start}..#{date_end}"
+    Rails.logger.info "delivered #{cbv_flows_delivered_recently.count} applications for az_des in time range #{date_start}..#{date_end}"
   end
 
   def sftp_gateway
