5a20ad71ffb643520b9343195476419c15cc4fa8
Tom Dooner <tomdooner@navapbc.com>
2025-05-21 15:34:44 -0700

    Update app/app/services/aggregators/sdk/argyle_service.rb
    Co-authored-by: Jeff Catania <jeffrey.catania@cms.hhs.gov>

--- app/app/services/aggregators/sdk/argyle_service.rb
+++ app/app/services/aggregators/sdk/argyle_service.rb
@@ -133,6 +133,7 @@ module Aggregators::Sdk
       @http.get(build_url("#{ACCOUNTS_ENDPOINT}/#{account}")).body
     end
 
+    # https://docs.argyle.com/api-reference/accounts#delete
     def delete_account_api(account:)
       @http.delete(build_url("#{ACCOUNTS_ENDPOINT}/#{account}")).body
     end
