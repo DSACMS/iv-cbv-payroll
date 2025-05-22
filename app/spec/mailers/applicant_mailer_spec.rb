31354f3eab08439522cf6d3e96c87f2a062faae9
Daphne Gold <daphnegold@navapbc.com>
2025-05-22 09:26:53 -0700

    Remove ma/nyc, refactor a little

--- app/spec/mailers/applicant_mailer_spec.rb
+++ app/spec/mailers/applicant_mailer_spec.rb
@@ -43,33 +43,5 @@ RSpec.describe ApplicantMailer, type: :mailer do
         expect(mail.body.encoded).to include(I18n.t("applicant_mailer.invitation_email.body_1.default", locale: :es, agency_acronym: "CBV"))
       end
     end
-
-    context "for a NYC CbvFlowInvitation" do
-      let(:cbv_flow_invitation) { create(:cbv_flow_invitation, :nyc, email_address: email) }
-
-      it "renders the body" do
-        unescaped_body = CGI.unescape_html(mail.body.encoded)
-        expect(unescaped_body).to match(I18n.t('applicant_mailer.invitation_email.header.nyc'))
-        expect(unescaped_body).to match(I18n.t("applicant_mailer.invitation_email.body_1.default", agency_acronym: "HRA"))
-        expect(unescaped_body).to match(I18n.t("applicant_mailer.invitation_email.body_2_html.default", deadline: "July 21, 2024"))
-        expect(unescaped_body).to match(I18n.t("applicant_mailer.invitation_email.body_3.default", app_name: "ACCESS HRA"))
-      end
-    end
-
-    context "for a MA CbvFlowInvitation" do
-      let(:cbv_flow_invitation) { create(:cbv_flow_invitation, :ma, email_address: email) }
-
-      it "renders the subject" do
-        expect(mail.subject).to eq(I18n.t('applicant_mailer.invitation_email.subject.ma'))
-      end
-
-      it "renders the body" do
-        unescaped_body = CGI.unescape_html(mail.body.encoded)
-        expect(unescaped_body).to match(I18n.t('applicant_mailer.invitation_email.header.ma'))
-        expect(unescaped_body).to match(I18n.t("applicant_mailer.invitation_email.body_1.ma", agency_acronym: "DTA"))
-        expect(unescaped_body).to match(I18n.t("applicant_mailer.invitation_email.body_2_html.ma", deadline: "July 21, 2024"))
-        expect(unescaped_body).to match(I18n.t("applicant_mailer.invitation_email.body_3.ma", app_name: "DTA Connect"))
-      end
-    end
   end
 end
--- app/spec/mailers/previews/weekly_report_mailer_preview.rb
+++ app/spec/mailers/previews/weekly_report_mailer_preview.rb
@@ -1,19 +1,5 @@
 # Preview all emails at http://localhost:3000/rails/mailers/weekly_report_mailer
 class WeeklyReportMailerPreview < ActionMailer::Preview
-  def report_email_nyc
-    WeeklyReportMailer.with(
-      client_agency_id: "nyc",
-      report_date: Date.today
-    ).report_email
-  end
-
-  def report_email_ma
-    WeeklyReportMailer.with(
-      client_agency_id: "ma",
-      report_date: Date.today
-    ).report_email
-  end
-
   def report_email_la_ldh
     WeeklyReportMailer.with(
       client_agency_id: "la_ldh",
