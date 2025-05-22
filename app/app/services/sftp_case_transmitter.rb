caabff7a27fb67a653934bb0030043b4243ea7a5
Ian Norris <iannorris@Ians-MacBook-Air.local>
2025-05-21 18:35:07 -0700

    fix rubocop

--- app/app/services/sftp_case_transmitter.rb
+++ app/app/services/sftp_case_transmitter.rb
@@ -9,7 +9,7 @@ class SftpCaseTransmitter
 
   def deliver_sftp!
     config = current_agency.transmission_method_configuration.with_indifferent_access
-    sftp_gateway = SftpGateway.new(config)  
+    sftp_gateway = SftpGateway.new(config)
     filename = ClientAgency::AzDes::Configuration.pdf_filename(cbv_flow, cbv_flow.consented_to_authorized_use_at)
     sftp_gateway.upload_data(StringIO.new(pdf_output.content), "#{config["sftp_directory"]}/#{filename}.pdf")
   end
