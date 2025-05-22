54da81ee055cd629114666f0560138a300ac1fd9
Daphne Gold <daphnegold@navapbc.com>
2025-05-22 09:59:21 -0700

    FFS-2572: Our mailer testers are broken (#703)

--- app/spec/mailers/previews/caseworker_mailer_preview.rb
+++ app/spec/mailers/previews/caseworker_mailer_preview.rb
@@ -1,25 +1,26 @@
-
 # Preview all emails at http://localhost:3000/rails/mailers/caseworker_mailer
 class CaseworkerMailerPreview < BaseMailerPreview
   include ReportViewHelper
   include TestHelpers
 
-  def summary_email
+  def summary_email_la_ldh
     caseworker_user = FactoryBot.create(:user, email: "#{SecureRandom.uuid}@example.com")
-    invitation = FactoryBot.create(:cbv_flow_invitation, :nyc, user: caseworker_user)
+
     cbv_flow = FactoryBot.create(
       :cbv_flow,
       :with_pinwheel_account,
       :completed,
-      cbv_flow_invitation: invitation
+      client_agency_id: "la_ldh",
+      cbv_applicant_attributes: {
+        case_number: "LA12345"
+      }
     )
 
     aggregator_report = FactoryBot.build(:pinwheel_report)
 
     CaseworkerMailer.with(
-      email_address: invitation.email_address,
+      email_address: caseworker_user.email,
       cbv_flow: cbv_flow,
-      case_number: "12345",
       aggregator_report: aggregator_report
     ).summary_email
   end
