6d5902e571e90f73ce1055d9c8059462d1cac9ec
Jeff Catania <Jeffrey.M.Catania@omb.eop.gov>
2025-05-27 18:36:08 -0400

    reset aggregate_data_point_component.rb

--- app/app/components/aggregate_data_point_component.rb
+++ app/app/components/aggregate_data_point_component.rb
@@ -133,28 +133,4 @@ class AggregateDataPointComponent < ViewComponent::Base
       value: ssn
     }
   end
-
-  def accrued_gross_earnings(accrued_gross_earnings)
-    {
-
-      label: I18n.t("components.report.monthly_summary_table.accrued_gross_earnings"),
-      value: format_money(accrued_gross_earnings)
-    }
-  end
-
-  def total_gig_hours(total_gig_hours)
-    {
-
-      label: I18n.t("components.report.monthly_summary_table.total_gig_hours"),
-      value: total_gig_hours
-    }
-  end
-
-  def month(date)
-    {
-
-      label: I18n.t("components.report.monthly_summary_table.month"),
-      value: format_parsed_date(date, :month_year)
-    }
-  end
 end
