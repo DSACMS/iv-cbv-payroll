d24d58024b4c98b579ede01b7514a15efd88b632
Jeff Catania <Jeffrey.M.Catania@omb.eop.gov>
2025-05-27 15:24:16 -0400

    Enhance monthly summary logic and improve test coverage
    Refactored the `MonthlySummaryTableComponent` to dynamically determine the employer name, replacing a hardcoded value. Removed outdated and unused methods to streamline the codebase. Added comprehensive tests for employer name resolution and summary calculations to ensure correctness and maintainability.

--- app/app/components/report/monthly_summary_table_component.rb
+++ app/app/components/report/monthly_summary_table_component.rb
@@ -5,11 +5,17 @@ class Report::MonthlySummaryTableComponent < ViewComponent::Base
   def initialize(report, payroll_account)
     @payroll_account = payroll_account
     @report = report
-    @employer_name = "test employer name"
+    @employer_name = employer_name
+  end
+
+  def employer_name
+    account_id = @payroll_account.pinwheel_account_id
+    employment = @report.employments.find { |e| e.account_id == account_id }
+    employment.employer_name if employment
   end
 
   def summarize_by_month(from_date: nil, to_date: nil)
-    account_id = @payroll_account.pinwheel_account_id # payroll_account.pinwheel_account_id
+    account_id = @payroll_account.pinwheel_account_id
     paystubs = @report.paystubs.filter { |paystub| paystub.account_id == account_id }
     gigs = @report.gigs.filter { |gig| gig.account_id == account_id }
     extracted_dates = extract_dates(paystubs, gigs)
@@ -38,61 +44,4 @@ class Report::MonthlySummaryTableComponent < ViewComponent::Base
 
     grouped_data
   end
-
-
-  #  # date_strings are used in ResponseObjects to store months in the "2010-01-01" format.
-  #  # This is a local static method to parse these into date objects. May return nil on error.
-  #  def parse_date_safely(date_string)
-  #    DateTime.parse(date_string) rescue nil
-  #  end
-  #
-  #  # month_strings are used in the context of the monthly summaries in the format "2010-01".
-  #  # This method parses these into date objects.  May return nil on error.
-  #  def parse_month_safely(month_string)
-  #    Date.strptime(month_string, "%Y-%m") rescue nil
-  #  end
-  #
-  #  # formats a date object into a month string "2010-05"
-  #  def format_month(date)
-  #    return nil unless date
-  #    date.strftime("%Y-%m")
-  #  end
-  #
-  #  def format_date(date)
-  #    return nil unless date
-  #    date.strftime("%Y-%m-%d")
-  #  end
-  #
-  #  def partial_month_description(range_start, range_end)
-  #    I18n.t("components.report.monthly_summary_table.partial_month_text",
-  #           start_date: format_parsed_date(range_start, :day_of_month),
-  #           end_date: format_parsed_date(range_end, :day_of_month))
-  #  end
-  #
-  #  # Given a list of dates, returns a deduplicated list of months in reverse chronological order (newest first).
-  #  def unique_months(dates)
-  #    dates.map { |date| format_month(date) }
-  #         .compact
-  #         .uniq
-  #         .sort_by { |month_string| parse_month_safely(month_string) }
-  #         .reverse
-  #  end
-  #
-  #  # return all paystub.pay_date and gig.start_date into a single list of dates.
-  #  # this is used as a helper to determine all unique months present in the report.
-  #  def extract_dates(paystubs, gigs)
-  #    (paystubs.map { |paystub| parse_date_safely(paystub.pay_date) } + gigs.map { |gig| parse_date_safely(gig.start_date) }).compact
-  #  end
-  #
-  #  def accrued_gross_earnings_cell_value(accrued_gross_earnings)
-  #    format_money(accrued_gross_earnings)
-  #  end
-  #
-  #  def total_gig_hours_cell_value(total_gig_hours)
-  #    total_gig_hours
-  #  end
-  #
-  #  def month_cell_value(date)
-  #    format_parsed_date(date, :month_year)
-  #  end
 end
--- app/spec/components/report/monthly_summary_table_component_spec.rb
+++ app/spec/components/report/monthly_summary_table_component_spec.rb
@@ -27,15 +27,11 @@ RSpec.describe Report::MonthlySummaryTableComponent, type: :component do
     context "with bob, a gig-worker" do
       let(:argyle_report) { Aggregators::AggregatorReports::ArgyleReport.new(payroll_accounts: [ payroll_account ], argyle_service: argyle_service) }# , from_date: current_time, to_date: current_time) }
       before do
-        # session[:cbv_flow_id] = cbv_flow.id
         argyle_stub_request_identities_response("bob")
         argyle_stub_request_paystubs_response("bob")
         argyle_stub_request_gigs_response("bob")
         argyle_stub_request_account_response("bob")
         Timecop.freeze(Time.local(2025, 04, 1, 0, 0))
-        # allow(argyle_service).to receive(:fetch_account_api).and_return(argyle_load_relative_json_file("bob", "request_account.json"))
-        # allow(argyle_service).to receive(:fetch_identities_api).and_return(argyle_load_relative_json_file("bob", "request_identity.json"))
-        # allow(argyle_service).to receive(:fetch_paystubs_api).and_return(argyle_load_relative_json_file("bob", "request_paystubs.json"))
         argyle_report.fetch
       end
 
@@ -46,7 +42,7 @@ RSpec.describe Report::MonthlySummaryTableComponent, type: :component do
       end
 
       it "includes table header" do
-        expect(subject.css("thead h4").to_html).to include "test employer name"
+        expect(subject.css("thead h4").to_html).to include "Lyft Driver"
         expect(subject.css("thead h4").to_html).to include "Monthly Summary"
 
         # assert that there are 3 column headers in table
@@ -76,227 +72,71 @@ RSpec.describe Report::MonthlySummaryTableComponent, type: :component do
         expect(subject.css("tbody tr:nth-child(3) td:nth-child(3)").to_html).to include "4.74"
       end
 
-      describe "#summarize_by_month" do
-        xit "returns a hash of monthly totals" do
-          summary_component = described_class.new(argyle_report, payroll_account)
-          expect(summary_component.summarize_by_month(from_date: Date.parse("2025-01-08"))).to eq({
-            "2025-04" => {
-              "gigs" => 100,
-              "paystubs" => 100,
-              "payroll_accounts" => 1,
-              "total_payroll_accounts" => 1,
-              "total_gigs" => 100,
-              "total_paystubs" => 100
-            }
-          })
+      describe "#find_employer_name" do
+        it "returns the correct employer name for the specified account id" do
+          # Initializing the component under test
+          employer_name = described_class.new(argyle_report, payroll_account).employer_name
+
+          # Verifying the method returns the correct employer name
+          expect(employer_name).to eq("Lyft Driver")
+        end
+
+        it "returns the correct employer name for the specified account id" do
+          invalid_payroll_account = create(
+            :payroll_account,
+            :argyle_fully_synced,
+            cbv_flow: cbv_flow,
+            pinwheel_account_id: "wrong-id"
+          )
+          # Initializing the component under test
+          employer_name = described_class.new(argyle_report, invalid_payroll_account).employer_name
+
+          # Verifying the method returns the correct employer name
+          expect(employer_name).to be_nil
         end
       end
-    end
-  end
 
-  # TODO: move this to a different test file
-  describe "module monthly_summary_table_helper" do
-    subject { described_class }
+      describe "#summarize_by_month" do
+        it "returns a hash of monthly totals" do
+          monthly_summary = described_class.new(argyle_report, payroll_account).summarize_by_month(from_date: Date.parse("2025-01-08"))
+          expect(monthly_summary.keys).to match_array([ "2025-03", "2025-02", "2025-01" ])
 
-    describe ".partial_month_details" do
-      it "detects complete month when there are no activities" do
-        current_month_string = "2025-01"
-        activity_dates = []
-        from_date = Date.parse("2025-01-01")
-        to_date = Date.parse("2025-03-31")
+          march = monthly_summary["2025-03"]
+          expect(march[:gigs].length).to eq(9)
+          expect(march[:paystubs].length).to eq(1)
+          expect(march[:accrued_gross_earnings]).to eq(3456) # in cents
+          expect(march[:total_gig_hours]).to eq(3.61)
+          expect(march[:partial_month_range]).to an_object_eq_to({
+                                                             is_partial_month: true,
+                                                             description: "Partial month: from 3/1-3/6",
+                                                             included_range_start: Date.parse("2025-03-01"),
+                                                             included_range_end: Date.parse("2025-03-06")
+                                                           })
 
-        partial_month_details = subject.partial_month_details(current_month_string, activity_dates, from_date, to_date)
-        expect(partial_month_details).to an_object_eq_to({
-                                                           is_partial_month: false,
-                                                           included_range_start: Date.parse("2025-01-01"),
-                                                           included_range_end: Date.parse("2025-01-31")
-                                                         })
-      end
+          feb = monthly_summary["2025-02"]
+          expect(feb[:gigs].length).to eq(47)
+          expect(feb[:paystubs].length).to eq(4)
+          expect(feb[:accrued_gross_earnings]).to eq(23075) # in cents
+          expect(feb[:total_gig_hours]).to eq(21.82)
+          expect(feb[:partial_month_range]).to an_object_eq_to({
+                                                                   is_partial_month: false,
+                                                                   description: nil,
+                                                                   included_range_start: Date.parse("2025-02-01"),
+                                                                   included_range_end: Date.parse("2025-02-28")
+                                                                 })
 
-      it "detects complete first month when there is an activity on the first day" do
-        current_month_string = "2025-01"
-        activity_dates = [ Date.parse("2025-01-01") ]
-        from_date = Date.parse("2025-01-01")
-        to_date = Date.parse("2025-03-31")
-
-        partial_month_details = subject.partial_month_details(current_month_string, activity_dates, from_date, to_date)
-        expect(partial_month_details).to an_object_eq_to({
-                                                           is_partial_month: false,
-                                                           included_range_start: Date.parse("2025-01-01"),
-                                                           included_range_end: Date.parse("2025-01-31")
-                                                         })
-      end
-
-      it "detects complete last month when there is an activity on the last day" do
-        current_month_string = "2025-03"
-        activity_dates = [ Date.parse("2025-03-31") ]
-        from_date = Date.parse("2025-01-01")
-        to_date = Date.parse("2025-03-31")
-
-        partial_month_details = subject.partial_month_details(current_month_string, activity_dates, from_date, to_date)
-        expect(partial_month_details).to an_object_eq_to({
-                                                           is_partial_month: false,
-                                                           included_range_start: Date.parse("2025-03-01"),
-                                                           included_range_end: Date.parse("2025-03-31")
-                                                         })
-      end
-
-      it "detects partial first month" do
-        current_month_string = "2025-01"
-        activity_dates = [ Date.parse("2025-01-04"), Date.parse("2025-01-06") ]
-        from_date = Date.parse("2025-01-01")
-        to_date = Date.parse("2025-03-31")
-
-        partial_month_details = subject.partial_month_details(current_month_string, activity_dates, from_date, to_date)
-        expect(partial_month_details).to an_object_eq_to({
-                                                           is_partial_month: true,
-                                                           included_range_start: Date.parse("2025-01-04"),
-                                                           included_range_end: Date.parse("2025-01-31")
-                                                         })
-      end
-
-      it "detects partial last month" do
-        current_month_string = "2025-03"
-        activity_dates = [ Date.parse("2025-03-04"), Date.parse("2025-03-06") ]
-        from_date = Date.parse("2025-01-01")
-        to_date = Date.parse("2025-03-31")
-
-        partial_month_details = subject.partial_month_details(current_month_string, activity_dates, from_date, to_date)
-        expect(partial_month_details).to an_object_eq_to({
-                                                           is_partial_month: true,
-                                                           included_range_start: Date.parse("2025-03-01"),
-                                                           included_range_end: Date.parse("2025-03-06")
-                                                         })
-      end
-    end
-
-    describe ".parse_date_safely" do
-      it "parses a valid date string" do
-        valid_date = "2024-12-25"
-        parsed_date = subject.parse_date_safely(valid_date)
-        expect(parsed_date).to eq(Date.parse(valid_date))
-      end
-
-      it "returns nil for an invalid date string" do
-        invalid_date = "invalid-date-string"
-        parsed_date = subject.parse_date_safely(invalid_date)
-        expect(parsed_date).to be_nil
-      end
-
-      it "returns nil for nil input" do
-        parsed_date = subject.parse_date_safely(nil)
-        expect(parsed_date).to be_nil
-      end
-
-      it "returns nil for empty string input" do
-        parsed_date = subject.parse_date_safely("")
-        expect(parsed_date).to be_nil
-      end
-    end
-
-    describe ".parse_month_safely" do
-      it "parses a valid month string" do
-        valid_month = "2024-12"
-        parsed_date = subject.parse_month_safely(valid_month)
-        expect(parsed_date).to eq(Date.new(2024, 12, 01))
-      end
-
-      it "returns nil for an invalid month string" do
-        invalid_month = "invalid-month-string"
-        parsed_date = subject.parse_month_safely(invalid_month)
-        expect(parsed_date).to be_nil
-      end
-
-      it "returns nil for nil input" do
-        parsed_date = subject.parse_month_safely(nil)
-        expect(parsed_date).to be_nil
-      end
-
-      it "returns nil for empty string input" do
-        parsed_date = subject.parse_month_safely("")
-        expect(parsed_date).to be_nil
-      end
-    end
-
-    describe ".format_date" do
-      it "formats a valid date correctly" do
-        date = Date.new(2024, 12, 25)
-        formatted_date = subject.format_date(date)
-        expect(formatted_date).to eq("2024-12-25")
-      end
-
-      it "returns nil for nil input" do
-        formatted_date = subject.format_date(nil)
-        expect(formatted_date).to be_nil
-      end
-    end
-
-    describe ".format_month" do
-      subject { described_class }
-      it "formats a valid month correctly" do
-        month = Date.new(2024, 12, 13)
-        formatted_month = subject.format_month(month)
-        expect(formatted_month).to eq("2024-12")
-      end
-
-      it "returns nil for nil input" do
-        formatted_month = subject.format_month(nil)
-        expect(formatted_month).to be_nil
-      end
-    end
-
-    describe ".unique_months" do
-      it "returns a unique list of months in reverse chronological order" do
-        dates = [
-          Date.new(2025, 3, 15),
-          Date.new(2025, 2, 25),
-          Date.new(2025, 3, 5),
-          Date.new(2025, 1, 1)
-        ]
-
-        unique_months = subject.unique_months(dates)
-
-        expect(unique_months).to eq([ "2025-03", "2025-02", "2025-01" ])
-      end
-
-      it "returns an empty array when given an empty list" do
-        dates = []
-
-        unique_months = subject.unique_months(dates)
-
-        expect(unique_months).to eq([])
-      end
-
-      it "handles a list with dates all in the same month" do
-        dates = [
-          Date.new(2025, 3, 15),
-          Date.new(2025, 3, 5),
-          Date.new(2025, 3, 10)
-        ]
-
-        unique_months = subject.unique_months(dates)
-
-        expect(unique_months).to eq([ "2025-03" ])
-      end
-
-      it "handles nil values gracefully" do
-        dates = [
-          Date.new(2025, 3, 15),
-          nil,
-          Date.new(2025, 3, 5)
-        ]
-
-        unique_months = subject.unique_months(dates)
-
-        expect(unique_months).to eq([ "2025-03" ])
-      end
-
-      it "handles a list with only nil values" do
-        dates = [ nil, nil, nil ]
-
-        unique_months = subject.unique_months(dates)
-
-        expect(unique_months).to eq([])
+          jan = monthly_summary["2025-01"]
+          expect(jan[:gigs].length).to eq(10)
+          expect(jan[:paystubs].length).to eq(5)
+          expect(jan[:accrued_gross_earnings]).to eq(28237) # in cents
+          expect(jan[:total_gig_hours]).to eq(4.74)
+          expect(jan[:partial_month_range]).to an_object_eq_to({
+                                                                 is_partial_month: true,
+                                                                 description: "Partial month: from 1/2-1/31",
+                                                                 included_range_start: Date.parse("2025-01-02"),
+                                                                 included_range_end: Date.parse("2025-01-31")
+                                                               })
+        end
       end
     end
   end
