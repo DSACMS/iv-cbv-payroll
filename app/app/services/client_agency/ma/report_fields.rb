b83445f22f30a4da734b23d286ab214abcfea16d
Ian Norris <iannorris@Ians-MacBook-Air.local>
2025-05-27 20:25:43 -0700

    at sign cleanup

--- app/app/services/client_agency/ma/report_fields.rb
+++ app/app/services/client_agency/ma/report_fields.rb
@@ -1,8 +1,8 @@
 class ClientAgency::Ma::ReportFields
   def self.fields_for(cbv_flow)
     [
-      [ ".pdf.caseworker.client_email_address", @cbv_flow.cbv_applicant.client_email_address ],
-      [ ".pdf.caseworker.snap_agency_id", @cbv_flow.cbv_applicant.agency_id_number ]
+      [ ".pdf.caseworker.client_email_address", cbv_flow.cbv_applicant.client_email_address ],
+      [ ".pdf.caseworker.snap_agency_id", cbv_flow.cbv_applicant.agency_id_number ]
     ]
   end
 end
