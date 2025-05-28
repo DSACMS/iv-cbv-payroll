9260e7f786e033832950f19a632a82afbcb257ec
Ian Norris <iannorris@Ians-MacBook-Air.local>
2025-05-28 05:03:09 -0700

    typofix

--- app/app/services/client_agency/ma/report_fields.rb
+++ app/app/services/client_agency/ma/report_fields.rb
@@ -1,7 +1,7 @@
 class ClientAgency::Ma::ReportFields
   def self.fields_for(cbv_flow)
     [
-      [ ".pdf.caseworker.client_email_address", cbv_flow.cbv_applicant.client_email_address ],
+      [ ".pdf.caseworker.client_email_address", cbv_flow.cbv_applicant.email_address ],
       [ ".pdf.caseworker.snap_agency_id", cbv_flow.cbv_applicant.agency_id_number ]
     ]
   end
