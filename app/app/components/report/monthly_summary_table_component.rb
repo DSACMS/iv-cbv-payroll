6d0fe44f3065eae7e16094940721ff0c6d2bac2b
Jeff Catania <Jeffrey.M.Catania@omb.eop.gov>
2025-05-28 15:51:36 -0400

    Refactor MonthlySummaryTableComponent to simplify logic
    Revised initialization to streamline data fetching by delegating account-specific logic to `report.find_account_report`. Removed redundant methods and replaced instance method calls with instance variables for consistency and clarity. This enhances maintainability and reduces code duplication.

--- app/app/components/report/monthly_summary_table_component.rb
+++ app/app/components/report/monthly_summary_table_component.rb
@@ -1,20 +1,15 @@
 class Report::MonthlySummaryTableComponent < ViewComponent::Base
   include ReportViewHelper
+  include Report::MonthlySummaryTableHelper
+
+  attr_reader :employer_name
 
   def initialize(report, payroll_account)
     # Note: payroll_account may either be the ID or the payroll_account object
     @account_id = payroll_account.class == String ? payroll_account : payroll_account.pinwheel_account_id
-    @report = report
-    @employer_name = employer_name
-    @monthly_summary_data = @report.summarize_by_month[@account_id]
-  end
-
-  def employer_name
-    employment = @report.employments.find { |e| e.account_id == @account_id }
-    employment.employer_name if employment
-  end
-
-  def paystubs
-    @report.paystubs.filter { |paystub| paystub.account_id == @account_id }
+    account_report = report.find_account_report(@account_id)
+    @paystubs = account_report&.paystubs
+    @employer_name = account_report&.dig(:employment, :employer_name)
+    @monthly_summary_data = report.summarize_by_month[@account_id]
   end
 end
