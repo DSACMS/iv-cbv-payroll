3b729b82cfdeef66953ef10a408d0a293c8b5152
Ian Norris <iannorris@Ians-MacBook-Air.local>
2025-05-28 15:39:37 -0700

    introduce it for the non caseworker specific fields too

--- app/app/services/client_agency/ma/report_fields.rb
+++ app/app/services/client_agency/ma/report_fields.rb
@@ -1,8 +1,14 @@
 class ClientAgency::Ma::ReportFields
-  def self.fields_for(cbv_flow)
+  def self.caseworker_fields_for(cbv_flow)
     [
       [ ".pdf.caseworker.client_email_address", cbv_flow.cbv_flow_invitation.email_address ],
       [ ".pdf.caseworker.snap_agency_id", cbv_flow.cbv_applicant.agency_id_number ]
+    ]
+  end
+
+  def self.applicant_fields_for(cbv_flow)
+    [
+      [ ".pdf.client.agency_id_number", cbv_flow.cbv_applicant.agency_id_number ]
     ]
   end
 end
