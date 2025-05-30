2480f10d7c171d796a2c7a0b854686572b87b110
Tom Dooner <tomdooner@navapbc.com>
2025-05-29 20:53:23 -0700

    Update copy on /payment_details, /summary, and PDF for AZ 6 months data
    * Add a helper, `report_data_range` that returns the text representing
      the duration of the employment objects in that report.
    * To handle cases where there may be both a W2 *and* Gig job (/summary,
      and the PDF), remove the data range copy since both "90 days" and "6
      months" will be wrong for one job if there is both a W2 and Gig job.

--- app/spec/controllers/cbv/summaries_controller_spec.rb
+++ app/spec/controllers/cbv/summaries_controller_spec.rb
@@ -71,7 +71,6 @@ RSpec.describe Cbv::SummariesController do
           doc = Nokogiri::HTML(response.body)
 
           expect(doc.css("title").text).to include("Review your income report")
-          expect(doc.at_xpath("//*[@data-testid=\"summary-description\"]").content).to include("from March 20, 2024 to June 18, 2024")
           expect(doc.at_xpath("//*[@data-testid=\"paystub-table-caption\"]").content).to include("Employer 1: Acme Corporation")
           expect(doc.at_xpath("//*[@data-testid=\"paystub-total-income\"]").content).to include("$4,807.20")
           expect(doc.at_xpath("//tr[@data-testid=\"paystub-row\"]").count).to eq(1)
@@ -90,7 +89,6 @@ RSpec.describe Cbv::SummariesController do
           doc = Nokogiri::HTML(response.body)
           expect(response).to be_successful
           expect(doc.css("title").text).to include("Review your income report")
-          expect(doc.at_xpath("//*[@data-testid=\"summary-description\"]").content).to include("from March 20, 2024 to June 18, 2024")
           expect(doc.at_xpath("//*[@data-testid=\"paystub-table-caption\"]").content).to include("Employer 1: Acme Corporation")
           expect(doc.at_xpath("//*[@data-testid=\"paystub-total-income\"]").content).to include("$9,614.40")
           expect(doc.at_xpath("//*[@data-testid=\"paystub-table\"]").css("td").count).to eq(2)
