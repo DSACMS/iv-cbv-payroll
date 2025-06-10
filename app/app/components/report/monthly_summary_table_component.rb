ba2b826d4b52056b9d6bae217338dfc4dcb73d11
Jeff Catania <Jeffrey.M.Catania@omb.eop.gov>
2025-06-09 20:04:22 -0400

    applied fix for missing paystubs and/or gigs in monthly summary table

--- app/app/components/report/monthly_summary_table_component.rb
+++ app/app/components/report/monthly_summary_table_component.rb
@@ -5,11 +5,31 @@ class Report::MonthlySummaryTableComponent < ViewComponent::Base
   attr_reader :employer_name
 
   def initialize(report, payroll_account)
+    @report = report
     # Note: payroll_account may either be the ID or the payroll_account object
     @account_id = payroll_account.class == String ? payroll_account : payroll_account.pinwheel_account_id
     account_report = report.find_account_report(@account_id)
     @paystubs = account_report&.paystubs
     @employer_name = account_report&.dig(:employment, :employer_name)
     @monthly_summary_data = report.summarize_by_month[@account_id]
+
+    @has_monthly_summary_results = @monthly_summary_data&.length == 0
+  end
+
+  def before_render
+    @report_data_range = report_data_range(@report)
+    @no_payments_found_content = I18n.t("cbv.payment_details.show.none_found", report_data_range: @report_data_range)
+  end
+
+  private
+
+  def format_accrued_gross_earnings(month_summary)
+    return I18n.t("shared.not_applicable") if month_summary[:paystubs].empty?
+    format_money(month_summary[:accrued_gross_earnings])
+  end
+
+  def format_total_gig_hours(month_summary)
+    return I18n.t("shared.not_applicable") if month_summary[:gigs].empty?
+    month_summary[:total_gig_hours]
   end
 end
--- app/app/components/table_cell_component.rb
+++ app/app/components/table_cell_component.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
 
 class TableCellComponent < ViewComponent::Base
-  def initialize(class_names: nil, is_header: false)
+  def initialize(class_names: nil, is_header: false, colspan: nil)
     @class_names = class_names
     @is_header = is_header
+    @colspan = colspan
   end
 
   def call
     tag_type = @is_header? :th : :td
-    content_tag tag_type, content, class: @class_names
+    content_tag tag_type, content, class: @class_names, colspan: @colspan
   end
 end
