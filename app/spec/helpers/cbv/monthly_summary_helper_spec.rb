5d83dc7ef74c74a97bdd5229bfbdd8e41283f450
Tom Dooner <tomdooner@navapbc.com>
2025-06-10 09:41:27 -0700

    Use `short` date format in monthly summary table (#737)

--- app/spec/helpers/cbv/monthly_summary_helper_spec.rb
+++ app/spec/helpers/cbv/monthly_summary_helper_spec.rb
@@ -57,7 +57,7 @@ RSpec.describe Cbv::MonthlySummaryHelper, type: :helper do
       partial_month_details = subject.partial_month_details(current_month, activity_dates, from_date, to_date)
       expect(partial_month_details).to an_object_eq_to({
                                                          is_partial_month: true,
-                                                         description: "(Partial month: from 1/4-1/31)",
+                                                         description: "(Partial month: from Jan 4-Jan 31)",
                                                          included_range_start: Date.parse("2025-01-04"),
                                                          included_range_end: Date.parse("2025-01-31")
                                                        })
@@ -72,7 +72,7 @@ RSpec.describe Cbv::MonthlySummaryHelper, type: :helper do
       partial_month_details = subject.partial_month_details(current_month, activity_dates, from_date, to_date)
       expect(partial_month_details).to an_object_eq_to({
                                                          is_partial_month: true,
-                                                         description: "(Partial month: from 3/1-3/6)",
+                                                         description: "(Partial month: from Mar 1-Mar 6)",
                                                          included_range_start: Date.parse("2025-03-01"),
                                                          included_range_end: Date.parse("2025-03-06")
                                                        })
@@ -87,7 +87,7 @@ RSpec.describe Cbv::MonthlySummaryHelper, type: :helper do
       partial_month_details = subject.partial_month_details(current_month, activity_dates, from_date, to_date)
       expect(partial_month_details).to an_object_eq_to({
                                                          is_partial_month: true,
-                                                         description: "(Partial month: from 3/4-3/6)",
+                                                         description: "(Partial month: from Mar 4-Mar 6)",
                                                          included_range_start: Date.parse("2025-03-04"),
                                                          included_range_end: Date.parse("2025-03-06")
                                                        })
@@ -117,7 +117,7 @@ RSpec.describe Cbv::MonthlySummaryHelper, type: :helper do
       partial_month_details = subject.partial_month_details(current_month, activity_dates, from_date, to_date)
       expect(partial_month_details).to an_object_eq_to({
                                                          is_partial_month: true,
-                                                         description: "(Partial month: from 2/28-2/28)",
+                                                         description: "(Partial month: from Feb 28-Feb 28)",
                                                          included_range_start: Date.parse("2025-02-28"),
                                                          included_range_end: Date.parse("2025-02-28")
                                                        })
