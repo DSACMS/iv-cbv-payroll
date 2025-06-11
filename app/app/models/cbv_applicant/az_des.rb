1fc44a8260441bc1be57b7180bb96d3910bf2fe4
George Byers <georgebyers@navapbc.com>
2025-06-11 14:52:30 -0400

    simplifying CbvApplicant::AzDes::redact! by calling super and then handling our custom field. @fields_to_redact seems to only be used within the redactable concern so this shouldn't be an issue

--- app/app/models/cbv_applicant/az_des.rb
+++ app/app/models/cbv_applicant/az_des.rb
@@ -11,26 +11,16 @@ class CbvApplicant::AzDes < CbvApplicant
   has_redactable_fields(
     first_name: :string,
     middle_name: :string,
-    last_name: :string,
-    income_changes: :object
+    last_name: :string
+    # Note: income_changes is handled separately in redact! method
   )
 
   validates :case_number, presence: true
 
   def redact!
-    fields_to_redact = self.class.fields_to_redact || self.class.superclass.fields_to_redact
-    raise "No fields to redact in #{self.class} (or its superclass)" unless fields_to_redact.present?
-
-    fields_to_redact.each do |field, type|
-      if field == :income_changes
-        # handle income_changes JSONB field specifically for AZ DES
-        self[field] = redact_member_names_in_json(self[field])
-      else
-        self[field] = Redactable::REDACTION_REPLACEMENTS[type]
-      end
-    end
-
-    self[Redactable::REDACTED_TIMESTAMP_COLUMN] = Time.now
+    super
+    
+    self[:income_changes] = redact_member_names_in_json(self[:income_changes])
     save(validate: false)
   end
 
