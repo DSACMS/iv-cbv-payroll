ef87c47171d937479a500a0eb878d8b41dc8648e
Daphne Gold <daphnegold@navapbc.com>
2025-06-12 11:14:08 -0700

    Generalize, add another test to make sure we don't get false

--- app/spec/services/aggregators/aggregator_reports/aggregator_report_spec.rb
+++ app/spec/services/aggregators/aggregator_reports/aggregator_report_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe Aggregators::AggregatorReports::AggregatorReport, type: :service 
   end
 
   describe '#summarize_by_employer' do
-    it "returns nil for employment when job succeeds but no data found" do
+    it "returns nil for income, employment & identity when job succeeds but no data found" do
       account_id = report.payroll_accounts.first.pinwheel_account_id
 
       allow(report.payroll_accounts.first).to receive(:job_succeeded?).with("income").and_return(false)
@@ -25,8 +25,24 @@ RSpec.describe Aggregators::AggregatorReports::AggregatorReport, type: :service 
       report.employments = []
 
       summary = report.summarize_by_employer
+      expect(summary[account_id][:income]).to be_nil
       expect(summary[account_id][:employment]).to be_nil
+      expect(summary[account_id][:identity]).to be_nil
       expect(summary[account_id][:has_employment_data]).to be_truthy
+    end
+
+    it "returns nil for income, employment & identity when job fails" do
+      account_id = report.payroll_accounts.first.pinwheel_account_id
+
+      allow(report.payroll_accounts.first).to receive(:job_succeeded?).with("income").and_return(false)
+      allow(report.payroll_accounts.first).to receive(:job_succeeded?).with("employment").and_return(false)
+      allow(report.payroll_accounts.first).to receive(:job_succeeded?).with("identity").and_return(false)
+
+      summary = report.summarize_by_employer
+      expect(summary[account_id][:income]).to be_nil
+      expect(summary[account_id][:employment]).to be_nil
+      expect(summary[account_id][:identity]).to be_nil
+      expect(summary[account_id][:has_employment_data]).to be_falsy
     end
   end
 end
