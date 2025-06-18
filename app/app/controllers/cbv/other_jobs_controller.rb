c2262e464e09cff89c096335988d0d554cf0670f
George Byers <georgebyers@navapbc.com>
2025-06-18 11:10:11 -0400

    cleaning up some comments

--- app/app/controllers/cbv/other_jobs_controller.rb
+++ app/app/controllers/cbv/other_jobs_controller.rb
@@ -1,11 +1,9 @@
 class Cbv::OtherJobsController < Cbv::BaseController
   def show
-    # Ensure has_other_jobs is nil so no radio button is pre-selected
     @cbv_flow.has_other_jobs = nil if @cbv_flow.has_other_jobs == false
   end
 
   def update
-    # Check if a value was selected
     if other_jobs_params[:has_other_jobs].blank?
       flash[:slim_alert] = { message: t("shared.next_path.notice_no_answer"), type: "error" }
       return redirect_to cbv_flow_other_job_path
