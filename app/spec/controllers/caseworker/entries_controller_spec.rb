2a1f5fc07d64265dc0038c28c3610913f39ff887
Daphne Gold <daphnegold@navapbc.com>
2025-06-26 13:38:04 -0700

    remove references left over

--- app/spec/controllers/caseworker/entries_controller_spec.rb
+++ app/spec/controllers/caseworker/entries_controller_spec.rb
@@ -8,7 +8,6 @@ RSpec.describe Caseworker::EntriesController do
 
     context "when state is ma" do
       it "should show ma specific copy with a link to /sso/ma" do
-        agency_short_name = client_agency_config["ma"].agency_short_name
         get :index, params: { client_agency_id: "ma" }
         expect(response).to redirect_to(root_url)
       end
@@ -20,11 +19,10 @@ RSpec.describe Caseworker::EntriesController do
       end
 
       it "should show nyc specific copy with a link to /sso/nyc" do
-        agency_short_name = client_agency_config["nyc"].agency_short_name
         get :index, params: { client_agency_id: "nyc" }
         expect(response).to be_successful
         unescaped_body = CGI.unescapeHTML(response.body)
-        expect(unescaped_body).to include(I18n.t("caseworker.entries.index.header.nyc", agency_short_name: agency_short_name))
+        expect(unescaped_body).to include(I18n.t("caseworker.entries.index.header.nyc"))
         expect(unescaped_body).to include("Log in with your LAN ID")
       end
     end
@@ -35,7 +33,6 @@ RSpec.describe Caseworker::EntriesController do
       end
 
       it "redirect to the root page" do
-        agency_short_name = client_agency_config["sandbox"].agency_short_name
         get :index, params: { client_agency_id: "sandbox" }
         expect(response).to redirect_to(root_url)
       end
