d9e2eb4c97996e536c3bed15b139660f858fe91d
Pat Sier <patrick.sier@cms.hhs.gov>
2025-07-02 09:26:23 -0400

    FFS-3031: Use TableComponent for applicant info table on /summary

--- app/spec/components/table_cell_component_spec.rb
+++ app/spec/components/table_cell_component_spec.rb
@@ -3,13 +3,42 @@
 require "rails_helper"
 
 RSpec.describe TableCellComponent, type: :component do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:result) { render_inline(described_class.new(**options)) { "Cell" } }
+  let(:options) { {} }
 
-  # it "renders something useful" do
-  #   expect(
-  #     render_inline(described_class.new(attr: "value")) { "Hello, components!" }.css("p").to_html
-  #   ).to include(
-  #     "Hello, components!"
-  #   )
-  # end
+  it "renders a default <td> with text" do
+    expect(result).to have_css(:td, text: "Cell")
+  end
+
+  context "with class_names" do
+    let(:options) { { class_names: "test" }}
+
+    it "adds class_names to the element" do
+      expect(result).to have_css("td.test")
+    end
+  end
+
+  context "when is_header is true" do
+    let(:options) { { is_header: true } }
+
+    it "renders a <th>" do
+      expect(result).to have_css("th", text: "Cell")
+    end
+  end
+
+  context "with data_label" do
+    let(:options) { { data_label: "test" } }
+
+    it "adds data-label attribute" do
+      expect(result).to have_css('td[data-label="test"]')
+    end
+  end
+
+  context "with arbitrary HTML attributes" do
+    let(:options) { { attributes: { "data-testid" => "test" } } }
+
+    it "includes them in the element" do
+      expect(result).to have_css('td[data-testid="test"]')
+    end
+  end
 end
