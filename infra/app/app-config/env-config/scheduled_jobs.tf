78c09da864165e39d7f5bae127e03fad040d97e4
Ian Norris <iannorris@Ians-MacBook-Air.local>
2025-07-08 13:29:25 -0700

    make edits to move certain jobs to solidqueue scheduler instead

--- infra/app/app-config/env-config/scheduled_jobs.tf
+++ infra/app/app-config/env-config/scheduled_jobs.tf
@@ -9,11 +9,6 @@ locals {
       schedule_expression = "cron(0 12 ? * MON *)" # Every Monday at 12pm UTC (7am EST / 8am EDT)
     }
 
-    send_newrelic_queue_metrics = {
-      task_command        = ["bin/rails", "telemetry:send_queue_metrics"]
-      schedule_expression = "cron(* * ? * * *)" # every minute
-    }
-
     send_az_reports = {
       task_command        = ["bin/rails", "az_des:deliver_csv_reports"]
       schedule_expression = "cron(0 15 ? * * *)" # Every day at 3pm UTC (8am MST / 10am EST / 9am EDT)
@@ -22,11 +17,6 @@ locals {
     redact_data = {
       task_command        = ["bin/rails", "data_deletion:redact_all"]
       schedule_expression = "cron(0 14 ? * * *)" # Every day at 2pm UTC (9am EST / 10am EDT)
-    }
-
-    clear_old_background_jobs = {
-      task_command        = ["bin/rails", "data_deletion:clear_old_background_jobs"]
-      schedule_expression = "cron(*/12 * * * * *)" # once every 12 minutes
     }
   }
 }
