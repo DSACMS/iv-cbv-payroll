26ed639e08a28f472e48301a3c0019d34aea5143
Daphne Gold <daphnegold@navapbc.com>
2025-07-22 14:31:28 -0700

    FFS-3082: Distinguish SMS and RFI links for LA users (#842)

--- app/spec/controllers/pages_controller_spec.rb
+++ app/spec/controllers/pages_controller_spec.rb
@@ -21,6 +21,18 @@ RSpec.describe PagesController do
         get :home
         expect(response).to redirect_to(cbv_flow_new_path(client_agency_id: "la_ldh"))
       end
+
+      it "defaults to sms source for LA when no source provided" do
+        request.host = "la.reportmyincome.org"
+        get :home
+        expect(session[:cbv_source]).to eq("sms")
+      end
+
+      it "uses provided source parameter when given" do
+        request.host = "la.reportmyincome.org"
+        get :home, params: { source: "mail" }
+        expect(session[:cbv_source]).to eq("mail")
+      end
     end
 
     context "when on an agency subdomain with an ended pilot" do
