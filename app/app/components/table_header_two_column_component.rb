2d019992c0b8be39e5812fa66c44928c39f38f43
Tim Miller <timmiller@navapbc.com>
2025-07-24 16:51:25 -0500

    fixing table header styles

--- app/app/components/table_header_two_column_component.rb
+++ app/app/components/table_header_two_column_component.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class TableHeaderTwoColumnComponent < ViewComponent::Base
-  def initialize(column1_title:, column2_title:)
+  def initialize(column1_title:, column2_title:, class_names:)
     @column1_title = column1_title
     @column2_title = column2_title
+    @class_names = class_names
   end
 end
