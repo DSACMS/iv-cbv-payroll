dd9f6a4cf973b47d0b855cf290b1c5c456bf9b50
Tim Miller <timmiller@navapbc.com>
2025-07-25 10:52:25 -0500

    corrects rspec

--- app/spec/components/table_header_two_column_component_spec.rb
+++ app/spec/components/table_header_two_column_component_spec.rb
@@ -4,7 +4,7 @@ require "rails_helper"
 
 RSpec.describe TableHeaderTwoColumnComponent, type: :component do
   it "renders content as expected" do
-    render_inline(described_class.new(column1_title: "column 1", column2_title: "column 2"))
+    render_inline(described_class.new(column1_title: "column 1", column2_title: "column 2", class_names: ""))
     expect(rendered_content).to have_css("th", text: "column 1")
     expect(rendered_content).to have_css("th", text: "column 2")
   end
