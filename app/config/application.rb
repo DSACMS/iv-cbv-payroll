211d41ff400cdf2b9838f64c779c5168fc6e56dc
jeffcatania-dpw <jeff@digitalpublicworks.org>
2025-08-14 16:32:00 -0400

    jeffffff

--- app/config/application.rb
+++ app/config/application.rb
@@ -67,5 +67,8 @@ module IvCbvPayroll
     config.active_record.encryption.primary_key = ENV["ACTIVE_RECORD_ENCRYPTION_PRIMARY_KEY"]
     config.active_record.encryption.deterministic_key = ENV["ACTIVE_RECORD_ENCRYPTION_DETERMINISTIC_KEY"]
     config.active_record.encryption.key_derivation_salt = ENV["ACTIVE_RECORD_ENCRYPTION_KEY_DERIVATION_SALT"]
+
+    print("jeffffff")
+    print(Rails.env)
   end
 end
