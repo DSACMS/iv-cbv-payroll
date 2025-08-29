6c3e01cf925ecef82c21e98e2abc076eb843f6f2
Tom Dooner <tomdooner@navapbc.com>
2025-08-28 17:22:45 -0700

    Add "success" to list of valid alert types

--- app/app/components/uswds/alert.rb
+++ app/app/components/uswds/alert.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Uswds::Alert < ViewComponent::Base
-  TYPES = %i[info warning error]
+  TYPES = %i[success info warning error]
 
   def initialize(type: :info, heading: nil, slim: false, **options)
     raise "Unsupported Alert type: #{type}" unless TYPES.include?(type.to_sym)
