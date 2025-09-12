1ec888e30bc7abbef1d2d8f678076acb235406b9
Andre Diggins <andrediggins@gmail.com>
2025-09-12 12:12:08 -0700

    checkov skip

--- infra/modules/domain/main.tf
+++ infra/modules/domain/main.tf
@@ -5,5 +5,5 @@
 resource "aws_route53_zone" "zone" {
   count = var.manage_dns ? 1 : 0
   name  = var.name
+  # checkov:skip=CKV2_AWS_38: TODO: dnssec signing
 }
-# checkov:skip=CKV2_AWS_38: TODO: dnssec signing
