004afed76df1d43b65c0ce5297ebf475017ea982
Ben Calegari <bencalegari@navapbc.com>
2025-09-12 10:35:38 -0700

    Rspec nits

--- app/spec/generic_event_tracker_spec.rb
+++ app/spec/generic_event_tracker_spec.rb
@@ -25,19 +25,19 @@ RSpec.describe GenericEventTracker do
 
     context 'raises an error' do
       let(:request_mock) { instance_double(ActionDispatch::Request, params: { "client_agency_id" => "client_agency_id" }, session: { cbv_flow_id: "cbv_flow_id" }, remote_ip: "ip", headers: { "User-Agent" => "user_agent" }) }
+      let(:event_type) { "myEvent" }
 
       before do
-        @event_type = "myEvent"
         allow(EventTrackingJob).to receive(:perform_later).and_raise(RuntimeError)
         allow(Rails.logger).to receive(:error)
         allow(Rails.env).to receive(:production?).and_return(false)
       end
 
       it 'logs to error with that event and reraises an error in non prod environments' do
-        expect { described_class.new.track(@event_type, request_mock, {}) }
+        expect { described_class.new.track(event_type, request_mock, {}) }
           .to raise_error(RuntimeError)
         expect(Rails.logger).to have_received(:error)
-          .with(/Unable to track event \(#{@event_type}\): RuntimeError, line: /)
+          .with(/Unable to track event \(#{event_type}\): RuntimeError, line: /)
           .with(/line: /)
           .with(/.rb:/)
       end
@@ -46,9 +46,9 @@ RSpec.describe GenericEventTracker do
         allow(Rails.env).to receive(:production?).and_return(true)
 
         request_mock = instance_double(ActionDispatch::Request, params: { "client_agency_id" => "client_agency_id" }, session: { cbv_flow_id: "cbv_flow_id" }, remote_ip: "ip", headers: { "User-Agent" => "user_agent" })
-        expect { described_class.new.track(@event_type, request_mock, {}) }
+        expect { described_class.new.track(event_type, request_mock, {}) }
           .not_to raise_error
-        expect(Rails.logger).to have_received(:error).with(/Unable to track event \(#{@event_type}\): RuntimeError/)
+        expect(Rails.logger).to have_received(:error).with(/Unable to track event \(#{event_type}\): RuntimeError/)
       end
     end
   end
