048456b966466940add7cac0a8dcd94b890e9050
Ben Calegari <bencalegari@navapbc.com>
2025-09-18 12:16:57 -0700

    Remove unnecessary test setup

--- app/spec/jobs/application_job_spec.rb
+++ app/spec/jobs/application_job_spec.rb
@@ -1,8 +1,6 @@
 require "rails_helper"
 
 RSpec.describe ApplicationJob do
-  include ActiveJob::TestHelper
-
   class TestJob < ApplicationJob
     def perform
       raise "failed"
@@ -10,10 +8,7 @@ RSpec.describe ApplicationJob do
   end
 
   it "records to newrelic" do
-    allow_any_instance_of(TestJob).to receive(:perform).and_raise(Exception.new)
     expect(NewRelic::Agent).to receive(:record_custom_event).with("SolidQueueJobFailed", anything)
-    perform_enqueued_jobs do
-      expect { TestJob.perform_now }.to raise_error(Exception)
-    end
+    expect { TestJob.perform_now }.to raise_error("failed")
   end
 end
