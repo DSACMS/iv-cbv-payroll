f149aea993d44bd7811c8ae0b1da73be33ed1e04
Daphne Gold <daphnegold@navapbc.com>
2025-09-25 09:18:07 -0700

    FFS-3255: Investigate Argyle controller error looking up a cbv_flow without an id (#962)

--- app/app/controllers/api/pinwheel_controller.rb
+++ app/app/controllers/api/pinwheel_controller.rb
@@ -3,7 +3,9 @@ class Api::PinwheelController < ApplicationController
 
   # run the token here with the included employer/payroll provider id
   def create_token
-    @cbv_flow = CbvFlow.find(session[:cbv_flow_id])
+    @cbv_flow = CbvFlow.find_by(id: session[:cbv_flow_id])
+    return redirect_to(root_url(cbv_flow_timeout: true)) unless @cbv_flow
+
     pinwheel = pinwheel_for(@cbv_flow)
     token_response = pinwheel.create_link_token(
       response_type: token_params[:response_type],
@@ -23,6 +25,8 @@ class Api::PinwheelController < ApplicationController
   end
 
   def track_event
+    return unless @cbv_flow.present?
+
     event_logger.track(TrackEvent::ApplicantBeganLinkingEmployer, request, {
       time: Time.now.to_i,
       cbv_flow_id: @cbv_flow.id,
