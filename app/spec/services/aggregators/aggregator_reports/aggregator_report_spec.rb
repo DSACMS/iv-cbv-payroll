2c4880c50bfb9e88aa1a69f15050f078c6c9845b
Ben Calegari <bencalegari@navapbc.com>
2025-09-30 16:09:19 -0700

    Review fixes

--- app/spec/services/aggregators/aggregator_reports/aggregator_report_spec.rb
+++ app/spec/services/aggregators/aggregator_reports/aggregator_report_spec.rb
@@ -123,100 +123,44 @@ RSpec.describe Aggregators::AggregatorReports::AggregatorReport, type: :service 
   describe '#income_report' do
     let(:comment) { "cool stuff" }
     let(:cbv_flow) { create(:cbv_flow, has_other_jobs: false, additional_information: { comment: comment }) }
-    let(:account_name) { "account1" }
-    let(:full_name) { "Cool Guy" }
-    let(:ssn) { "XXX-XX-1234" }
-    let(:employer_name) { "Cool Company" }
-    let(:employment_start_date) { "2020-01-01" }
-    let(:employment_end_date) { "2020-01-02" }
-    let(:employment_status) { "inactive" }
-    let(:employer_phone_number) { "604-555-1234" }
-    let(:employer_address) { "1234 Main St Vancouver BC V5K 0A1" }
-    let(:employment_type) { :gig }
-    let(:employment_start_date) { Date.new(2014, 1, 1).iso8601 }
-    let(:employment_end_date) { Date.new(2014, 1, 2).iso8601 }
-    let(:pay_frequency) { "variable" }
-    let(:compensation_amount) { 100 }
-    let(:compensation_unit) { "hour" }
-    let(:pay_period_start) { Date.new(2014, 1, 1) }
-    let(:pay_date) { pay_period_start.iso8601 }
-    let(:pay_period_end) { Date.new(2014, 1, 2) }
-    let(:pay) { 12345 }
-    let(:hours_paid) { 12.0 }
-    let(:report) {
-      build(:pinwheel_report, :with_pinwheel_account,
-        identities: [
-          Aggregators::ResponseObjects::Identity.new(
-            account_id: account_name,
-            full_name: full_name,
-            ssn: ssn,
-          )
-        ],
-        employments: [
-          Aggregators::ResponseObjects::Employment.new(
-            account_id: account_name,
-            employer_name: employer_name,
-            start_date: employment_start_date,
-            termination_date: employment_end_date,
-            status: employment_status,
-            employment_type: employment_type,
-            account_source: "pinwheel_payroll_provider",
-            employer_phone_number: employer_phone_number,
-            employer_address: employer_address,
-          )
-        ],
-        incomes: [
-          Aggregators::ResponseObjects::Income.new(
-            account_id: account_name,
-            pay_frequency: pay_frequency,
-            compensation_amount: compensation_amount,
-            compensation_unit: compensation_unit
-          )
-        ],
-        paystubs: [
-          Aggregators::ResponseObjects::Paystub.new(
-            account_id: account_name,
-            gross_pay_amount: pay,
-            net_pay_amount: pay,
-            gross_pay_ytd: pay,
-            pay_period_start: pay_period_start,
-            pay_period_end: pay_period_end,
-            pay_date: pay_date,
-            hours: hours_paid
-          )
-        ],
-      ) }
+    let(:report) { build(:pinwheel_report, :hydrated, :with_pinwheel_account) }
 
     before do
       report.payroll_accounts.first.cbv_flow = cbv_flow
     end
 
     it 'income information' do
-      expect(report.income_report[:has_other_jobs]).to eq(false)
-      expect(report.income_report[:employments].length).to eq(1)
-      employment = report.income_report[:employments].first
-      expect(employment[:applicant_full_name]).to eq full_name
-      expect(employment[:applicant_ssn]).to eq ssn
-      expect(employment[:applicant_extra_comments]).to eq comment
-      expect(employment[:employer_name]).to eq employer_name
-      expect(employment[:employer_phone]).to eq employer_phone_number
-      expect(employment[:employer_address]).to eq employer_address
-      expect(employment[:employment_status]).to eq employment_status
-      expect(employment[:employment_type]).to eq employment_type
-      expect(employment[:employment_start_date]).to eq employment_start_date
-      expect(employment[:employment_end_date]).to eq employment_end_date
-      expect(employment[:pay_frequency]).to eq pay_frequency
-      expect(employment[:compensation_amount]).to eq compensation_amount
-      expect(employment[:compensation_unit]).to eq compensation_unit
-
-      paystub_1 = employment[:paystubs].first
-      expect(paystub_1[:pay_date]).to eq pay_date
-      expect(paystub_1[:pay_period_start]).to eq pay_period_start
-      expect(paystub_1[:pay_period_end]).to eq pay_period_end
-      expect(paystub_1[:pay_gross]).to eq pay
-      expect(paystub_1[:pay_gross_ytd]).to eq pay
-      expect(paystub_1[:pay_net]).to eq pay
-      expect(paystub_1[:hours_paid]).to eq hours_paid
+      expect(report.income_report).to eq(
+        has_other_jobs: false,
+        employments: [
+          {
+            applicant_full_name: "Cool Guy",
+            applicant_ssn: "XXX-XX-1234",
+            applicant_extra_comments: "cool stuff",
+            employer_name: "Cool Company",
+            employer_phone: "604-555-1234",
+            employer_address: "1234 Main St Vancouver BC V5K 0A1",
+            employment_status: "inactive",
+            employment_type: "gig",
+            employment_start_date: Date.new(2014, 1, 1).iso8601,
+            employment_end_date: Date.new(2014, 1, 2).iso8601,
+            pay_frequency: "variable",
+            compensation_amount: 100,
+            compensation_unit: "hour",
+            paystubs: [
+              {
+                pay_date: Date.new(2014, 1, 1).iso8601,
+                pay_period_start: Date.new(2014, 1, 1),
+                pay_period_end: Date.new(2014, 1, 2),
+                pay_gross: 12345,
+                pay_gross_ytd: 12345,
+                pay_net: 12345,
+                hours_paid: 12.0
+              }
+            ]
+          }
+        ]
+      )
     end
   end
 end
