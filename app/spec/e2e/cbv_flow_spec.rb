1a1bd9aa421008329682e0171194f9131c0136f6
Tim Miller <timmiller@navapbc.com>
2025-10-03 16:31:14 -0500

    experimental fix for e2e tests

--- app/spec/e2e/cbv_flow_spec.rb
+++ app/spec/e2e/cbv_flow_spec.rb
@@ -19,7 +19,7 @@ RSpec.describe "e2e CBV flow test", type: :feature, js: true do
     visit URI(root_url).request_uri
     visit URI(cbv_flow_invitation.to_url).request_uri
     verify_page(page, title: I18n.t("cbv.entries.show.header"))
-    find("label", text: I18n.t("cbv.entries.show.checkbox.default", agency_full_name: I18n.t("shared.agency_full_name.sandbox"))).click
+    find('[data-cbv-entry-page-target="consentCheckbox"]').click
     click_button I18n.t("cbv.entries.show.continue")
 
     # /cbv/employer_search
