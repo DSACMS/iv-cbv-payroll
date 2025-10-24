bde2a0adf235182cdcfbafb3cbaebd04cee7d18a
jeffcatania-dpw <jeff@digitalpublicworks.org>
2025-10-24 12:01:34 -0400

    did some simple cleanup and added documentation

--- app/app/components/report/w2_paystub_details_table_component.rb
+++ app/app/components/report/w2_paystub_details_table_component.rb
@@ -3,28 +3,32 @@
 class Report::W2PaystubDetailsTableComponent < ViewComponent::Base
   include ReportViewHelper
 
+  # @param paystub [Aggregators::ResponseObjects::Paystub] The paystub data to display
+  # @param income [Aggregators::ResponseObjects::Income, nil] Income object containing pay frequency info
+  # @param is_caseworker [Boolean] If true, highlights certain fields for caseworker view
+  # @param is_responsive [Boolean] If true, table adapts to mobile screens.  If false, does not (e.g. PDF)
+  # @param is_personalized [Boolean] If true, uses "Your details" header; if false, uses "Details"
+  # @param show_hours_breakdown [Boolean] If true, shows hours by earning category (Regular, Commission, etc.)
+  # @param show_gross_pay_ytd [Boolean] If true, shows gross pay year-to-date (only if value > 0)
+  # @param show_pay_frequency [Boolean] If true, shows pay period with frequency
   def initialize(
     paystub,
     income: nil,
-    employer_name: nil,
     is_caseworker: false,
     is_responsive: true,
+    is_personalized: false,
     show_hours_breakdown: true,
     show_gross_pay_ytd: true,
-    show_pay_frequency: true,
-    is_personalized: false,
-    pay_frequency_text: nil
+    show_pay_frequency: true
   )
     @paystub = paystub
     @income = income
-    @employer_name = employer_name
     @is_caseworker = is_caseworker
     @is_responsive = is_responsive
+    @is_personalized = is_personalized
     @show_hours_breakdown = show_hours_breakdown
     @show_gross_pay_ytd = show_gross_pay_ytd
     @show_pay_frequency = show_pay_frequency
-    @is_personalized = is_personalized
-    @custom_pay_frequency_text = pay_frequency_text
   end
 
   private
@@ -38,8 +42,6 @@ class Report::W2PaystubDetailsTableComponent < ViewComponent::Base
   end
 
   def pay_frequency_text
-    return @custom_pay_frequency_text if @custom_pay_frequency_text
-
     if @income&.pay_frequency
       @income.pay_frequency&.humanize
     else
@@ -48,14 +50,7 @@ class Report::W2PaystubDetailsTableComponent < ViewComponent::Base
   end
 
   def show_gross_pay_ytd?
-    case @show_gross_pay_ytd
-    when :if_positive
-      @paystub.gross_pay_ytd.to_f > 0
-    when false
-      false
-    else
-      true
-    end
+    @show_gross_pay_ytd && @paystub.gross_pay_ytd.to_f > 0
   end
 
   def show_pay_frequency?
