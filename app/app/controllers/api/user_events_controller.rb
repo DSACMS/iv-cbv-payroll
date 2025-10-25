f3f48389aad7d426656f4de1f51df2314e831e7a
Ben Calegari <bencalegari@navapbc.com>
2025-10-24 17:40:03 -0700

    Unify events under TrackEvent

--- app/app/controllers/api/user_events_controller.rb
+++ app/app/controllers/api/user_events_controller.rb
@@ -1,44 +1,4 @@
 class Api::UserEventsController < ApplicationController
-  EVENT_NAMES = %w[
-    ApplicantOpenedHelpModal
-    ApplicantCopiedInvitationLink
-    ApplicantSelectedEmployerOrPlatformItem
-    ApplicantAttemptedClosingPinwheelModal
-    ApplicantAttemptedPinwheelLogin
-    ApplicantClosedPinwheelModal
-    ApplicantEncounteredPinwheelError
-    ApplicantViewedPinwheelDefaultProviderSearch
-    ApplicantViewedPinwheelLoginPage
-    ApplicantViewedPinwheelProviderConfirmation
-    ApplicantSucceededWithPinwheelLogin
-    ApplicantSucceededWithArgyleLogin
-    ApplicantCreatedArgyleAccount
-    ApplicantEncounteredArgyleAccountError
-    ApplicantRemovedArgyleAccount
-    ApplicantClosedArgyleModal
-    ApplicantEncounteredArgyleError
-    ApplicantEncounteredArgyleTokenExpired
-    ApplicantEncounteredModalAdapterError
-    ApplicantViewedArgyleProviderConfirmation
-    ApplicantViewedArgyleLoginPage
-    ApplicantAttemptedArgyleLogin
-    ApplicantViewedArgyleDefaultProviderSearch
-    ApplicantAccessedArgyleModalMFAScreen
-    ApplicantEncounteredArgyleInvalidCredentialsLoginError
-    ApplicantEncounteredArgyleAuthRequiredLoginError
-    ApplicantEncounteredArgyleConnectionUnavailableLoginError
-    ApplicantEncounteredArgyleExpiredCredentialsLoginError
-    ApplicantEncounteredArgyleInvalidAuthLoginError
-    ApplicantEncounteredArgyleMfaCanceledLoginError
-    ApplicantUpdatedArgyleSearchTerm
-    ApplicantManuallySwitchedLanguage
-    ApplicantConsentedToTerms
-    ApplicantViewedHelpText
-    ApplicantWarnedAboutTimeout
-    ApplicantExtendedSession
-    ApplicantTimedOut
-  ]
-
   def user_action
     base_attributes = {
       time: Time.now.to_i
@@ -58,7 +18,7 @@ class Api::UserEventsController < ApplicationController
     event_attributes = (user_action_params[:attributes] || {}).merge(base_attributes)
     event_name = user_action_params[:event_name]
 
-    if EVENT_NAMES.include?(event_name)
+    if TrackEvent.constants.map(&:to_s).include?(event_name)
       event_logger.track(
         event_name,
         request,
