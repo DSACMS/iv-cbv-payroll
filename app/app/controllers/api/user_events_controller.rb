271e5a155d854d8efce992da42a6dad53769a956
Tim Miller <timmiller@navapbc.com>
2025-11-06 09:19:37 -0600

    two more device_id additions

--- app/app/controllers/api/user_events_controller.rb
+++ app/app/controllers/api/user_events_controller.rb
@@ -11,6 +11,7 @@ class Api::UserEventsController < ApplicationController
         cbv_flow_id: @cbv_flow.id,
         cbv_applicant_id: @cbv_flow.cbv_applicant_id,
         client_agency_id: @cbv_flow.client_agency_id,
+        device_id: @cbv_flow.device_id,
         invitation_id: @cbv_flow.cbv_flow_invitation_id
       })
     end
