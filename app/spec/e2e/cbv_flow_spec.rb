0d6b92fdad5575d18a737a1b0c6da3444840b80a
Jake Shilling <shilling.jake@gmail.com>
2025-11-13 09:34:05 -0500

    Use VRC recording instead of mocking service class

--- app/spec/e2e/cbv_flow_spec.rb
+++ app/spec/e2e/cbv_flow_spec.rb
@@ -12,26 +12,6 @@ RSpec.describe "e2e CBV flow test", type: :feature, js: true do
     end
   end
 
-  it "completes the flow for a missing employer" do
-    employer = build(:search_result)
-    # /cbv/entry
-    visit URI(root_url).request_uri
-    visit URI(cbv_flow_invitation.to_url).request_uri
-    verify_page(page, title: I18n.t("cbv.entries.show.header", agency_full_name: I18n.t("shared.agency_full_name.sandbox")))
-    find('[data-cbv-entry-page-target="consentCheckbox"]').click
-    click_button I18n.t("cbv.entries.show.continue")
-
-    # /cbv/employer_search
-    allow_any_instance_of(ProviderSearchService).to receive(:search).and_return([ employer ])
-
-    verify_page(page, title: I18n.t("cbv.employer_searches.show.header"), wait: 10)
-    find('.usa-input[type="search"]').fill_in with: "blahblahblah"
-    click_button I18n.t("cbv.employer_searches.show.search")
-
-    click_link I18n.t("cbv.employer_searches.show.can_not_find_employer")
-    verify_page(page, title: I18n.t("cbv.missing_results.show.header", agency_full_name: I18n.t("shared.agency_full_name.sandbox")))
-  end
-
   it "completes the flow for an Argyle employer" do
     raise "Argyle not in supported_providers!" unless Rails.application.config.supported_providers.include?(:argyle)
 
