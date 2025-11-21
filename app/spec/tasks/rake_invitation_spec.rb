f2c4275793ba009d7897c79e0fcaa1c63443c0b3
Ben Calegari <bencalegari@navapbc.com>
2025-11-21 09:36:27 -0800

    Not a feature test

--- app/spec/tasks/rake_invitation_spec.rb
+++ app/spec/tasks/rake_invitation_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe InvitationGenerator, type: :feature do
+RSpec.describe InvitationGenerator do
   it 'creates an invitation link for the given client agency id' do
     expect { Rake::Task["invitation:create"].execute(client_agency_id: 'la_ldh') }.to change(CbvFlowInvitation, :count).by(1)
   end
