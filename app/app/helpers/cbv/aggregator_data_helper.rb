1aff07a46c74a3f8ebf755affab5089da60c1b35
Tom Dooner <tomdooner@navapbc.com>
2025-11-24 16:14:02 -0800

    Refactor `AggregatorDataHelper` logic into service object
    The `Cbv::AggregatorDataHelper` has been a thorn in our side for a
    while: it has become the standard way to instantiate an AggregatorReport
    class and therefore has been mixed into seven places.
    
    That's not optimal because the AggregatorDataHelper operates via side
    effects - it requires an `agency_config` and `@cbv_flow` to be defined
    wherever it's mixed in, and it does its work by setting the
    `@aggregator_report` instance variable.
    
    This commit is a straight-up relocation of its logic into a service
    object, to make it easier to instantiate reports in future locations.

--- app/app/helpers/cbv/aggregator_data_helper.rb
+++ app/app/helpers/cbv/aggregator_data_helper.rb
@@ -1,72 +1,13 @@
 module Cbv::AggregatorDataHelper
   include ReportViewHelper
-  include Aggregators::AggregatorReports
 
   def set_aggregator_report
-    if has_payroll_accounts("pinwheel") && has_payroll_accounts("argyle")
-      @aggregator_report = CompositeReport.new(
-        [ make_pinwheel_report, make_argyle_report ],
-        days_to_fetch_for_w2: agency_config[@cbv_flow.client_agency_id].pay_income_days[:w2],
-        days_to_fetch_for_gig: agency_config[@cbv_flow.client_agency_id].pay_income_days[:gig]
-      )
-    elsif has_payroll_accounts("pinwheel")
-      @aggregator_report = make_pinwheel_report
-    elsif has_payroll_accounts("argyle")
-      @aggregator_report = make_argyle_report
-    else
-      Rails.logger.error "no reports found for #{@cbv_flow.id}"
-    end
+    fetcher = AggregatorReportFetcher.new(@cbv_flow)
+    @aggregator_report = fetcher.report
   end
 
   def set_aggregator_report_for_account(payroll_account)
-    if payroll_account.type == "pinwheel"
-      @aggregator_report = make_pinwheel_report(payroll_account: payroll_account)
-    elsif payroll_account.type == "argyle"
-      @aggregator_report = make_argyle_report(payroll_account: payroll_account)
-    else
-      raise "Unidentified aggregator type"
-    end
-  end
-
-  def filter_payroll_accounts(aggregator)
-    @cbv_flow.payroll_accounts.filter do |payroll_account|
-      payroll_account.type == aggregator && payroll_account.sync_succeeded?
-    end
-  end
-
-  def has_payroll_accounts(aggregator)
-    filter_payroll_accounts(aggregator).length > 0
-  end
-
-  def make_pinwheel_report(payroll_account: nil)
-    report = PinwheelReport.new(
-      payroll_accounts: if payroll_account.present? then [ payroll_account ] else filter_payroll_accounts("pinwheel") end,
-      pinwheel_service: pinwheel,
-      days_to_fetch_for_w2: agency_config[@cbv_flow.client_agency_id].pay_income_days[:w2],
-      days_to_fetch_for_gig: agency_config[@cbv_flow.client_agency_id].pay_income_days[:gig]
-    )
-    report.fetch
-    report
-  end
-
-  def make_argyle_report(payroll_account: nil)
-    report = ArgyleReport.new(
-      payroll_accounts: if payroll_account.present? then [ payroll_account ] else filter_payroll_accounts("argyle") end,
-      argyle_service: argyle,
-      days_to_fetch_for_w2: agency_config[@cbv_flow.client_agency_id].pay_income_days[:w2],
-      days_to_fetch_for_gig: agency_config[@cbv_flow.client_agency_id].pay_income_days[:gig]
-    )
-    report.fetch
-    report
-  end
-
-  def pinwheel
-    environment = agency_config[@cbv_flow.client_agency_id].pinwheel_environment
-    Aggregators::Sdk::PinwheelService.new(environment)
-  end
-
-  def argyle
-    environment = agency_config[@cbv_flow.client_agency_id].argyle_environment
-    Aggregators::Sdk::ArgyleService.new(environment)
+    fetcher = AggregatorReportFetcher.new(@cbv_flow)
+    @aggregator_report = fetcher.report_for_payroll_account(payroll_account)
   end
 end
---
+++ app/app/services/aggregator_report_fetcher.rb
@@ -0,0 +1,78 @@
+# Instantiate and fetch the `AggregatorReports` subclass for a given CbvFlow.
+class AggregatorReportFetcher
+  def initialize(cbv_flow, agency_config: Rails.application.config.client_agencies)
+    @cbv_flow = cbv_flow
+    @agency_config = agency_config
+  end
+
+  def report
+    if has_payroll_accounts("pinwheel") && has_payroll_accounts("argyle")
+      Aggregators::AggregatorReports::CompositeReport.new(
+        [ make_pinwheel_report, make_argyle_report ],
+        days_to_fetch_for_w2: @agency_config[@cbv_flow.client_agency_id].pay_income_days[:w2],
+        days_to_fetch_for_gig: @agency_config[@cbv_flow.client_agency_id].pay_income_days[:gig]
+      )
+    elsif has_payroll_accounts("pinwheel")
+      make_pinwheel_report
+    elsif has_payroll_accounts("argyle")
+      make_argyle_report
+    else
+      Rails.logger.error "no reports found for #{@cbv_flow.id}"
+      nil
+    end
+  end
+
+  def report_for_payroll_account(payroll_account)
+    if payroll_account.type == "pinwheel"
+      make_pinwheel_report(payroll_account: payroll_account)
+    elsif payroll_account.type == "argyle"
+      make_argyle_report(payroll_account: payroll_account)
+    else
+      raise "Unidentified aggregator type"
+    end
+  end
+
+  private
+
+  def has_payroll_accounts(aggregator)
+    filter_payroll_accounts(aggregator).length > 0
+  end
+
+  def make_pinwheel_report(payroll_account: nil)
+    report = Aggregators::AggregatorReports::PinwheelReport.new(
+      payroll_accounts: if payroll_account.present? then [ payroll_account ] else filter_payroll_accounts("pinwheel") end,
+      pinwheel_service: pinwheel,
+      days_to_fetch_for_w2: @agency_config[@cbv_flow.client_agency_id].pay_income_days[:w2],
+      days_to_fetch_for_gig: @agency_config[@cbv_flow.client_agency_id].pay_income_days[:gig]
+    )
+    report.fetch
+    report
+  end
+
+  def make_argyle_report(payroll_account: nil)
+    report = Aggregators::AggregatorReports::ArgyleReport.new(
+      payroll_accounts: if payroll_account.present? then [ payroll_account ] else filter_payroll_accounts("argyle") end,
+      argyle_service: argyle,
+      days_to_fetch_for_w2: @agency_config[@cbv_flow.client_agency_id].pay_income_days[:w2],
+      days_to_fetch_for_gig: @agency_config[@cbv_flow.client_agency_id].pay_income_days[:gig]
+    )
+    report.fetch
+    report
+  end
+
+  def pinwheel
+    environment = @agency_config[@cbv_flow.client_agency_id].pinwheel_environment
+    Aggregators::Sdk::PinwheelService.new(environment)
+  end
+
+  def argyle
+    environment = @agency_config[@cbv_flow.client_agency_id].argyle_environment
+    Aggregators::Sdk::ArgyleService.new(environment)
+  end
+
+  def filter_payroll_accounts(aggregator)
+    @cbv_flow.payroll_accounts.filter do |payroll_account|
+      payroll_account.type == aggregator && payroll_account.sync_succeeded?
+    end
+  end
+end
