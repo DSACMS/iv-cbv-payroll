fc81132e599316cf06148bb8c2d9ca439c1bdec9
Jake Shilling <jake.shilling@cms.hhs.gov>
2025-11-26 14:47:39 -0500

    Abstract steps into service class

--- app/app/controllers/activities/education_controller.rb
+++ app/app/controllers/activities/education_controller.rb
@@ -1,33 +1,46 @@
 class Activities::EducationController < ApplicationController
   include ActionController::Live
 
-  class EducationState
-    attr_accessor :school, :enrollment, :hours
-
-    def initialize
-      @school = :in_progress
-      @enrollment = :in_progress
-      @hours = :in_progress
-    end
-  end
-
   def index
-    @state = EducationState.new
+    @stream_path = activities_flow_education_stream_path(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      date_of_birth: params[:date_of_birth]
+    )
   end
 
   def start
+    service = EducationService.new
+
+    first_name = params[:first_name]
+    last_name = params[:last_name]
+    date_of_birth = params[:date_of_birth]
+
     response.headers["Content-Type"] = "text/event-stream"
     response.headers["Last-Modified"] = Time.now.httpdate
 
     sse = SSE.new(response.stream, event: "message")
-
-    sleep 2
+    service.search_for_schools(
+      first_name: first_name,
+      last_name: last_name,
+      date_of_birth: date_of_birth
+    )
     sse.write("succeeded", event: "school")
-    sleep 2
+
+    service.search_for_schools(
+      first_name: first_name,
+      last_name: last_name,
+      date_of_birth: date_of_birth
+    )
     sse.write("succeeded", event: "enrollment")
-    sleep 2
+
+    service.search_for_schools(
+      first_name: first_name,
+      last_name: last_name,
+      date_of_birth: date_of_birth
+    )
     sse.write("succeeded", event: "hours")
   ensure
-    sse.close
+    sse.close if sse
   end
 end
