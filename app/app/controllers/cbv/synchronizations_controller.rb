ca17c49796b9fa4ea41fc71ee71752eeea65c1a7
Tom Dooner <tomdooner@navapbc.com>
2025-11-26 12:14:05 -0800

    FFS-3501: Handle session reinitialization on /synchronizations (#1136)
    If the user clicks the Generic or Tokenized Link in a new tab, while
    waiting on the /synchronizations tab, we want the page to go to
    /synchronization_failures rather than crashing.
    
    This adds a before_action to do that.

--- app/app/controllers/cbv/synchronizations_controller.rb
+++ app/app/controllers/cbv/synchronizations_controller.rb
@@ -1,5 +1,6 @@
 class Cbv::SynchronizationsController < Cbv::BaseController
   before_action :set_payroll_account, only: %i[show update]
+  before_action :redirect_if_session_was_reset, only: %i[update]
   before_action :redirect_if_sync_finished, only: %i[show]
   skip_before_action :capture_page_view, only: %i[update]
 
@@ -23,15 +24,29 @@ class Cbv::SynchronizationsController < Cbv::BaseController
 
   private
 
+  def set_payroll_account
+    account_id = params[:user][:account_id]
+
+    @payroll_account = @cbv_flow.payroll_accounts.find_by(aggregator_account_id: account_id)
+  end
+
   def redirect_if_sync_finished
     if @payroll_account&.has_fully_synced?
       redirect_to cbv_flow_payment_details_path(user: { account_id: @payroll_account.aggregator_account_id })
     end
   end
 
-  def set_payroll_account
-    account_id = params[:user][:account_id]
+  # Redirect to /synchronization_failures if the Payroll Account actually
+  # exists, but is associated with a different CbvFlow (as would happen if the
+  # user started a CbvFlow in a new tab).
+  def redirect_if_session_was_reset
+    return if @payroll_account.present?
 
-    @payroll_account = @cbv_flow.payroll_accounts.find_by(aggregator_account_id: account_id)
+    payroll_account_for_other_flow = PayrollAccount
+      .where(aggregator_account_id: params[:user][:account_id])
+      .where.not(cbv_flow: @cbv_flow)
+    return unless payroll_account_for_other_flow.exists?
+
+    render turbo_stream: turbo_stream.action(:redirect, cbv_flow_synchronization_failures_path)
   end
 end
