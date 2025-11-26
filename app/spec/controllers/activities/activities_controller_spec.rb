01a2dc9f365b495c098df97219754cb1d9dbc9bb
Tom Dooner <tomdooner@navapbc.com>
2025-11-26 11:33:42 -0800

    FFS-3537: Add skeleton for job training activities
    * Create JobTrainingActivity model, view, and controller
    * Update Activity hub to have sections for different types of activities
    * Test it via controller and E2E test

--- app/spec/controllers/activities/activities_controller_spec.rb
+++ app/spec/controllers/activities/activities_controller_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Activities::ActivitiesController, type: :controller do
       flow = create(:activity_flow)
       other_flow = create(:activity_flow)
 
-      visible_activity = flow.volunteering_activities.create!(
+      visible_volunteering = flow.volunteering_activities.create!(
         organization_name: "Scoped",
         hours: 1,
         date: Date.new(2000, 1, 1)
@@ -16,12 +16,23 @@ RSpec.describe Activities::ActivitiesController, type: :controller do
         hours: 2,
         date: Date.new(2000, 2, 2)
       )
+      visible_job_training = flow.job_training_activities.create!(
+        program_name: "Resume Workshop",
+        organization_address: "123 Main St",
+        hours: 6
+      )
+      other_flow.job_training_activities.create!(
+        program_name: "Other Workshop",
+        organization_address: "456 Elm St",
+        hours: 8
+      )
 
       session[:activity_flow_id] = flow.id
 
       get :show
 
-      expect(assigns(:activities)).to match_array([ visible_activity ])
+      expect(assigns(:volunteering_activities)).to match_array([ visible_volunteering ])
+      expect(assigns(:job_training_activities)).to match_array([ visible_job_training ])
     end
   end
 end
