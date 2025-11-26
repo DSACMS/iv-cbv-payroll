69e2841c1d91fa8be42068aae78dd8128b512146
Jake Shilling <jake.shilling@cms.hhs.gov>
2025-11-26 14:47:39 -0500

    Abstract steps into service class

---
+++ app/app/services/education_service.rb
@@ -0,0 +1,13 @@
+class EducationService
+  def search_for_schools(data)
+    sleep 2
+  end
+
+  def search_for_enrollments(data)
+    sleep 2
+  end
+
+  def search_for_hours(data)
+    sleep 2
+  end
+end
