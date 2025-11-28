3570e0c69a404bc48e7936bb0d152835460018c3
Jake Shilling <jake.shilling@cms.hhs.gov>
2025-11-28 17:24:04 -0500

    Connect show page to active record models

--- app/app/controllers/activities/education_controller.rb
+++ app/app/controllers/activities/education_controller.rb
@@ -1,22 +1,6 @@
 class Activities::EducationController < ApplicationController
   include ActionController::Live
 
-  class StudentInformation
-    attr_accessor :first_name, :last_name, :date_of_birth
-  end
-
-  class SchoolInformation
-    attr_accessor :name, :address
-  end
-
-  class EnrollmentInformation
-    attr_accessor :most_recent_semester, :enrollment_status
-
-    def less_than_part_time?
-      true
-    end
-  end
-
   def index
     @stream_path = activities_flow_education_stream_path(
       first_name: params[:first_name],
@@ -26,18 +10,26 @@ class Activities::EducationController < ApplicationController
   end
 
   def show
-    @student_information = StudentInformation.new
-    @student_information.first_name = "jake"
-    @student_information.last_name = "shilling"
-    @student_information.date_of_birth = "4/27/1990"
+    @student_information = Identity.find_by(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      date_of_birth: params[:date_of_birth],
+    )
 
-    @school_information = SchoolInformation.new
-    @school_information.name = "U of MD"
-    @school_information.address = "123 Main St"
+    unless @student_information
+      redirect_to(
+        activities_flow_root_path,
+        flash: { alert: t("activities.education.error_no_data") }
+      )
+    end
 
-    @enrollment_information = EnrollmentInformation.new
-    @enrollment_information.most_recent_semester = "Fall 2024"
-    @enrollment_information.enrollment_status = "Part Time"
+    @schools = @student_information.schools.select do |school|
+      school.most_recent_enrollment.current?
+    end
+
+    @less_than_part_time = @schools.all? do |school|
+      school.most_recent_enrollment.less_than_part_time?
+    end
   end
 
   def start
---
+++ app/spec/features/education_controller_spec.rb
@@ -0,0 +1,34 @@
+require "rails_helper"
+
+RSpec.feature "Education confirmation page", type: :feature do
+  context "education data has been collected" do
+    let (:enrollment) {build(:enrollment, :current)}
+    let (:school) {build(:school, enrollments: [enrollment])}
+
+    let(:identity) {create(:identity, schools: [school])}
+
+    before do
+      visit activities_flow_education_success_path(
+              first_name: identity.first_name,
+              last_name: identity.last_name,
+              date_of_birth: identity.date_of_birth
+            )
+    end
+
+    it "displays student information" do
+      expect(page).to have_text(identity.first_name)
+      expect(page).to have_text(identity.last_name)
+      expect(page).to have_text(identity.date_of_birth)
+    end
+
+    it "displays school information" do
+      expect(page).to have_text(school.name)
+      expect(page).to have_text(school.address)
+    end
+
+    it "displays enrollment information" do
+      expect(page).to have_text(enrollment.semester_start)
+      expect(page).to have_text(enrollment.status)
+    end
+  end
+end
