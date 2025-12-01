73277a9072cc378bf873f41279b8b6b8962cd9ae
Daphne Gold <daphnegold@navapbc.com>
2025-12-01 15:14:36 -0800

    Testing

--- app/spec/models/volunteering_activity_spec.rb
+++ app/spec/models/volunteering_activity_spec.rb
@@ -2,14 +2,16 @@ require 'rails_helper'
 
 RSpec.describe VolunteeringActivity, type: :model do
   it 'has fields for organization_name, date, and hours' do
+    activity_flow = ActivityFlow.create!
     activity = VolunteeringActivity.new(
-      organization_name: 'Helping Hands',
-      date: Date.new(1990, 12, 10),
-      hours: 20
+      activity_flow: activity_flow,
+      organization_name: "Daph's Fun House",
+      date: Date.new(2000, 2, 20),
+      hours: 2
     )
 
-    expect(activity.organization_name).to eq('Helping Hands')
-    expect(activity.date).to eq(Date.new(1990, 12, 10))
-    expect(activity.hours).to eq(20)
+    expect(activity.organization_name).to eq("Daph's Fun House")
+    expect(activity.date).to eq(Date.new(2000, 2, 20))
+    expect(activity.hours).to eq(2)
   end
 end
