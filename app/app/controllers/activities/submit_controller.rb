a781a814563f4d93744f8f750785a48bf8ca9a9c
Tom Dooner <tomdooner@navapbc.com>
2025-12-08 11:18:14 -0800

    Rename `@activity_flow` -> `@flow`

--- app/app/controllers/activities/submit_controller.rb
+++ app/app/controllers/activities/submit_controller.rb
@@ -19,10 +19,10 @@ class Activities::SubmitController < Activities::BaseController
   private
 
   def render_pdf
-    @volunteering_activities = @activity_flow.volunteering_activities.order(date: :desc, created_at: :desc)
-    @job_training_activities = @activity_flow.job_training_activities.order(created_at: :desc)
+    @volunteering_activities = @flow.volunteering_activities.order(date: :desc, created_at: :desc)
+    @job_training_activities = @flow.job_training_activities.order(created_at: :desc)
     @submission_timestamp = submission_timestamp
-    progress = ActivityFlowProgressCalculator.progress(@activity_flow)
+    progress = ActivityFlowProgressCalculator.progress(@flow)
     @total_hours = progress.total_hours
 
     render pdf: pdf_filename,
@@ -33,7 +33,7 @@ class Activities::SubmitController < Activities::BaseController
   end
 
   def submission_timestamp
-    @activity_flow.completed_at || Time.zone.now
+    @flow.completed_at || Time.zone.now
   end
 
   def pdf_filename
