59bd9f8c6cf2d662d98cf027644802b5f0fea0d0
Tom Dooner <tomdooner@navapbc.com>
2025-12-08 10:19:50 -0800

    Fix migration when there are ActivityFlow records
    With existing ActivityFlow records in the database, this will fail due
    a failed foreign key constraint on `cbv_applicant_id`. To fix, let's
    just delete everything (since ActivityFlow objects are not important
    yet).

--- app/db/migrate/20251204005440_add_cbv_applicant_to_activity_flows.rb
+++ app/db/migrate/20251204005440_add_cbv_applicant_to_activity_flows.rb
@@ -1,5 +1,6 @@
 class AddCbvApplicantToActivityFlows < ActiveRecord::Migration[7.2]
   def change
+    ActivityFlow.destroy_all
     add_reference :activity_flows, :cbv_applicant, null: false, foreign_key: true
   end
 end
