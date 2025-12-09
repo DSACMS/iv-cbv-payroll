7404a0ddf0a101c4178a7f0aff9361fde7419a17
Tom Dooner <tomdooner@navapbc.com>
2025-12-09 14:19:17 -0800

    Add more NewRelic queue metrics (#1209)
    I want to be able to alert on the number of failed jobs, not necessarily
    just the individual job failures (as we currently have).

--- app/app/jobs/record_batched_newrelic_metrics_job.rb
+++ app/app/jobs/record_batched_newrelic_metrics_job.rb
@@ -3,5 +3,7 @@ class RecordBatchedNewrelicMetricsJob < ApplicationJob
 
   def perform
     NewRelic::Agent.record_metric("Custom/SolidQueue/PendingJobs", SolidQueue::ReadyExecution.count)
+    NewRelic::Agent.record_metric("Custom/SolidQueue/FailedJobs", SolidQueue::FailedExecution.count)
+    NewRelic::Agent.record_metric("Custom/SolidQueue/ClaimedJobs", SolidQueue::ClaimedExecution.count)
   end
 end
