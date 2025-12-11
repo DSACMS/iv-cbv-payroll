e3b02de9b09c6cf3170f32253ac9243841c19142
Imhotep Benjamin <imhotepbenjamin@gmail.com>
2025-12-11 12:37:46 -0500

    Update activities_controller.rb

--- app/app/controllers/activities/activities_controller.rb
+++ app/app/controllers/activities/activities_controller.rb
@@ -5,8 +5,9 @@ class Activities::ActivitiesController < Activities::BaseController
       @flow.save
     end
 
+
     @volunteering_activities = @flow.volunteering_activities.order(created_at: :desc)
     @job_training_activities = @flow.job_training_activities.order(created_at: :desc)
-    @education_activities = @flow.education_activities.order(created_at: :desc)
+    @education_activities = @flow.education_activities.where(confirmed: true).order(created_at: :desc)
   end
 end
