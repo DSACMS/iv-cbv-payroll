06ddd7776ee217cd9e2fe35a657a176ab1731659
Ben Calegari <bencalegari@navapbc.com>
2025-12-23 13:31:51 -0800

    Set flow_type on existing payroll accounts

--- app/db/migrate/20251223032643_create_flows_polymorphic_association.rb
+++ app/db/migrate/20251223032643_create_flows_polymorphic_association.rb
@@ -2,6 +2,7 @@ class CreateFlowsPolymorphicAssociation < ActiveRecord::Migration[8.1]
   def change
     add_reference :payroll_accounts, :activity_flow, null: true, index: true
     add_column :payroll_accounts, :flow_type, :string
+    PayrollAccount.in_batches(of: 500).update_all(flow_type: "CbvFlow")
     rename_column :payroll_accounts, :cbv_flow_id, :flow_id
     add_index :payroll_accounts, [ :flow_type, :flow_id ]
   end
