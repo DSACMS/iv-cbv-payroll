b0069dfff4ab9ad6027c2d01d7f628b1fe2aea32
Daphne Gold <daphnegold@navapbc.com>
2025-12-24 11:49:10 -0800

    Controllers & views

--- app/app/controllers/activities/job_training_controller.rb
+++ app/app/controllers/activities/job_training_controller.rb
@@ -1,4 +1,6 @@
 class Activities::JobTrainingController < Activities::BaseController
+  before_action :set_job_training_activity, only: %i[edit update destroy]
+
   def new
     @job_training_activity = @flow.job_training_activities.new
   end
@@ -12,7 +14,25 @@ class Activities::JobTrainingController < Activities::BaseController
     end
   end
 
+  def update
+    if @job_training_activity.update(job_training_activity_params)
+      redirect_to activities_flow_root_path, notice: t("activities.job_training.updated")
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @job_training_activity.destroy
+
+    redirect_to activities_flow_root_path, notice: t("activities.job_training.deleted")
+  end
+
   private
+
+  def set_job_training_activity
+    @job_training_activity = @flow.job_training_activities.find(params[:id])
+  end
 
   def job_training_activity_params
     params.require(:job_training_activity).permit(:program_name, :organization_address, :hours)
