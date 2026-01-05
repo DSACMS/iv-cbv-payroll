a82a10b230e0e699bd28c5ac738aa9b8fae21693
Jake Shilling <Jake.Shilling@cms.hhs.gov>
2026-01-05 13:29:39 -0500

    FFS-3645 Implement PDF Doc Transmitter (#1248)
    * Extract #pdf_output from SftpTransmitter
    
    Create a base class that can implement #pdf_output for any transmitter
    that sends a PDF file. Create a set of shared examples and a shared
    context that can be used to test future derived classes.
    
    * Create HttpPdfTramsmitter stub
    
    * Immplement POST request
    
    * Add retry logic to HttpPdfTransmitter
    
    * Get confirmation code from cbv_flow
    
    * Add http pdf transmitter to the transmission job
    
    * Extract signature logic from JsonTransmitter
    
    * Fix text failure
    
    * Update app/spec/services/transmitters/http_pdf_transmitter_spec.rb
    
    Co-authored-by: Tom Dooner <tomdooner@navapbc.com>
    
    * Remove retriable
    
    * Rename PdfTansmitter->BasePdfTransmitter
    
    * Run rubocop
    
    * Update app/app/services/transmitters/http_pdf_transmitter.rb
    
    Co-authored-by: Tom Dooner <tomdooner@navapbc.com>
    
    * Remove VCR.turned_off and change transmission configuration validation
    
    ---------
    
    Co-authored-by: Tom Dooner <tomdooner@navapbc.com>

--- app/app/services/transmitters/sftp_transmitter.rb
+++ app/app/services/transmitters/sftp_transmitter.rb
@@ -1,17 +1,8 @@
-class Transmitters::SftpTransmitter
-  include Transmitter
-
+class Transmitters::SftpTransmitter < Transmitters::BasePdfTransmitter
   def deliver
     config = current_agency.transmission_method_configuration.with_indifferent_access
     sftp_gateway = SftpGateway.new(config)
     filename = ClientAgency::AzDes::Configuration.pdf_filename(cbv_flow, cbv_flow.consented_to_authorized_use_at)
     sftp_gateway.upload_data(StringIO.new(pdf_output.content), "#{config["sftp_directory"]}/#{filename}.pdf")
-  end
-
-  def pdf_output
-    @_pdf_output ||= begin
-      pdf_service = PdfService.new(language: :en)
-      pdf_service.generate(cbv_flow, aggregator_report, current_agency)
-    end
   end
 end
---
+++ app/spec/services/transmitters/http_pdf_transmitter_spec.rb
@@ -0,0 +1,74 @@
+require 'rails_helper'
+
+RSpec.describe Transmitters::HttpPdfTransmitter do
+  let(:transmission_method_configuration) do
+    {
+      "url" => "http://fake-state.api.gov/api/v1/income-report-pdf"
+    }
+  end
+
+  let(:client_agency) { instance_double(ClientAgencyConfig::ClientAgency) }
+
+  before do
+    allow(client_agency).to receive(:id).and_return("sandbox")
+    allow(client_agency).to receive(:transmission_method_configuration)
+      .and_return(transmission_method_configuration)
+    allow(client_agency).to receive(:transmission_method)
+      .and_return(Transmitters::HttpPdfTransmitter::TRANSMISSION_METHOD)
+  end
+
+  let(:cbv_flow) { create(:cbv_flow, confirmation_code: "ABC123") }
+  let(:aggregator_report) { build(:argyle_report, :with_argyle_account) }
+
+  subject do
+    described_class.new(cbv_flow, client_agency, aggregator_report)
+  end
+
+  include_examples "Transmitters::BasePdfTransmitter"
+  include_examples "Transmitter#signature"
+
+  describe "#deliver" do
+    include_context "with #pdf_output" do
+      let(:pdf_output) do
+        PdfService::PdfGenerationResult.new(
+          'dummy_pdf_content',
+          'dummy_html_content',
+          1,
+          10,
+        )
+      end
+    end
+
+    let(:sig) { "dummysignature" }
+    let(:code) { "dummyconfirmationcode" }
+    let(:time_now) { Time.now }
+
+    before do
+      allow(subject).to receive(:timestamp).and_return(time_now.to_i)
+      allow(subject).to receive(:signature).and_return(sig)
+    end
+
+    it "sends #pdf_output as a POST request" do
+      stub = stub_request(
+          :post,
+          transmission_method_configuration["url"]
+        ).with(
+          body: pdf_output.content,
+          headers: {
+            'Content-Type': 'application/pdf',
+            'Content-Length': pdf_output.file_size,
+            'X-IVAAS-Timestamp': time_now.to_i,
+            'X-IVAAS-Signature': sig,
+            'X-IVAAS-Confirmation-Code': cbv_flow.confirmation_code
+          }
+        )
+
+      subject.deliver
+
+
+      expect(
+        stub
+      ).to have_been_made
+    end
+  end
+end
