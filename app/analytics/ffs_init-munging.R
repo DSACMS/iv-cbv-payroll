e137d0e81ff93faec1976b08a5dbb2a0b850c70f
Aaron Chafetz <aaron.chafetz@cms.hhs.gov>
2026-01-08 13:07:14 -0500

    fix: remove non LA data; feat: reorder to improve review

--- app/analytics/ffs_init-munging.R
+++ app/analytics/ffs_init-munging.R
@@ -3,7 +3,7 @@
 # PURPOSE:  initial munging of Mixpanel json data
 # LICENSE:  MIT
 # DATE:     2025-12-11
-# UPDATED:  2025-12-31
+# UPDATED:  2026-01-08
 
 # DEPENDENCIES ------------------------------------------------------------
 
@@ -23,18 +23,18 @@ basename(paths)
 # IMPORT FUNCTION ---------------------------------------------------------
 
 # function to read the JSON file & convert to tibble, keeping properties as a nest list
-read_mp <- function(path) {
-  json <- readr::read_lines(path) |> purrr::map(jsonlite::fromJSON)
+# read_mp <- function(path) {
+#   json <- readr::read_lines(path) |> purrr::map(jsonlite::fromJSON)
 
-  df <-
-    tibble::tibble(
-      event = purrr::map_chr(json, ~ .x$event),
-      properties = purrr::map(json, ~ .x$properties),
-      timestamp = purrr::map_chr(json, ~ .x$timestamp)
-    )
+#   df <-
+#     tibble::tibble(
+#       event = purrr::map_chr(json, ~ .x$event),
+#       properties = purrr::map(json, ~ .x$properties),
+#       timestamp = purrr::map_chr(json, ~ .x$timestamp)
+#     )
 
-  return(df)
-}
+#   return(df)
+# }
 
 # IMPORT ------------------------------------------------------------------
 
@@ -56,10 +56,6 @@ df_import <- paths |> map(read_json_arrow) |> list_rbind()
 
 # MUNGE -------------------------------------------------------------------
 
-#drop page view events - no property data useful in analysis
-df_import <- df_import |>
-  filter(event != "CbvPageView")
-
 #convert time to a time variable and identify pilot period
 df_import <- df_import |>
   mutate(
@@ -77,12 +73,8 @@ df_import <- df_import |>
 df_import <- df_import %>%
   mutate(
     distinct_id = properties$distinct_id,
-    cbv_flow_id = properties$cbv_flow_id
+    cbv_flow_id = properties$cbv_flow_id,
   )
-
-#remove events without CBV flow id (case workers + timeouts)
-df_import <- df_import |>
-  filter(!is.na(cbv_flow_id))
 
 #extract type from event to make events generic
 df_import <- df_import |>
@@ -91,14 +83,6 @@ df_import <- df_import |>
     event = str_remove(event, "Pinwheel|Argyle")
   )
 
-#reorder variables
-df_import <- df_import |>
-  relocate(distinct_id, cbv_flow_id, timestamp, pilot, .before = everything())
-
-#arrange time descending (most recent events on top by user)
-df_import <- df_import |>
-  arrange(distinct_id, desc(timestamp))
-
 #grab additional property variables needed in analysis
 df_import <- df_import |>
   mutate(
@@ -106,6 +90,47 @@ df_import <- df_import |>
     origin = properties$origin,
     employer_name = properties$employment_employer_name
   )
+
+#how many were invited?
+
+df_import |>
+  filter()
+
+# SUBSET ------------------------------------------------------------------
+
+#fill missing client_agency_ids and then filter out those that are not LA
+df_import <- df_import |>
+  mutate(client_agency_id = properties$client_agency_id) |>
+  group_by(distinct_id) |>
+  fill(client_agency_id, .direction = "downup") |>
+  ungroup() |>
+  filter(client_agency_id == "la_ldh") |>
+  select(-client_agency_id)
+
+#drop page view events - no property data useful in analysis
+df_import <- df_import |>
+  filter(event != "CbvPageView")
+
+#remove events without CBV flow id (case workers + timeouts)
+df_import <- df_import |>
+  filter(!is.na(cbv_flow_id))
+# filter(!is.na(cbv_flow_id) | event != "CaseworkerInvitedApplicantToFlow")
+
+#convert distinct_id to applicant from caseworker to determine number invited
+# df_import <- df_import |>
+#   filter(event == "CaseworkerInvitedApplicantToFlow") |>
+#   mutate(distinct_id = paste0("applicant-",properties$cbv_applicant_id)) |>
+#   count(pilot)
+
+# TIDY --------------------------------------------------------------------
+
+#reorder variables
+df_import <- df_import |>
+  relocate(distinct_id, cbv_flow_id, timestamp, pilot, .before = everything())
+
+#arrange time descending (most recent events on top by user)
+df_import <- df_import |>
+  arrange(distinct_id, desc(timestamp))
 
 #drop properties nested tibble to save as a parquest file
 df_import <- df_import |>
