5701a62c6d55deaca909da916fdd7fb0a779363c
Jan <242286144+dx-gov@users.noreply.github.com>
2026-01-08 16:40:50 -0500

    fix comment education activity status comment

--- app/app/models/education_activity.rb
+++ app/app/models/education_activity.rb
@@ -1,7 +1,7 @@
 class EducationActivity < ApplicationRecord
   belongs_to :activity_flow
 
-  # Enums values: 0=not_enrolled, 1=quarter_time, 2=half_time, 3=full_time
+  # Status is stored as int in db: 0=unknown, 1=not_enrolled, 2=enrolled
   enum :status, [ :unknown, :not_enrolled, :enrolled ], default: :unknown
 
   def display_status
