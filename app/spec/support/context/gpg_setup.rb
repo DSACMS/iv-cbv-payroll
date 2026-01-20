c9f9a499a181dab345ae7b23b72d0098d98dd4d7
Tom Dooner <tomdooner@navapbc.com>
2026-01-20 10:03:49 -0800

    Fix gpg setup for Dotenv 3.0
    Dotenv 3.0 resets the ENV hash between tests, which breaks our GPG
    environment setup in this test.
    
    To fix that, I've removed the ENV variable stubbing and replaced it with
    a call to configure:
    
    > GPGME::Engine.home_dir

--- app/spec/support/context/gpg_setup.rb
+++ app/spec/support/context/gpg_setup.rb
@@ -1,8 +1,6 @@
 RSpec.shared_context "gpg_setup" do
   before(:all) do
-    @original_gpg_home = ENV['GNUPGHOME']
-    ENV['GNUPGHOME'] = Rails.root.join('tmp', 'gpghome').to_s
-    FileUtils.mkdir_p(ENV['GNUPGHOME'])
+    @tmp_gpg_dir = Dir.mktmpdir("gpg-testing")
 
     key_script = <<~SCRIPT
       %echo Generating a basic OpenPGP key
@@ -18,20 +16,20 @@ RSpec.shared_context "gpg_setup" do
       %echo done
     SCRIPT
 
-    Open3.popen3("gpg", "--batch", "--generate-key") do |stdin, stdout, stderr, wait_thr|
+    Open3.popen3({ "GNUPGHOME" => @tmp_gpg_dir }, "gpg --batch --generate-key") do |stdin, stdout, stderr, wait_thr|
       stdin.write(key_script)
       stdin.close_write
 
       wait_thr.join
     end
 
+    GPGME::Engine.home_dir = @tmp_gpg_dir
     @public_key = GPGME::Key.find(:public, 'test@example.com').first.export(armor: true).to_s
     # Verify that the key was imported successfully
     raise "Failed to import GPG key" unless @public_key
   end
 
   after(:all) do
-    FileUtils.remove_entry ENV['GNUPGHOME'] if File.exist?(ENV['GNUPGHOME'])
-    ENV['GNUPGHOME'] = @original_gpg_home
+    FileUtils.remove_entry(@tmp_gpg_dir) if File.exist?(@tmp_gpg_dir)
   end
 end
