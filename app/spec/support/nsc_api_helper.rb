c30fc28fa8f078fc8bc059c4fe9d102dcce1e2ab
Tom Dooner <tomdooner@navapbc.com>
2026-01-22 16:46:49 -0800

    Add NSC test cases for four test users
    See https://docs.studentclearinghouse.org/vs/insights-json/integration-testing#test-cases-request
    
    This adds test fixtures for four users that we will probably want to
    test the NSC results against (in a later commit):
    
    * Single Enrollment: Dominique Ricardo, DOB 1978-01-12 (Test case N22)
    * Enrolled at 2 schools: Rick Banas, DOB 1979-08-18 (Test case N24)
    * Not enrolled: Lakhshmi Saxena, DOB 1968-02-16 (Test case N25)
    * Previous Enrollment: Scott Tobin, DOB 1998-02-03 (Test case N28)

--- app/spec/support/nsc_api_helper.rb
+++ app/spec/support/nsc_api_helper.rb
@@ -15,4 +15,30 @@ module NscApiHelper
   def nsc_load_relative_json_file(user_folder, filename)
     JSON.parse(File.read(nsc_fixture_path(user_folder, filename)))
   end
+
+  # See this page for various NSC test cases:
+  # https://docs.studentclearinghouse.org/vs/insights-json/integration-testing#test-cases-request
+  #
+  # To save a test case as a fixture, run these in a Rails console (replacing
+  # the name and DOB with the values for the test case you want to save):
+  #
+  # ```
+  # require_relative './spec/support/nsc_api_helper.rb'
+  # include NscApiHelper
+  # nsc_save_fixture_for_user("Johnson", "White", "1982-04-21")
+  # ```
+  def nsc_save_fixture_for_user(first_name, last_name, date_of_birth)
+    fixture_file = nsc_fixture_path("#{first_name}_#{last_name}".downcase, "insight.json")
+    FileUtils.mkdir_p(File.dirname(fixture_file))
+
+    nsc_api = Aggregators::Sdk::NscService.new
+    enrollment_data = nsc_api.fetch_enrollment_data(
+      first_name: first_name,
+      last_name: last_name,
+      date_of_birth: date_of_birth
+    )
+    File.open(fixture_file, "w") do |f|
+      f.puts JSON.pretty_generate(enrollment_data)
+    end
+  end
 end
