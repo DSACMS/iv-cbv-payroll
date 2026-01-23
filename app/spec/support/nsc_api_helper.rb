e683adae527345daa763ea470f8f2fda863b4ea7
Tom Dooner <tomdooner@navapbc.com>
2026-01-23 10:06:06 -0800

    Fix NscService tests
    * Reenable the ones for `#fetch_enrollment_data`
    * Reorganize them to use the NscApiHelper stubs rather than injecting
      http_client
    * Fix request stubs to stub POST requests

--- app/spec/support/nsc_api_helper.rb
+++ app/spec/support/nsc_api_helper.rb
@@ -1,10 +1,34 @@
 module NscApiHelper
   def nsc_stub_request_education_search_response(user_folder)
-    stub_request(:get, %r{#{Aggregators::Sdk::NscService::ENROLLMENT_ENDPOINT}})
+    stub_request(:post, %r{#{Aggregators::Sdk::NscService::ENROLLMENT_ENDPOINT}})
       .to_return(
         status: 200,
         body: nsc_load_relative_json_file(user_folder, 'insight.json').to_json,
         headers: { 'Content-Type': 'application/json;charset=UTF-8' }
+      )
+  end
+
+  # Stub a response where the OAuth token is expired.
+  # Tests should expect that we request a new token and retry the education search.
+  def nsc_stub_request_education_search_token_expired_response(user_folder)
+    stub_request(:post, %r{#{Aggregators::Sdk::NscService::ENROLLMENT_ENDPOINT}})
+      .to_return(
+        { status: 401, body: "" },
+        {
+          status: 200,
+          body: nsc_load_relative_json_file(user_folder, 'insight.json').to_json,
+          headers: { 'Content-Type': 'application/json;charset=UTF-8' }
+        }
+      )
+  end
+
+  def nsc_stub_token_request
+    stub_request(:post, %r{/token})
+      .to_return(
+        status: 200,
+        body: JSON.generate(
+          access_token: "this-is-a-fake-access-token-for-testing"
+        )
       )
   end
 
