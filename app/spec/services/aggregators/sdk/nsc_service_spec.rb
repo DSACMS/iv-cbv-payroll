db9994d81d8c1489e583d6952d4716a8bf10e1b5
Tom Dooner <tomdooner@navapbc.com>
2026-01-26 06:51:46 -0800

    Improve NscService logging and testing (#1303)
    * Use custom tagged logger, rather than manually adding the
      "[NscService]" prefix
    * Log and raise some errors (in development)
    * Don't log request/response bodies as they contain PII

--- app/spec/services/aggregators/sdk/nsc_service_spec.rb
+++ app/spec/services/aggregators/sdk/nsc_service_spec.rb
@@ -1,84 +1,92 @@
 require 'rails_helper'
+
 RSpec.describe Aggregators::Sdk::NscService, type: :service do
-  subject(:service) { described_class.new }
+  subject(:service) { described_class.new(environment: :test, logger: logger) }
 
-  let(:http_client) { instance_double(Faraday::Connection) }
+  let(:logger) { Logger.new(StringIO.new) }
 
-  let(:user_linda) do
-    {
-      first_name: "Linda",
-      last_name: "Cooper",
-      date_of_birth: "1999-01-01"
-    }
+  before do
+    stub_const("Aggregators::Sdk::NscService::ENVIRONMENTS", {
+      test: {
+        base_url: "http://fake-nsc-api.local",
+        token_url: "http://fake-nsc-api.local/token",
+        client_id: "123",
+        client_secret: "top-secret",
+        account_id: "456",
+        scope: "vs.api.insights"
+      }
+    })
   end
 
-  let(:user_lynette) do
-    {
-      first_name: "Lynnette",
-      last_name: "Oyola",
-      date_of_birth: "1988-10-24"
-    }
-  end
-
-  describe 'token retry on unauthorized response' do
-    let(:unauthorized_response) do
-      instance_double(Faraday::Response, status: 401, body: '{}')
+  describe "#fetch_enrollment_data" do
+    let!(:token_request_stub) do
+      nsc_stub_token_request
     end
 
-    let(:success_response) do
-      instance_double(
-        Faraday::Response,
-        status: 200,
-        body: '{"transactionDetails": {}}'
-      )
+    context "for Lynette, a user with enrollment details" do
+      before do
+        nsc_stub_request_education_search_response("lynette")
+      end
+
+      let(:user_lynette) do
+        {
+          first_name: "Lynnette",
+          last_name: "Oyola",
+          date_of_birth: "1988-10-24"
+        }
+      end
+
+      it "returns response with enrollmentDetails for found student" do
+        response = service.fetch_enrollment_data(**user_lynette)
+
+        expect(response).to have_key("studentInfoProvided")
+        expect(response).to have_key("enrollmentDetails")
+      end
+
+      context "when the OAuth token is expired" do
+        let!(:education_search_stub) do
+          nsc_stub_request_education_search_token_expired_response("lynette")
+        end
+
+        it "retries once after unauthorized and clears cached token" do
+          result = service.fetch_enrollment_data(**user_lynette)
+
+          expect(result).to include("transactionDetails")
+          expect(token_request_stub).to have_been_requested
+          expect(education_search_stub).to have_been_requested.twice
+        end
+      end
+
+      context "when the server returns a 500 error" do
+        it "raises an ApiError" do
+          stub_request(:post, %r{#{Aggregators::Sdk::NscService::ENROLLMENT_ENDPOINT}})
+            .to_return(status: 500, body: "", headers: {})
+
+          expect { service.fetch_enrollment_data(**user_lynette) }
+            .to raise_error(Aggregators::Sdk::NscService::ApiError)
+        end
+      end
     end
 
-    before do
-      allow(service).to receive(:http_client).and_return(http_client)
-      allow(http_client).to receive(:post)
-        .and_return(unauthorized_response, success_response)
+    context "for Linda, a user with no enrollment result" do
+      before do
+        nsc_stub_request_education_search_response("linda")
+      end
 
-      allow(Rails.cache).to receive(:delete)
-    end
+      let(:user_linda) do
+        {
+          first_name: "Linda",
+          last_name: "Cooper",
+          date_of_birth: "1999-01-01"
+        }
+      end
 
-    it 'retries once after unauthorized and clears cached token' do
-      result = service.fetch_enrollment_data(**user_linda)
+      it "returns a response without enrollmentDetails" do
+        response = service.fetch_enrollment_data(**user_linda)
 
-      expect(JSON.parse(result)).to eq("transactionDetails" => {})
-      expect(Rails.cache).to have_received(:delete).once
-      expect(http_client).to have_received(:post).twice
-    end
-  end
-
-  xdescribe 'fetch_enrollment_data' do
-    let(:requests) do
-      WebMock::RequestRegistry.instance.requested_signatures.hash.keys
-    end
-
-    it 'returns response with enrollmentDetails for found student' do
-      nsc_stub_request_education_search_response("lynette")
-
-      response = service.fetch_enrollment_data(**user_lynette)
-
-      expect(response).to have_key(:studentInfoProvided)
-      expect(response).to have_key(:enrollmentDetails)
-    end
-
-    it 'returns limited response for not-found student' do
-      nsc_stub_request_education_search_response("linda")
-
-      response = service.fetch_enrollment_data(**user_linda)
-
-      expect(response).to have_key(:studentInfoProvided)
-      expect(response).not_to have_key(:enrollmentDetails)
-    end
-
-    it 'raises Faraday::ServerError on 500 error' do
-      stub_request(:get, Aggregators::Sdk::NscService::ENROLLMENT_ENDPOINT)
-        .to_return(status: 500, body: "", headers: {})
-
-      expect { service.fetch_enrollment_data(**user_linda) }
-        .to raise_error(Faraday::ServerError)
+        expect(response).to have_key("studentInfoProvided")
+        expect(response).not_to have_key("enrollmentDetails")
+      end
     end
   end
 end
--- app/spec/support/nsc_api_helper.rb
+++ app/spec/support/nsc_api_helper.rb
@@ -1,10 +1,34 @@
 module NscApiHelper
   def nsc_stub_request_education_search_response(user_folder)
-    stub_request(:get, %r{#{Aggregators::Sdk::NscService::ENROLLMENT_ENDPOINT}})
+    stub_request(:post, %r{#{Aggregators::Sdk::NscService::ENROLLMENT_ENDPOINT}})
       .to_return(
         status: 200,
         body: nsc_load_relative_json_file(user_folder, 'insight.json').to_json,
         headers: { 'Content-Type': 'application/json;charset=UTF-8' }
+      )
+  end
+
+  # Stub a response where the OAuth token is expired.
+  # Tests should expect that we request a new token and retry the education search.
+  def nsc_stub_request_education_search_token_expired_response(user_folder)
+    stub_request(:post, %r{#{Aggregators::Sdk::NscService::ENROLLMENT_ENDPOINT}})
+      .to_return(
+        { status: 401, body: "" },
+        {
+          status: 200,
+          body: nsc_load_relative_json_file(user_folder, 'insight.json').to_json,
+          headers: { 'Content-Type': 'application/json;charset=UTF-8' }
+        }
+      )
+  end
+
+  def nsc_stub_token_request
+    stub_request(:post, %r{/token})
+      .to_return(
+        status: 200,
+        body: JSON.generate(
+          access_token: "this-is-a-fake-access-token-for-testing"
+        )
       )
   end
 
@@ -14,5 +38,31 @@ module NscApiHelper
 
   def nsc_load_relative_json_file(user_folder, filename)
     JSON.parse(File.read(nsc_fixture_path(user_folder, filename)))
+  end
+
+  # See this page for various NSC test cases:
+  # https://docs.studentclearinghouse.org/vs/insights-json/integration-testing#test-cases-request
+  #
+  # To save a test case as a fixture, run these in a Rails console (replacing
+  # the name and DOB with the values for the test case you want to save):
+  #
+  # ```
+  # require_relative './spec/support/nsc_api_helper.rb'
+  # include NscApiHelper
+  # nsc_save_fixture_for_user("Johnson", "White", "1982-04-21")
+  # ```
+  def nsc_save_fixture_for_user(first_name, last_name, date_of_birth)
+    fixture_file = nsc_fixture_path("#{first_name}_#{last_name}".downcase, "insight.json")
+    FileUtils.mkdir_p(File.dirname(fixture_file))
+
+    nsc_api = Aggregators::Sdk::NscService.new
+    enrollment_data = nsc_api.fetch_enrollment_data(
+      first_name: first_name,
+      last_name: last_name,
+      date_of_birth: date_of_birth
+    )
+    File.open(fixture_file, "w") do |f|
+      f.puts JSON.pretty_generate(enrollment_data)
+    end
   end
 end
