c69f44a8d7f32e653c09eece50a1e003fac78fb4
Daphne Gold <daphnegold@navapbc.com>
2026-01-27 11:31:47 -0800

    FFS-3713: Update work program and volunteering progress calculation/routing to be per-month (#1317)

--- app/app/services/activity_flow_progress_calculator.rb
+++ app/app/services/activity_flow_progress_calculator.rb
@@ -1,18 +1,41 @@
 # frozen_string_literal: true
 
 class ActivityFlowProgressCalculator
-  HOURS_THRESHOLD = 80
+  PER_MONTH_HOURS_THRESHOLD = 80
   Result = Struct.new(:total_hours, :meets_requirements, keyword_init: true)
 
   def self.progress(activity_flow)
-    volunteering_hours = activity_flow.volunteering_activities.sum(:hours).to_i
-    job_training_hours = activity_flow.job_training_activities.sum(:hours).to_i
-    total_hours = volunteering_hours + job_training_hours
-    threshold = HOURS_THRESHOLD * activity_flow.reporting_window_months
+    new(activity_flow).result
+  end
 
+  def initialize(activity_flow)
+    @activity_flow = activity_flow
+    @activities = activity_flow.volunteering_activities + activity_flow.job_training_activities
+  end
+
+  def result
     Result.new(
       total_hours: total_hours,
-      meets_requirements: total_hours >= threshold
+      meets_requirements: each_month_meets_threshold?
     )
+  end
+
+  private
+
+  def total_hours
+    @activities.sum { |activity| activity.hours.to_i }
+  end
+
+  def each_month_meets_threshold?
+    @activity_flow.reporting_window_months.times.all? do |i|
+      month_start = @activity_flow.reporting_window_range.begin + i.months
+      hours_for_month(month_start) >= PER_MONTH_HOURS_THRESHOLD
+    end
+  end
+
+  def hours_for_month(month_start)
+    @activities
+      .select { |activity| activity.date&.between?(month_start, month_start.end_of_month) }
+      .sum { |activity| activity.hours.to_i }
   end
 end
