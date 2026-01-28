d329a46d9744aaa7b2d5a6dcbb048f54f2e853fc
Tom Dooner <tomdooner@navapbc.com>
2026-01-28 08:29:55 -0800

    Update /summary page for education activities

--- app/app/controllers/activities/summary_controller.rb
+++ app/app/controllers/activities/summary_controller.rb
@@ -2,5 +2,6 @@ class Activities::SummaryController < Activities::BaseController
   def show
     @volunteering_activities = @flow.volunteering_activities.order(created_at: :desc)
     @job_training_activities = @flow.job_training_activities.order(created_at: :desc)
+    @education_activities = @flow.education_activities.order(created_at: :desc)
   end
 end
