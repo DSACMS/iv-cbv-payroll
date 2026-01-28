c7d1959a217698634235944ddf9687821cadddaa
Daphne Gold <daphnegold@navapbc.com>
2026-01-28 09:58:30 -0800

    test

--- app/spec/controllers/activities/summary_controller_spec.rb
+++ app/spec/controllers/activities/summary_controller_spec.rb
@@ -9,7 +9,8 @@ RSpec.describe Activities::SummaryController, type: :controller do
     create(
       :activity_flow,
       job_training_activities_count: 0,
-      volunteering_activities_count: 0
+      volunteering_activities_count: 0,
+      education_activities_count: 0
     )
   }
   let(:other_flow) { create(:activity_flow) }
@@ -32,6 +33,55 @@ RSpec.describe Activities::SummaryController, type: :controller do
       expect(assigns(:job_training_activities)).to contain_exactly(visible_job_training)
       expect(response.body).to include("Scoped")
       expect(response.body).to include("Resume Workshop")
+    end
+
+    it "builds all_activities list with all activity types" do
+      create(:volunteering_activity, activity_flow: activity_flow, organization_name: "Org A", hours: 1)
+      create(:job_training_activity, activity_flow: activity_flow, program_name: "Program B", organization_address: "123 Main St", hours: 6)
+      create(:education_activity, activity_flow: activity_flow, school_name: "State University")
+
+      get :show
+
+      all_activities = assigns(:all_activities)
+      expect(all_activities.length).to eq(3)
+      expect(all_activities.map { |a| a[:type] }).to contain_exactly(:volunteering, :job_training, :education)
+    end
+
+    context "with payroll accounts" do
+      render_views false
+
+      it "includes synced payroll accounts (income activities) in all_activities list" do
+        payroll_account = create(:payroll_account, :pinwheel_fully_synced, flow: activity_flow)
+
+        get :show
+
+        all_activities = assigns(:all_activities)
+        income_activity = all_activities.find { |a| a[:type] == :income }
+        expect(income_activity).to be_present
+        expect(income_activity[:payroll_account]).to eq(payroll_account)
+      end
+
+      it "excludes unsynced payroll accounts from all_activities list" do
+        create(:payroll_account, flow: activity_flow, synchronization_status: :in_progress)
+
+        get :show
+
+        all_activities = assigns(:all_activities)
+        income_activity = all_activities.find { |a| a[:type] == :income }
+        expect(income_activity).to be_nil
+      end
+    end
+
+    it "renders the legal agreement checkbox" do
+      get :show
+
+      expect(response.body).to include(I18n.t("activities.summary.legal_agreement"))
+    end
+
+    it "renders the submit button with agency name" do
+      get :show
+
+      expect(response.body).to include("Submit to")
     end
   end
 end
