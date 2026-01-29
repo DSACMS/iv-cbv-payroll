51246a1693cbae81d2e72c428503925c023dc2f1
Tom Dooner <tomdooner@navapbc.com>
2026-01-29 10:49:23 -0800

    Remove other `show_demo_banner` variables

--- app/config/environments/development.rb
+++ app/config/environments/development.rb
@@ -76,6 +76,4 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-
-  config.x.show_demo_banner = ENV["SHOW_DEMO_BANNER"] == "true"
 end
--- app/config/environments/test.rb
+++ app/config/environments/test.rb
@@ -64,6 +64,4 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
-
-  config.x.show_demo_banner = false
 end
