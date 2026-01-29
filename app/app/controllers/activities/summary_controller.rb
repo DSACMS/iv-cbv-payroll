5550d1db8c81d80eec7f14e64da03045df36fe77
Daphne Gold <daphnegold@navapbc.com>
2026-01-29 10:41:23 -0800

    refactor summary controller

--- app/app/controllers/activities/summary_controller.rb
+++ app/app/controllers/activities/summary_controller.rb
@@ -1,23 +1,25 @@
 class Activities::SummaryController < Activities::BaseController
   include Cbv::AggregatorDataHelper
 
+  before_action :set_flow_for_aggregator
+  before_action :set_aggregator_report, if: -> { synced_payroll_accounts.any? }
+
   def show
-    load_activities
+    @volunteering_activities = @flow.volunteering_activities.order(created_at: :asc)
+    @job_training_activities = @flow.job_training_activities.order(created_at: :asc)
+    @education_activities = @flow.education_activities.order(created_at: :asc)
+    @employment_payroll_accounts = synced_payroll_accounts
+    @all_activities = build_activities_list
   end
 
   private
 
-  def load_activities
-    @volunteering_activities = @flow.volunteering_activities.order(created_at: :asc)
-    @job_training_activities = @flow.job_training_activities.order(created_at: :asc)
-    @education_activities = @flow.education_activities.order(created_at: :asc)
-    @synced_payroll_accounts = @flow.payroll_accounts.select(&:sync_succeeded?)
-    @all_activities = build_activities_list
+  def set_flow_for_aggregator
+    @cbv_flow = @flow
+  end
 
-    if @synced_payroll_accounts.any?
-      @cbv_flow = @flow
-      set_aggregator_report
-    end
+  def synced_payroll_accounts
+    @synced_payroll_accounts ||= @flow.payroll_accounts.select(&:sync_succeeded?)
   end
 
   def build_activities_list
@@ -35,7 +37,7 @@ class Activities::SummaryController < Activities::BaseController
       activities << { type: :job_training, activity: activity, created_at: activity.created_at }
     end
 
-    @synced_payroll_accounts.each do |payroll_account|
+    @employment_payroll_accounts.each do |payroll_account|
       activities << { type: :income, payroll_account: payroll_account, created_at: payroll_account.created_at }
     end
 
