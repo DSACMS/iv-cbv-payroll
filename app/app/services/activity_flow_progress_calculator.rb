76a582b9a12860aa4d435359ba786a73d8b5cdef
Daphne Gold <daphnegold@navapbc.com>
2026-02-06 12:11:55 -0800

    Logic updates

--- app/app/services/activity_flow_progress_calculator.rb
+++ app/app/services/activity_flow_progress_calculator.rb
@@ -4,7 +4,7 @@ class ActivityFlowProgressCalculator
   PER_MONTH_HOURS_THRESHOLD = 80
   PER_MONTH_EARNINGS_THRESHOLD = 580_00 # in cents
 
-  OverallResult = Struct.new(:total_hours, :meets_requirements, keyword_init: true)
+  OverallResult = Struct.new(:total_hours, :meets_requirements, :meets_routing_requirements, keyword_init: true)
   MonthlyResult = Struct.new(:month, :total_hours, :meets_requirements, keyword_init: true)
 
   def initialize(activity_flow)
@@ -15,7 +15,8 @@ class ActivityFlowProgressCalculator
   def overall_result
     OverallResult.new(
       total_hours: total_hours,
-      meets_requirements: each_month_meets_threshold?
+      meets_requirements: each_month_meets_threshold?,
+      meets_routing_requirements: each_month_meets_threshold_with_validated_data?
     )
   end
 
@@ -47,6 +48,13 @@ class ActivityFlowProgressCalculator
     reporting_months.all? do |month_start|
       hours_for_month(month_start) >= PER_MONTH_HOURS_THRESHOLD ||
         earnings_for_month(month_start) >= PER_MONTH_EARNINGS_THRESHOLD
+    end
+  end
+
+  def each_month_meets_threshold_with_validated_data?
+    reporting_months.all? do |month_start|
+      validated_hours_for_month(month_start) >= PER_MONTH_HOURS_THRESHOLD ||
+        validated_earnings_for_month(month_start) >= PER_MONTH_EARNINGS_THRESHOLD
     end
   end
 
@@ -97,6 +105,57 @@ class ActivityFlowProgressCalculator
 
       month_data[:accrued_gross_earnings].to_i
     end
+  end
+
+  def validated_hours_for_month(month_start)
+    validated_employment_hours_for_month(month_start) +
+      validated_education_hours_for_month(month_start) +
+      validated_volunteering_and_training_hours_for_month(month_start)
+  end
+
+  def validated_earnings_for_month(month_start)
+    return 0 unless payroll_report
+    raise "Payroll report not fetched" unless payroll_report.has_fetched?
+
+    month_key = month_start.strftime("%Y-%m")
+
+    monthly_summaries.slice(*validated_account_ids).sum do |_account_id, months|
+      month_data = months[month_key]
+      next 0 unless month_data
+
+      month_data[:accrued_gross_earnings].to_i
+    end
+  end
+
+  def validated_employment_hours_for_month(month_start)
+    return 0 unless payroll_report
+    raise "Payroll report not fetched" unless payroll_report.has_fetched?
+
+    month_key = month_start.strftime("%Y-%m")
+
+    monthly_summaries.slice(*validated_account_ids).sum do |_account_id, months|
+      month_data = months[month_key]
+      next 0 unless month_data
+
+      month_data[:total_w2_hours].to_f + month_data[:total_gig_hours].to_f
+    end
+  end
+
+  def validated_education_hours_for_month(month_start)
+    @education_activities
+      .select(&:validated?)
+      .sum { |education| education.progress_hours_for_month(month_start) }
+  end
+
+  def validated_volunteering_and_training_hours_for_month(month_start)
+    @activities
+      .select(&:validated?)
+      .select { |activity| activity.date&.between?(month_start, month_start.end_of_month) }
+      .sum { |activity| activity.hours.to_i }
+  end
+
+  def validated_account_ids
+    @validated_account_ids ||= @activity_flow.payroll_accounts.select(&:validated?).map(&:aggregator_account_id).compact
   end
 
   def monthly_summaries
