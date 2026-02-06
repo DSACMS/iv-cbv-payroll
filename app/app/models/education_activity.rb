54dca9df8606ce6ca5d55e4ffe2fc737d5a6f5a4
Daphne Gold <daphnegold@navapbc.com>
2026-02-06 11:41:08 -0800

    Add data_source to models

--- app/app/models/education_activity.rb
+++ app/app/models/education_activity.rb
@@ -2,6 +2,8 @@ class EducationActivity < ApplicationRecord
   belongs_to :activity_flow
   has_many :nsc_enrollment_terms, dependent: :destroy
 
+  enum :data_source, { self_attested: "self_attested", validated: "validated" }, default: :validated
+
   # Status is the API request/verification status
   enum :status, {
     unknown: "unknown",
