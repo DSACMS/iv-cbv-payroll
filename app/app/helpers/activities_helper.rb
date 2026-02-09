93327097c523c022a023dcf3d23b0cecca9be4a5
Daphne Gold <daphnegold@navapbc.com>
2026-02-09 10:23:40 -0800

    View and logic updates

--- app/app/helpers/activities_helper.rb
+++ app/app/helpers/activities_helper.rb
@@ -1,4 +1,13 @@
 module ActivitiesHelper
+  def any_activities_added?(flow)
+    return false unless flow
+
+    flow.education_activities.exists? ||
+      flow.volunteering_activities.exists? ||
+      flow.job_training_activities.exists? ||
+      flow.payroll_accounts.exists?
+  end
+
   def display_progress_indicator?(progress_calculator)
     progress_calculator.overall_result.total_hours > 0
   end
