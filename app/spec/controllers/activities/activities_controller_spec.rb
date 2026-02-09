68f6b5ee0ff2ca2aba38bda8131440a45306174c
Daphne Gold <daphnegold@navapbc.com>
2026-02-09 10:57:21 -0800

    controller specs

--- app/spec/controllers/activities/activities_controller_spec.rb
+++ app/spec/controllers/activities/activities_controller_spec.rb
@@ -4,8 +4,6 @@ RSpec.describe Activities::ActivitiesController, type: :controller do
   include_context "activity_hub"
   render_views
 
-  let(:flow) { create(:activity_flow) }
-
   describe "#index" do
     let(:current_flow) { create(:activity_flow) }
 
@@ -45,6 +43,41 @@ RSpec.describe Activities::ActivitiesController, type: :controller do
 
     it "renders the progress indicator when hours exist" do
       expect(response.body).to include("activity-flow-progress-indicator")
+    end
+  end
+
+  context "when no activities are added" do
+    let(:current_flow) do
+      create(:activity_flow, volunteering_activities_count: 0, job_training_activities_count: 0, education_activities_count: 0)
+    end
+
+    before do
+      session[:flow_id] = current_flow.id
+      session[:flow_type] = :activity
+      get :index
+    end
+
+    it "renders empty-state copy and hides continue" do
+      expect(response.body).to include(I18n.t("activities.hub.empty.employment"))
+      expect(response.body).to include(I18n.t("activities.hub.empty.education"))
+      expect(response.body).to include(I18n.t("activities.hub.empty.community_service"))
+      expect(response.body).to include(I18n.t("activities.hub.empty.work_programs"))
+      expect(response.body).not_to include(I18n.t("activities.hub.continue"))
+    end
+  end
+
+  context "when at least one activity is added" do
+    let(:current_flow) { create(:activity_flow, volunteering_activities_count: 0, job_training_activities_count: 0, education_activities_count: 0) }
+
+    before do
+      create(:volunteering_activity, activity_flow: current_flow, hours: 1)
+      session[:flow_id] = current_flow.id
+      session[:flow_type] = :activity
+      get :index
+    end
+
+    it "shows continue" do
+      expect(response.body).to include(I18n.t("activities.hub.continue"))
     end
   end
 end
