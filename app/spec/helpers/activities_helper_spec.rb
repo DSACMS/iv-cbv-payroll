7f64327f942c3a777f979cdfe29feaf29301a028
Daphne Gold <daphnegold@navapbc.com>
2026-02-09 10:37:43 -0800

    activities helper specs

---
+++ app/spec/helpers/activities_helper_spec.rb
@@ -0,0 +1,32 @@
+require "rails_helper"
+
+RSpec.describe ActivitiesHelper do
+  describe "#any_activities_added?" do
+    let(:empty_flow) do
+      create(
+        :activity_flow,
+        volunteering_activities_count: 0,
+        job_training_activities_count: 0,
+        education_activities_count: 0
+      )
+    end
+
+    it "returns false when flow has no activities" do
+      expect(helper.any_activities_added?(empty_flow)).to be false
+    end
+
+    [
+      [ :education_activity, :activity_flow ],
+      [ :volunteering_activity, :activity_flow ],
+      [ :job_training_activity, :activity_flow ],
+      [ :payroll_account, :flow ]
+    ].each do |factory_name, flow_attribute|
+      activity_name = factory_name.to_s.humanize.downcase
+
+      it "returns true when flow includes #{activity_name}" do
+        create(factory_name, flow_attribute => empty_flow)
+        expect(helper.any_activities_added?(empty_flow)).to be true
+      end
+    end
+  end
+end
