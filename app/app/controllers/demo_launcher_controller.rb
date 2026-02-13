01d741504b14d2a08586f80889139a743e6d3d51
Ben Calegari <bencalegari@navapbc.com>
2026-02-12 16:27:46 -0800

    Add NSC scenario flows to /demo

--- app/app/controllers/demo_launcher_controller.rb
+++ app/app/controllers/demo_launcher_controller.rb
@@ -7,11 +7,14 @@ class DemoLauncherController < ApplicationController
   def create
     client_agency_id = params[:client_agency_id]
     launch_type = params[:launch_type]
+    nsc_test_user = params[:nsc_test_user]
 
     overrides = params.permit(:reporting_window, :reporting_window_months, :demo_timeout).select { |_, v| v.present? }
 
     url = if launch_type == "generic"
             build_generic_url(client_agency_id, overrides)
+          elsif nsc_test_user.present?
+            build_nsc_test_user_url(nsc_test_user, client_agency_id, overrides)
           else
             build_tokenized_url(client_agency_id, overrides)
           end
@@ -39,6 +42,37 @@ class DemoLauncherController < ApplicationController
       client_agency_id: client_agency_id,
       reference_id: "demo-#{SecureRandom.hex(4)}"
     )
+    invitation.to_url(
+      host: request.host_with_port,
+      protocol: request.protocol,
+      **overrides
+    )
+  end
+
+  NSC_TEST_USERS = {
+    "lynette" => { first_name: "Lynette", last_name: "Oyola", date_of_birth: "1988-10-24" },
+    "rick" => { first_name: "Rick", last_name: "Banas", date_of_birth: "1979-08-18" },
+    "dominique" => { first_name: "Dominique", last_name: "Ricardo", date_of_birth: "1978-01-12" },
+    "linda" => { first_name: "Linda", last_name: "Cooper", date_of_birth: "1999-01-01" }
+  }.freeze
+
+  def build_nsc_test_user_url(user_key, client_agency_id, overrides)
+    user_data = NSC_TEST_USERS[user_key]
+    raise ArgumentError, "Unknown NSC test user: #{user_key}" unless user_data
+
+    cbv_applicant = CbvApplicant.create!(
+      first_name: user_data[:first_name],
+      last_name: user_data[:last_name],
+      date_of_birth: Date.parse(user_data[:date_of_birth]),
+      client_agency_id: client_agency_id
+    )
+
+    invitation = ActivityFlowInvitation.create!(
+      cbv_applicant: cbv_applicant,
+      client_agency_id: client_agency_id,
+      reference_id: "nsc-demo-#{user_key}"
+    )
+
     invitation.to_url(
       host: request.host_with_port,
       protocol: request.protocol,
--- app/spec/controllers/demo_launcher_controller_spec.rb
+++ app/spec/controllers/demo_launcher_controller_spec.rb
@@ -1,10 +1,26 @@
 require "rails_helper"
 
 RSpec.describe DemoLauncherController, type: :controller do
+  render_views
+
   describe "GET #show" do
     it "renders the show template" do
       get :show
       expect(response).to have_http_status(:success)
+    end
+
+    it "displays NSC test scenario options", :aggregate_failures do
+      get :show
+      rendered = response.body
+      expect(rendered).to match(/NSC Test Scenarios/i)
+      expect(rendered).to match(/Lynette Oyola/)
+      expect(rendered).to match(/Currently enrolled.*1 school/)
+      expect(rendered).to match(/Rick Banas/)
+      expect(rendered).to match(/Currently enrolled.*2 schools/)
+      expect(rendered).to match(/Dominique Ricardo/)
+      expect(rendered).to match(/Not currently enrolled/)
+      expect(rendered).to match(/Linda Cooper/)
+      expect(rendered).to match(/No NSC record found/)
     end
   end
 
@@ -73,6 +89,58 @@ RSpec.describe DemoLauncherController, type: :controller do
         location = response.location
         expect(location).to include("reporting_window=renewal")
         expect(location).to include("demo_timeout=15")
+      end
+    end
+
+    context "with an NSC test user" do
+      shared_examples "creates CbvApplicant with correct data" do |user_key, first_name, last_name, dob|
+        it "creates a CbvApplicant with #{first_name}'s data" do
+          expect {
+            post :create, params: {
+              client_agency_id: "sandbox",
+              nsc_test_user: user_key,
+              reporting_window: "application"
+            }
+          }.to change(CbvApplicant, :count).by(1)
+
+          applicant = CbvApplicant.last
+          expect(applicant.first_name).to eq(first_name)
+          expect(applicant.last_name).to eq(last_name)
+          expect(applicant.date_of_birth).to eq(Date.parse(dob))
+        end
+      end
+
+      it_behaves_like "creates CbvApplicant with correct data", "lynette", "Lynette", "Oyola", "1988-10-24"
+      it_behaves_like "creates CbvApplicant with correct data", "rick", "Rick", "Banas", "1979-08-18"
+      it_behaves_like "creates CbvApplicant with correct data", "dominique", "Dominique", "Ricardo", "1978-01-12"
+      it_behaves_like "creates CbvApplicant with correct data", "linda", "Linda", "Cooper", "1999-01-01"
+
+      it "creates an ActivityFlowInvitation and redirects to its URL" do
+        expect {
+          post :create, params: {
+            client_agency_id: "sandbox",
+            nsc_test_user: "lynette",
+            reporting_window: "renewal"
+          }
+        }.to change(ActivityFlowInvitation, :count).by(1)
+
+        invitation = ActivityFlowInvitation.last
+        expect(invitation.client_agency_id).to eq("sandbox")
+        expect(invitation.reference_id).to eq("nsc-demo-lynette")
+        expect(invitation.cbv_applicant).to be_present
+        expect(response).to redirect_to(%r{/activities/start/#{invitation.auth_token}})
+      end
+
+      it "includes override params in the URL" do
+        post :create, params: {
+          client_agency_id: "sandbox",
+          nsc_test_user: "rick",
+          reporting_window: "renewal",
+          demo_timeout: "20"
+        }
+        location = response.location
+        expect(location).to include("reporting_window=renewal")
+        expect(location).to include("demo_timeout=20")
       end
     end
   end
