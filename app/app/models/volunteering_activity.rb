2042b3cb4fda07ca5c2978cdb441b27088e15fbd
Daphne Gold <daphnegold@navapbc.com>
2026-02-19 12:33:35 -0800

    compact_blank everything, because we can

--- app/app/models/volunteering_activity.rb
+++ app/app/models/volunteering_activity.rb
@@ -7,8 +7,8 @@ class VolunteeringActivity < Activity
 
   def formatted_address
     locality = [ city, state ].compact_blank.join(", ")
-    locality_zip = [ locality.presence, zip_code.presence ].compact.join(" ")
-    [ street_address, street_address_line_2.presence, locality_zip.presence ].compact.join(", ")
+    locality_zip = [ locality, zip_code ].compact_blank.join(" ")
+    [ street_address, street_address_line_2, locality_zip ].compact_blank.join(", ")
   end
 
   def document_upload_object_title
