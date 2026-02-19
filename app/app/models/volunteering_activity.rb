84fb45f95f19a0492829ccee8cc4aca87518b0b5
Daphne Gold <daphnegold@navapbc.com>
2026-02-19 11:33:05 -0800

    Review & Submit updates

--- app/app/models/volunteering_activity.rb
+++ app/app/models/volunteering_activity.rb
@@ -3,4 +3,10 @@ class VolunteeringActivity < Activity
 
   has_many :volunteering_activity_months, dependent: :destroy
   has_activity_months :volunteering_activity_months
+
+  def formatted_address
+    locality = [ city, state ].compact_blank.join(", ")
+    locality_zip = [ locality.presence, zip_code.presence ].compact.join(" ")
+    [ street_address, street_address_line_2.presence, locality_zip.presence ].compact.join(", ")
+  end
 end
