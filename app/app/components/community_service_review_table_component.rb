16073f9a363d71add41b5ee0c9f7a60f22ac7167
Tim Miller <timmiller@navapbc.com>
2026-02-19 13:56:57 -0600

    visual tweaks to the review org details table

--- app/app/components/community_service_review_table_component.rb
+++ app/app/components/community_service_review_table_component.rb
@@ -5,10 +5,10 @@ class CommunityServiceReviewTableComponent < ViewComponent::Base
 
   def formatted_address
     parts = [ @activity.street_address, @activity.city, @activity.state ].select(&:present?)
-    parts.join(", ") if parts.any?
+    parts.join(", ").presence
   end
 
-  def phone_display
-    @activity.coordinator_phone_number.presence || I18n.t("shared.not_applicable")
+  def display_value(value)
+    value.presence || I18n.t("shared.not_applicable")
   end
 end
