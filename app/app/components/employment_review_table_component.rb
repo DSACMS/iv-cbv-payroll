1124fa1a547b404cb4b710d06a9318bcc1bf48e9
Tim Miller <timmiller@navapbc.com>
2026-02-24 09:05:38 -0600

    first draft

---
+++ app/app/components/employment_review_table_component.rb
@@ -0,0 +1,14 @@
+class EmploymentReviewTableComponent < ViewComponent::Base
+  def initialize(employment_activity:)
+    @activity = employment_activity
+  end
+
+  def formatted_address
+    parts = [ @activity.street_address, @activity.city, @activity.state ].select(&:present?)
+    parts.join(", ").presence
+  end
+
+  def display_value(value)
+    value.presence || I18n.t("shared.not_applicable")
+  end
+end
