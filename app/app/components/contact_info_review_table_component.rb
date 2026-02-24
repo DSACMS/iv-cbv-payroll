b49ed723be9d49996108b008c672e574c0d2c771
Tim Miller <timmiller@navapbc.com>
2026-02-24 09:23:00 -0600

    brought back format_address

--- app/app/components/contact_info_review_table_component.rb
+++ app/app/components/contact_info_review_table_component.rb
@@ -3,6 +3,10 @@ class ContactInfoReviewTableComponent < ViewComponent::Base
     @rows = rows
   end
 
+  def self.formatted_address(activity)
+    [ activity.street_address, activity.city, activity.state ].select(&:present?).join(", ").presence
+  end
+
   def display_value(value)
     value.presence || I18n.t("shared.not_applicable")
   end
