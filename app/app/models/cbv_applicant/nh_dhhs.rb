a2a8a06a3d842e0ac75c0a4e2e71d06cb1bef257
Jake Shilling <Jake.Shilling@cms.hhs.gov>
2026-02-25 11:13:43 -0500

    FFS-3835 Add NH DHHS Part 1 (#1416)
    * Add NH DHHS as a valid client agency id
    
    * Add domain name for NH

---
+++ app/app/models/cbv_applicant/nh_dhhs.rb
@@ -0,0 +1,16 @@
+class CbvApplicant::NhDhhs < CbvApplicant
+  VALID_ATTRIBUTES = %i[
+    first_name
+    middle_name
+    last_name
+    case_number
+    date_of_birth
+    ]
+
+  has_redactable_fields(
+    first_name: :string,
+    middle_name: :string,
+    last_name: :string,
+    date_of_birth: :date
+  )
+end
