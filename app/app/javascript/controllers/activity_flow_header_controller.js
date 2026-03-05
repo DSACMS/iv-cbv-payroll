f7445b1d9d3d09502e2962cc822aea1a2c55b94f
Tim Miller <timmiller@navapbc.com>
2026-03-05 14:14:31 -0600

    iterate over list of listeners

--- app/app/javascript/controllers/activity_flow_header_controller.js
+++ app/app/javascript/controllers/activity_flow_header_controller.js
@@ -6,17 +6,20 @@ export default class extends Controller {
   connect() {
     this.isDirty = false
     this._boundMarkDirty = this._markDirty.bind(this)
+    this._listeners = []
     document.querySelectorAll("form input, form textarea, form select").forEach((el) => {
       el.addEventListener("input", this._boundMarkDirty)
       el.addEventListener("change", this._boundMarkDirty)
+      this._listeners.push(el)
     })
   }
 
   disconnect() {
-    document.querySelectorAll("form input, form textarea, form select").forEach((el) => {
+    this._listeners.forEach((el) => {
       el.removeEventListener("input", this._boundMarkDirty)
       el.removeEventListener("change", this._boundMarkDirty)
     })
+    this._listeners = []
   }
 
   handleExit(event) {
