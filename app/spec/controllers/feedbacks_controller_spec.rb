03b8b1b0d739b610a51df1f49f0e542b069a6b30
Tim Miller <timmiller@navapbc.com>
2026-03-17 08:09:45 -0500

    FFS-3609: add device id to feedback form (#1481)

--- app/spec/controllers/feedbacks_controller_spec.rb
+++ app/spec/controllers/feedbacks_controller_spec.rb
@@ -13,36 +13,56 @@ RSpec.describe FeedbacksController, type: :controller do
       allow(ApplicationController.helpers).to receive(:feedback_form_url).and_return(feedback_form_url)
     end
 
-    it "redirects to the feedback form and tracks the event with source" do
-      referer_url = cbv_flow_employer_search_path(locale: :en)
-      get :show, params: { referer: referer_url }
+    context "with device_id cookie" do
+      before do
+        cookies.permanent.signed[:device_id] = "test-device-id-123"
+      end
 
-      expect(event_logger).to have_received(:track).with(
-        "ApplicantClickedFeedbackLink",
-        kind_of(ActionDispatch::Request),
-        hash_including(
-          referer: referer_url,
-          client_agency_id: cbv_flow.cbv_applicant.client_agency_id,
-          cbv_flow_id: cbv_flow.id
+      it "redirects to the feedback form with prefill params and tracks the event" do
+        referer_url = cbv_flow_employer_search_path(locale: :en)
+        get :show, params: { referer: referer_url }
+
+        expect(event_logger).to have_received(:track).with(
+          "ApplicantClickedFeedbackLink",
+          kind_of(ActionDispatch::Request),
+          hash_including(
+            referer: referer_url,
+            client_agency_id: cbv_flow.cbv_applicant.client_agency_id,
+            cbv_flow_id: cbv_flow.id
+          )
         )
-      )
-      expect(response).to have_http_status(:redirect)
-      expect(response.location).to eq(ApplicationController.helpers.feedback_form_url)
+        expect(response).to have_http_status(:redirect)
+        expected_url = "#{feedback_form_url}?usp=pp_url&#{ApplicationHelper::FEEDBACK_FORM_DEVICE_ID_ENTRY}=test-device-id-123"
+        expect(response.location).to eq(expected_url)
+      end
+
+      it "redirects to the survey form without prefill params" do
+        get :show, params: { form: "survey" }
+
+        expect(event_logger).to have_received(:track).with(
+          "ApplicantClickedFeedbackSurveyLink",
+          kind_of(ActionDispatch::Request),
+          hash_including(
+            client_agency_id: cbv_flow.cbv_applicant.client_agency_id,
+            cbv_flow_id: cbv_flow.id
+          )
+        )
+        expect(response).to have_http_status(:redirect)
+        expect(response.location).to eq(ApplicationController.helpers.survey_form_url)
+      end
     end
 
-    it "redirects to the survey form and tracks the event with source" do
-      get :show, params: { form: "survey" }
+    context "without device_id cookie" do
+      before do
+        allow(controller).to receive(:set_device_id_cookie)
+      end
 
-      expect(event_logger).to have_received(:track).with(
-        "ApplicantClickedFeedbackSurveyLink",
-        kind_of(ActionDispatch::Request),
-        hash_including(
-          client_agency_id: cbv_flow.cbv_applicant.client_agency_id,
-          cbv_flow_id: cbv_flow.id
-        )
-      )
-      expect(response).to have_http_status(:redirect)
-      expect(response.location).to eq(ApplicationController.helpers.survey_form_url)
+      it "redirects to the base feedback form URL" do
+        get :show, params: { referer: "http://example.com" }
+
+        expect(response).to have_http_status(:redirect)
+        expect(response.location).to eq(feedback_form_url)
+      end
     end
   end
 end
