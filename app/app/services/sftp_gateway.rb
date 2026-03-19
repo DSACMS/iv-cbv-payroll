67747e72bf139439ec3a92cdd2a8ac96a1988a1e
Tom Dooner <tomdooner@navapbc.com>
2026-03-19 10:43:56 -0700

    FFS-3837: Allow SFTP transmission with SSH key authentication
    We want to implement SSH key-based authentication for a new state
    partner. This commit adds the ability to specify a private key via the
    YAML file to use for SFTP authentication.
    
    The private key must start with the "-----BEGIN OPENSSH PRIVATE
    KEY-----" header and have its contents joined onto one line with "\n"
    characters.

--- app/app/services/sftp_gateway.rb
+++ app/app/services/sftp_gateway.rb
@@ -1,18 +1,30 @@
 class SftpGateway
-  attr_reader :url, :user, :password
+  attr_reader :url, :user, :password, :private_key
+
   def initialize(options)
     @url = options[:url]
     @user = options[:user]
     @password = options[:password]
+    @private_key = options[:private_key]
   end
 
   def upload_data(local_file, remote_file_location)
-    session = Net::SSH.start(url, user, password: password, port: 22)
+    session = Net::SSH.start(url, user, **ssh_options)
     sftp = Net::SFTP::Session.new(session)
     sftp.connect!
     sftp.upload! local_file, remote_file_location
     sftp.channel.eof!
     # https://github.com/net-ssh/net-ssh/issues/716
     sftp.close_channel
+  end
+
+  private
+
+  def ssh_options
+    {
+      port: 22,
+      password: password,
+      key_data: private_key.present? ? [ private_key ] : nil
+    }.compact
   end
 end
