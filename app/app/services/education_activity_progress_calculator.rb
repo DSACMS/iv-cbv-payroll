a354331c735469b93cc2b791117b917f83e98a1c
Tom Dooner <tomdooner@navapbc.com>
2026-03-25 17:09:52 -0700

    Add a comment for the wicked logic in EducationActivityProgressCalculator

--- app/app/services/education_activity_progress_calculator.rb
+++ app/app/services/education_activity_progress_calculator.rb
@@ -28,25 +28,39 @@ class EducationActivityProgressCalculator
   def monthly_hours_for_reporting_month(month_start)
     terms = terms_for_month(month_start)
 
+    # Buckle up, here's the logic to determine how many hours to credit the
+    # user for. The routing hours may be different so that we can force the
+    # user to always go back through the edit pages if necessary.
     if @education_activity.fully_self_attested?
+      # For fully self-attested activities, we calculate hours based on how
+      # many credit hours the user was enrolled in for that month.
       monthly_credit_hours = @education_activity.education_activity_months.find_by(month: month_start)&.hours
       MonthHours.new(
         progress: @education_activity.community_engagement_hours(monthly_credit_hours),
         routing: 0      # Always route to hours entry/doc upload pages.
       )
     elsif !@education_activity.sync_succeeded?
+      # For activities where NSC failed, we return empty so that they'll be
+      # treated as partially self-attested.
       MonthHours.new(progress: 0, routing: 0)
     elsif EducationSummerCarryoverService.applies?(@education_activity.nsc_enrollment_terms, month_start)
+      # For activities where the summer carryover logic applies (so therefore a
+      # spring term qualifies the user for the summer months), we return a
+      # complete progress and routing calculation.
       MonthHours.new(
         progress: ActivityFlowProgressCalculator::PER_MONTH_HOURS_THRESHOLD,
         routing: ActivityFlowProgressCalculator::PER_MONTH_HOURS_THRESHOLD
       )
     elsif month_has_half_time_or_above?(terms)
+      # For activities where NSC returns half-time or greater, that's complete.
       MonthHours.new(
         progress: ActivityFlowProgressCalculator::PER_MONTH_HOURS_THRESHOLD,
         routing: ActivityFlowProgressCalculator::PER_MONTH_HOURS_THRESHOLD
       )
     elsif @education_activity.partially_self_attested? && terms.present?
+      # For partially self-attested activities (where the user had to
+      # supplement NSC data for a given month), we compute monthly hours
+      # similar to a fully self-attested activity.
       partially_self_attested_monthly_hours(terms)
     else
       MonthHours.new(progress: 0, routing: 0)
