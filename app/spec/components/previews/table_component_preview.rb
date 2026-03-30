628d8c83e7a91c814aa7016d8735f27145f12eff
Ben Calegari <bencalegari@navapbc.com>
2026-03-30 16:05:07 -0700

    New table style
    - Allow for more than two column table headers

--- app/spec/components/previews/table_component_preview.rb
+++ app/spec/components/previews/table_component_preview.rb
@@ -1,28 +1,14 @@
 # frozen_string_literal: true
 
 class TableComponentPreview < ApplicationPreview
-  def with_sections_and_responsive_rows
-    render(TableComponent.new(is_responsive: true, thead_class_names: "")) do |table|
-      table.with_header_two_column(column1_title: "Field", column2_title: "Value", class_names: "text-left")
-
-      table.with_subheader_row(class_names: "subheader-row base-lightest") do |row|
-        row.with_data_cell(is_header: true) { "Profile" }
-        row.with_data_cell(is_header: true) { "Latest update" }
-      end
+  def default
+    render(TableComponent.new(is_responsive: true, class_names: "cbv-table", thead_class_names: "")) do |table|
+      table.with_header_cell(is_header: true, scope: "col") { "Field" }
+      table.with_header_cell(is_header: true, scope: "col") { "Value" }
 
       table.with_row do |row|
         row.with_data_cell(is_header: true, data_label: "Field") { "Employer" }
         row.with_data_cell(data_label: "Value") { "Patuxent Labs" }
-      end
-
-      table.with_row(highlight: true) do |row|
-        row.with_data_cell(is_header: true, data_label: "Field") { "Status" }
-        row.with_data_cell(data_label: "Value") { "Processing" }
-      end
-
-      table.with_row do |row|
-        row.with_data_cell(is_header: true, data_label: "Field") { "Pay date" }
-        row.with_data_cell(data_label: "Value") { "August 15, 2023" }
       end
 
       table.with_row do |row|
