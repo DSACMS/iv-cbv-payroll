bae63579679c4f4839a4183f7e506b5e26a4810d
Jake Shilling <jake.shilling@cms.hhs.gov>
2026-04-02 10:08:53 -0400

    Configure SFTP PDF filename formats

--- app/spec/services/transmitters/sftp_transmitter_spec.rb
+++ app/spec/services/transmitters/sftp_transmitter_spec.rb
@@ -18,6 +18,7 @@ RSpec.describe Transmitters::SftpTransmitter do
   let(:cbv_flow) { create(:cbv_flow) }
   let(:aggregator_report) { build(:argyle_report, :with_argyle_account) }
   let(:sftp_gateway) { instance_double(SftpGateway, upload_data: true) }
+  let(:formatted_filename) { "formatted_filename" }
 
   before do
     allow(client_agency).to receive_messages(
@@ -25,9 +26,28 @@ RSpec.describe Transmitters::SftpTransmitter do
       transmission_method: described_class::TRANSMISSION_METHOD
     )
     allow(SftpGateway).to receive(:new).and_return(sftp_gateway)
+    allow(Transmitters::PdfFilenameFormatter).to receive(:format).and_return(formatted_filename)
   end
 
   it_behaves_like "Transmitters::BasePdfTransmitter"
+
+  describe "#pdf_filename_format" do
+    subject(:pdf_filename_format) { transmitter.send(:pdf_filename_format) }
+
+    it "returns the default format when no custom format is configured" do
+      expect(pdf_filename_format).to eq(described_class::DEFAULT_PDF_FILENAME_FORMAT)
+    end
+
+    context "when a custom pdf filename format is configured" do
+      let(:transmission_method_configuration) do
+        super().merge("pdf_filename_format" => "CBV_%{case_number}_%{consent_timestamp}_%{confirmation_code}.pdf")
+      end
+
+      it "returns the configured format" do
+        expect(pdf_filename_format).to eq("CBV_%{case_number}_%{consent_timestamp}_%{confirmation_code}.pdf")
+      end
+    end
+  end
 
   describe "#deliver" do
     include_context "with #pdf_output" do
@@ -48,6 +68,26 @@ RSpec.describe Transmitters::SftpTransmitter do
       )
     end
 
+    context "when a custom pdf filename format is configured" do
+      let(:transmission_method_configuration) do
+        super().merge("pdf_filename_format" => "chosen_format")
+      end
+
+      it "passes the selected format to PdfFilenameFormatter" do
+        expect(Transmitters::PdfFilenameFormatter).to receive(:format).with(
+          cbv_flow,
+          "chosen_format"
+        ).and_return(formatted_filename)
+
+        expect(sftp_gateway).to receive(:upload_data).with(
+          an_instance_of(StringIO),
+          "test/formatted_filename.pdf"
+        )
+
+        transmitter.deliver
+      end
+    end
+
     context "when the agency uses username and password authentication" do
       it "passes the password-based config through to the gateway" do
         expect(SftpGateway).to receive(:new).with(
@@ -61,7 +101,7 @@ RSpec.describe Transmitters::SftpTransmitter do
 
         expect(sftp_gateway).to receive(:upload_data).with(
           an_instance_of(StringIO),
-          "test/CBVPilot_20250101_ConfSANDBOX001.pdf"
+          "test/formatted_filename.pdf"
         )
 
         transmitter.deliver
@@ -90,7 +130,7 @@ RSpec.describe Transmitters::SftpTransmitter do
 
         expect(sftp_gateway).to receive(:upload_data).with(
           an_instance_of(StringIO),
-          "test/CBVPilot_20250101_ConfSANDBOX001.pdf"
+          "test/formatted_filename.pdf"
         )
 
         transmitter.deliver
