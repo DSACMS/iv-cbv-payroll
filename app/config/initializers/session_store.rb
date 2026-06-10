316fd3358b0b2485771cded1b252033644b43212
Ian Norris <iannorris@Ians-MacBook-Pro.local>
2026-06-10 14:58:53 -0700

    [FFS-4474] fix issue with cookie store
    https://api.rubyonrails.org/classes/ActionDispatch/Session/CookieStore.html
    its expire_after not expires_after

--- app/config/initializers/session_store.rb
+++ app/config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 Rails.application.config.session_store :cookie_store,
   key: "_iv_cbv_payroll_session",
-  expires_after: Rails.application.config.cbv_session_expires_after
+  expire_after: Rails.application.config.cbv_session_expires_after
