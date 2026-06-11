66fda3b839942be2e631ea39b25d325fe1f5e570
Ian Norris <iannorris@Ians-MacBook-Pro.local>
2026-06-11 08:39:26 -0700

    flailing

--- app/config/initializers/session_store.rb
+++ app/config/initializers/session_store.rb
@@ -1,6 +1,10 @@
 # end to end tests require no auth of session length--TODO fix
-unless Rails.env.test?
+if Rails.env.test?
   Rails.application.config.session_store :cookie_store,
     key: "_iv_cbv_payroll_session",
-    expire_after: Rails.application.config.cbv_session_expires_after
+    expires_after: Rails.application.config.cbv_session_expires_after
+else
+  Rails.application.config.session_store :cookie_store,
+                                         key: "_iv_cbv_payroll_session",
+                                         expire_after: Rails.application.config.cbv_session_expires_after
 end
