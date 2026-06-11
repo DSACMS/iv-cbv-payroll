d77daa55fe0e9347a681644aee7898522874abe6
Ian Norris <iannorris@Ians-MacBook-Pro.local>
2026-06-11 14:05:21 -0700

    yeet part 4

--- app/config/initializers/session_store.rb
+++ app/config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 # end to end tests require no auth of session length--TODO fix
-unless Rails.env.test?
+if ENV["E2E_RUN_TESTS"].nil?
   Rails.application.config.session_store :cookie_store,
                                          key: "_iv_cbv_payroll_session",
                                          expire_after: Rails.application.config.cbv_session_expires_after
