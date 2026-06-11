44f6e1daad54ebdebf7c0f9cf371e85f463c346e
iannorriswork <ian.norris.1991+work@gmail.com>
2026-06-11 14:36:10 -0700

    [FFS-4474] fix issue with cookie store (#1808)
    Co-authored-by: Ian Norris <iannorris@Ians-MacBook-Pro.local>

--- app/config/initializers/session_store.rb
+++ app/config/initializers/session_store.rb
@@ -1,3 +1,6 @@
-Rails.application.config.session_store :cookie_store,
-  key: "_iv_cbv_payroll_session",
-  expires_after: Rails.application.config.cbv_session_expires_after
+# end to end tests require no auth of session length--TODO fix
+if ENV["E2E_RUN_TESTS"].nil?
+  Rails.application.config.session_store :cookie_store,
+                                         key: "_iv_cbv_payroll_session",
+                                         expire_after: Rails.application.config.cbv_session_expires_after
+end
