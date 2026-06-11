55d89b5ca402f19bda25ab96cf5c49811a579cdd
Ian Norris <iannorris@Ians-MacBook-Pro.local>
2026-06-11 09:39:35 -0700

    see if somehow the key matters at all

--- app/config/initializers/session_store.rb
+++ app/config/initializers/session_store.rb
@@ -1,9 +1,5 @@
 # end to end tests require no auth of session length--TODO fix
-if Rails.env.test?
-  Rails.application.config.session_store :cookie_store,
-    key: "_iv_cbv_payroll_session",
-    expires_after: Rails.application.config.cbv_session_expires_after
-else
+unless Rails.env.test?
   Rails.application.config.session_store :cookie_store,
                                          key: "_iv_cbv_payroll_session",
                                          expire_after: Rails.application.config.cbv_session_expires_after
