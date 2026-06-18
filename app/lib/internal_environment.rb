be6a96b52d6572cc84580d51218be5289ee2ccbe
Daphne Gold <daphnegold@navapbc.com>
2026-06-18 15:20:06 -0700

    Enable launcher in UAT + small refactor

---
+++ app/lib/internal_environment.rb
@@ -0,0 +1,18 @@
+module InternalEnvironment
+  DOMAINS = [
+    # "Dev" deployed environment
+    "verify-demo.navapbc.cloud",
+    # "Demo" deployed environment
+    "demo.reportmyincome.org",
+    # "UAT" deployed environment
+    "uat.emmy.cms.gov"
+  ].freeze
+
+  PR_REVIEW_APP_DOMAIN_PATTERN = /\Ap-\d+\.navapbc\.cloud\z/
+
+  def self.internal?(domain_name:, rails_env:)
+    return true if rails_env.development? || rails_env.test?
+
+    DOMAINS.include?(domain_name) || domain_name.to_s.match?(PR_REVIEW_APP_DOMAIN_PATTERN)
+  end
+end
---
+++ app/spec/lib/internal_environment_spec.rb
@@ -0,0 +1,70 @@
+require "rails_helper"
+
+RSpec.describe InternalEnvironment do
+  describe ".internal?" do
+    subject(:internal_environment) do
+      described_class.internal?(domain_name: domain_name, rails_env: rails_env)
+    end
+
+    let(:domain_name) { "example.com" }
+    let(:rails_env) { ActiveSupport::StringInquirer.new("production") }
+
+    context "when Rails env is development" do
+      let(:rails_env) { ActiveSupport::StringInquirer.new("development") }
+
+      it { is_expected.to be true }
+    end
+
+    context "when Rails env is test" do
+      let(:rails_env) { ActiveSupport::StringInquirer.new("test") }
+
+      it { is_expected.to be true }
+    end
+
+    context "when the domain is UAT" do
+      let(:domain_name) { "uat.emmy.cms.gov" }
+
+      it { is_expected.to be true }
+    end
+
+    context "when the domain is dev" do
+      let(:domain_name) { "verify-demo.navapbc.cloud" }
+
+      it { is_expected.to be true }
+    end
+
+    context "when the domain is demo" do
+      let(:domain_name) { "demo.reportmyincome.org" }
+
+      it { is_expected.to be true }
+    end
+
+    context "when the domain is a PR review app" do
+      let(:domain_name) { "p-123.navapbc.cloud" }
+
+      it { is_expected.to be true }
+    end
+
+    context "when the domain looks like a PR review app but does not match the expected pattern" do
+      let(:domain_name) { "p-123.navapbc.cloud.evil.com" }
+
+      it { is_expected.to be false }
+    end
+
+    context "when the domain is production" do
+      let(:domain_name) { "reportmyincome.org" }
+
+      it { is_expected.to be false }
+    end
+
+    context "when the domain is the CMS production host" do
+      let(:domain_name) { "emmy.cms.gov" }
+
+      it { is_expected.to be false }
+    end
+
+    context "when the domain is unknown" do
+      it { is_expected.to be false }
+    end
+  end
+end
