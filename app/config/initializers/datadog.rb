2ffe135c8eaaa051947dc4ac48cf5651cb923228
Daphne Gold <daphnegold@navapbc.com>
2026-07-17 15:08:20 -0700

    Make Datadog IMAGE_TAG optional

--- app/config/initializers/datadog.rb
+++ app/config/initializers/datadog.rb
@@ -2,7 +2,7 @@ if ENV["DD_TRACE_ENABLED"] == "true"
   require "datadog"
 
   Datadog.configure do |config|
-    config.version = ENV.fetch("IMAGE_TAG")
+    config.version = ENV["IMAGE_TAG"]
     config.tracing.instrument :rails
   end
 end
