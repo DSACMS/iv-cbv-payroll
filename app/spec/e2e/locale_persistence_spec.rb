333801acbb16b5abdea6c7afb0d04379d9159452
Chris Vetter <chrisvetter@navapbc.com>
2026-09-02 09:51:20 -0400

    Remove translation

--- app/spec/e2e/locale_persistence_spec.rb
+++ app/spec/e2e/locale_persistence_spec.rb
@@ -6,20 +6,20 @@ RSpec.describe "Locale persistence across pages", :js, type: :feature do
   let(:cbv_flow_invitation) { create(:cbv_flow_invitation, :sandbox) }
 
   # Commenting out until translations are complete, FFS-4642
-  # it "persists locale and allows switching back" do
-  #   # Visit the entry page via invitation URL (in English by default)
-  #   visit URI(cbv_flow_invitation.to_url).request_uri
-  #   verify_page(page, title: I18n.t("cbv.entries.show.header", locale: :en))
-  #   click_link I18n.t("shared.languages.es")
-  #   expect(page).to have_content(I18n.t("cbv.entries.show.header", locale: :es))
+  xit "persists locale and allows switching back" do
+    # Visit the entry page via invitation URL (in English by default)
+    visit URI(cbv_flow_invitation.to_url).request_uri
+    verify_page(page, title: I18n.t("cbv.entries.show.header", locale: :en))
+    click_link I18n.t("shared.languages.es")
+    expect(page).to have_content(I18n.t("cbv.entries.show.header", locale: :es))
 
-  #   # Navigate to next page - should stay in Spanish
-  #   find('[data-cbv-entry-page-target="consentCheckbox"]').click
-  #   click_button I18n.t("cbv.entries.show.continue", locale: :es)
-  #   expect(page).to have_content(I18n.t("cbv.employer_searches.show.header", locale: :es))
+    # Navigate to next page - should stay in Spanish
+    find('[data-cbv-entry-page-target="consentCheckbox"]').click
+    click_button I18n.t("cbv.entries.show.continue", locale: :es)
+    expect(page).to have_content(I18n.t("cbv.employer_searches.show.header", locale: :es))
 
-  #   # Switch back to English
-  #   click_link I18n.t("shared.languages.en")
-  #   expect(page).to have_content(I18n.t("cbv.employer_searches.show.header", locale: :en))
-  # end
+    # Switch back to English
+    click_link I18n.t("shared.languages.en")
+    expect(page).to have_content(I18n.t("cbv.employer_searches.show.header", locale: :en))
+  end
 end
