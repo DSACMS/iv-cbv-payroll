d65a02f3bc9f49d08ef33a5a4cc474b5f6179cb1
Chris Vetter <chrisvetter@navapbc.com>
2026-09-04 14:57:33 -0400

    Change to allow switcher on income side

--- app/spec/e2e/locale_persistence_spec.rb
+++ app/spec/e2e/locale_persistence_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe "Locale persistence across pages", :js, type: :feature do
 
   let(:cbv_flow_invitation) { create(:cbv_flow_invitation, :sandbox) }
 
-  it "persists locale and allows switching back", skip: 'Translations not complete - FFS-4642' do
+  it "persists locale and allows switching back" do
     # Visit the entry page via invitation URL (in English by default)
     visit URI(cbv_flow_invitation.to_url).request_uri
     verify_page(page, title: I18n.t("cbv.entries.show.header", locale: :en))
