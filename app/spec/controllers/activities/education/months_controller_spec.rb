80209356c6f8ceb798bdaeaf55af2c7bf9f17e03
Krista Chan <kristachan@Kristas-MacBook-Pro.local>
2026-09-04 14:27:34 -0700

    Revert "either income or hours"
    This reverts commit f9a016bf75bbea1f38ce2d76334d22dbc1056035.

--- app/spec/controllers/activities/education/months_controller_spec.rb
+++ app/spec/controllers/activities/education/months_controller_spec.rb
@@ -56,12 +56,7 @@ RSpec.describe Activities::Education::MonthsController, type: :controller do
 
       it_behaves_like "tracks an event", TrackEvent::EducationMonthValidationFailed,
         extra_attributes: -> {
-          {
-            education_activity_id: kind_of(Integer),
-            month_index: 0,
-            month: kind_of(String),
-            error_fields: array_including(a_string_matching(/gross_income|hours/))
-          }
+          { education_activity_id: kind_of(Integer), month_index: 0, month: kind_of(String), error_fields: %w[gross_income hours] }
         }
     end
 
