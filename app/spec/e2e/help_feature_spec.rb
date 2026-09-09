6dca8b27654bf7e6248dd2a7daaab632acd0505d
Chris Vetter <chrisvetter@navapbc.com>
2026-09-09 07:39:47 -0400

    Include activity_hub context for test

--- app/spec/e2e/help_feature_spec.rb
+++ app/spec/e2e/help_feature_spec.rb
@@ -1,6 +1,8 @@
 require "rails_helper"
 
 RSpec.describe "Help Features", :js, type: :feature do
+  include_context "activity_hub"
+
   include E2e::TestHelpers
   include PinwheelApiHelper
   include ApplicationHelper
@@ -83,17 +85,16 @@ RSpec.describe "Help Features", :js, type: :feature do
       find("button[aria-label='Close this window']").click
       expect(page).not_to have_selector(".usa-modal__content", visible: true)
     end
+  end
 
-    context "When in the activity flow" do
-      let(:activity_flow_invitation) { create(:activity_flow_invitation) }
+  context "When in the activity flow" do
+    before do
+      visit activities_flow_entry_path(client_agency_id: "sandbox")
+      verify_page(page, title: I18n.t("activities.entries.show.title", benefit: "Medicaid"))
+    end
 
-      before do
-        visit URI(activity_flow_invitation.to_url).request_uri
-      end
-
-      it "does not show the help link" do
-        expect(page).not_to have_link("Help")
-      end
+    it "does not show the help link" do
+      expect(page).not_to have_link("Help")
     end
   end
 end
