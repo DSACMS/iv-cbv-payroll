b071c7a986fb50675d3d1da71ce4460b1767b737
Chris Vetter <chrisvetter@navapbc.com>
2026-09-24 15:19:02 -0400

    Update invitations tests

--- app/spec/controllers/api/v2/invitations_controller_spec.rb
+++ app/spec/controllers/api/v2/invitations_controller_spec.rb
@@ -14,12 +14,12 @@ RSpec.describe Api::V2::InvitationsController do
 
     let(:valid_params) do
       attributes_for(:cbv_flow_invitation, client_agency_id).tap do |params|
-        params[:invitation_type] = "income"
+        params[:invitation_type] = "community_engagement"
+        params[:verification_range] = "last_complete_month"
         params[:agency_partner_metadata] = attributes_for(:cbv_applicant, client_agency_id)
+        params[:agency_partner_metadata][:individual_id] = "ABC1234"
         params[:agency_partner_metadata][:first_name] = "John"
         params[:agency_partner_metadata][:last_name] = "Doe"
-        params[:agency_partner_metadata][:case_number] = "123456789"
-        params[:agency_partner_metadata][:date_of_birth] = "1990-01-01"
         # ensure that client_agency_id is not considered a valid param. it should be inferred from the api token
         params[:agency_partner_metadata].delete(:client_agency_id)
         params.delete(:client_agency_id)
@@ -32,7 +32,7 @@ RSpec.describe Api::V2::InvitationsController do
 
     it "creates an invitation with an associated cbv_applicant" do
       expect { create_invitation }
-          .to change(CbvFlowInvitation, :count).by(1)
+          .to change(ActivityFlowInvitation, :count).by(1)
           .and change(CbvApplicant, :count).by(1)
 
       expect(response).to have_http_status(:created)
@@ -42,10 +42,10 @@ RSpec.describe Api::V2::InvitationsController do
 
     it "creates an invitation using the client_agency_id in the access_token" do
       expect { create_invitation }
-          .to change(CbvFlowInvitation, :count).by(1)
+          .to change(ActivityFlowInvitation, :count).by(1)
           .and change(CbvApplicant, :count).by(1)
 
-      invitation = CbvFlowInvitation.last
+      invitation = ActivityFlowInvitation.last
       expect(invitation.client_agency_id).to eq(client_agency_id.to_s)
     end
 
@@ -53,21 +53,22 @@ RSpec.describe Api::V2::InvitationsController do
       let(:client_agency_id) { "la_ldh".to_sym }
       let(:valid_params) do
         attributes_for(:cbv_flow_invitation, client_agency_id).tap do |params|
-          params[:invitation_type] = "income"
+          params[:invitation_type] = "community_engagement"
+          params[:verification_range] = "last_complete_month"
           params[:agency_partner_metadata] = {
-            case_number: nil,
-            date_of_birth: "1977-09-13",
-            individual_id: "ABC1234"
+            individual_id: "ABC1234",
+            first_name: "John",
+            last_name: "Doe"
           }
         end
       end
 
       it "creates an invitation" do
         expect { create_invitation }
-          .to change(CbvFlowInvitation, :count).by(1)
+          .to change(ActivityFlowInvitation, :count).by(1)
           .and change(CbvApplicant, :count).by(1)
 
-        invitation = CbvFlowInvitation.last
+        invitation = ActivityFlowInvitation.last
         expect(invitation.client_agency_id).to eq(client_agency_id.to_s)
 
         applicant = invitation.cbv_applicant
@@ -80,76 +81,9 @@ RSpec.describe Api::V2::InvitationsController do
         parsed_response = JSON.parse(response.body)
         expect(parsed_response["agency_partner_metadata"]).to eq(
           "individual_id" => valid_params[:agency_partner_metadata][:individual_id],
-          "case_number" => valid_params[:agency_partner_metadata][:case_number],
-          "date_of_birth" => valid_params[:agency_partner_metadata][:date_of_birth],
+          "first_name" => valid_params[:agency_partner_metadata][:first_name],
+          "last_name" => valid_params[:agency_partner_metadata][:last_name]
         )
-      end
-
-      it "returns 422 when both doc_id and individual_id are nil for income" do
-        valid_params[:agency_partner_metadata] = {
-          case_number: nil,
-          date_of_birth: "1977-09-13",
-          doc_id: nil,
-          individual_id: nil
-        }
-
-        post :create, params: valid_params
-
-        expect(response).to have_http_status(:unprocessable_content)
-
-        parsed_response = JSON.parse(response.body)
-
-        expect(parsed_response["errors"]).to include(
-          "field" => "doc_id_or_individual_id",
-          "message" =>
-            I18n.t("api.v2.la_ldh.fields.doc_id_or_individual_id.blank")
-        )
-      end
-    end
-
-    context "when inviting a user to community engagement" do
-      let(:client_agency_id) { "sandbox".to_sym }
-
-      let(:valid_params) do
-        {
-          invitation_type: "community_engagement",
-          verification_range: "last_complete_month",
-          language: "en",
-          agency_partner_metadata: {
-            individual_id: "IND123",
-            first_name: "Jane",
-            last_name: "Doe",
-            date_of_birth: "1977-09-13"
-          }
-        }
-      end
-
-      it "creates income and activity invitations for the same applicant" do
-        expect do
-          create_invitation
-        end.to change(CbvFlowInvitation, :count).by(1)
-          .and change(ActivityFlowInvitation, :count).by(1)
-          .and change(CbvApplicant, :count).by(1)
-
-        expect(response).to have_http_status(:created)
-
-        parsed_response = JSON.parse(response.body)
-
-        expect(parsed_response).to include(
-          "tokenized_url",
-          "activity_tokenized_url"
-        )
-
-        applicant = CbvFlowInvitation.last.cbv_applicant
-
-        expect(applicant).to have_attributes(
-          client_agency_id: "sandbox",
-          first_name: "Jane",
-          last_name: "Doe",
-          individual_id: "IND123"
-        )
-
-        expect(ActivityFlowInvitation.last).to have_attributes(cbv_applicant: applicant, verification_range: "last_complete_month")
       end
 
       %i[individual_id first_name last_name].each do |field|
@@ -178,7 +112,7 @@ RSpec.describe Api::V2::InvitationsController do
         post :create, params: invalid_attribute_params
 
         expect(response).to have_http_status(:created)
-        expect(CbvFlowInvitation.last.cbv_applicant.case_number).to be_nil
+        expect(ActivityFlowInvitation.last.cbv_applicant.case_number).to be_nil
       end
     end
   end
